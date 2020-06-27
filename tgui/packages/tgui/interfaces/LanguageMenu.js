a168e1c55749eb607f44af74fe29184b6c93c81b
yorii <yoriisoet@gmail.com>
2020-06-27 08:15:23 +0200

    Update LanguageMenu.js

--- tgui/packages/tgui/interfaces/LanguageMenu.js
+++ tgui/packages/tgui/interfaces/LanguageMenu.js
@@ -91,6 +91,8 @@ export const LanguageMenu = (props, context) => {
                   {' '}
                   Key: ,{language.key}
                   {' '}
+                  {!!language.shadow && '(gained from mob)'}
+                  {' '}
                   {language.can_understand
                     ? 'Can understand.'
                     : 'Cannot understand.'}
