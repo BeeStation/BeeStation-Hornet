c6b4e41494bdc907be083dcd658c641cf21be07f
LatteKat <sterlite2902@gmail.com>
2020-11-11 11:55:47 +1100

    ided

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
+EXIT_CODE=0
 
 tools/deploy.sh ci_test
 rm ci_test/*.dll
@@ -9,6 +10,15 @@ mkdir ci_test/config
 cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
-DreamDaemon beestation.dmb -close -trusted -verbose -params "log-directory=ci"
+ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
+DreamDaemon beestation.dmb -close -trusted -verbose -params "log-directory=ci" || EXIT_CODE=$?
+
+#We don't care if extools dies
+if [ $EXIT_CODE != 134 ]; then
+   if [ $EXIT_CODE != 0 ]; then
+      exit $EXIT_CODE
+   fi
+fi
+
 cd ..
 cat ci_test/data/logs/ci/clean_run.lk
