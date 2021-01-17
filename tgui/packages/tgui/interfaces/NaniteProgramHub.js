3fbe135845daa3502b1c13f6b2a42b577f076515
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2021-01-16 22:33:43 -0800

    Slightly Updated
    Co-Authored-By: St0rmC4st3r <34888552+St0rmC4st3r@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/NaniteProgramHub.js
+++ tgui/packages/tgui/interfaces/NaniteProgramHub.js
@@ -1,45 +1,47 @@
-import { map } from 'common/collections';
-import { Fragment } from 'inferno';
-import { useBackend, useSharedState } from '../backend';
-import { Button, Flex, LabeledList, NoticeBox, Section, Tabs } from '../components';
-import { Window } from '../layouts';
+import { map } from "common/collections";
+import { Fragment } from "inferno";
+import { useBackend, useSharedState } from "../backend";
+import {
+  Box,
+  Flex,
+  Icon,
+  Tabs,
+  Button,
+  Section,
+  NoticeBox,
+  LabeledList,
+} from "../components";
+import { Window } from "../layouts";
 
 export const NaniteProgramHub = (props, context) => {
   const { act, data } = useBackend(context);
-  const {
-    detail_view,
-    disk,
-    has_disk,
-    has_program,
-    programs = {},
-  } = data;
-  const [
-    selectedCategory,
-    setSelectedCategory,
-  ] = useSharedState(context, 'category');
-  const programsInCategory = programs
-    && programs[selectedCategory]
-    || [];
+  const { detail_view, disk, has_disk, has_program, programs = {} } = data;
+  const [selectedCategory, setSelectedCategory] = useSharedState(
+    context,
+    "category"
+  );
+  const programsInCategory = (programs && programs[selectedCategory]) || [];
   return (
-    <Window
-      resizable
-      width={500}
-      height={700}>
-      <Window.Content scrollable>
+    <Window resizable width={650} height={700}>
+      <Window.Content>
         <Section
           title="Program Disk"
-          buttons={(
+          height="20%"
+          overflowY="scroll"
+          buttons={
             <Fragment>
               <Button
                 icon="eject"
                 content="Eject"
-                onClick={() => act('eject')} />
+                onClick={() => act("eject")}
+              />
               <Button
                 icon="minus-circle"
                 content="Delete Program"
-                onClick={() => act('clear')} />
+                onClick={() => act("clear")}
+              />
             </Fragment>
-          )}>
+          }>
           {has_disk ? (
             has_program ? (
               <LabeledList>
@@ -51,67 +53,82 @@ export const NaniteProgramHub = (props, context) => {
                 </LabeledList.Item>
               </LabeledList>
             ) : (
-              <NoticeBox>
-                No Program Installed
-              </NoticeBox>
+              <NoticeBox>No Program Installed</NoticeBox>
             )
           ) : (
-            <NoticeBox>
-              Insert Disk
-            </NoticeBox>
+            <NoticeBox>Insert Disk</NoticeBox>
           )}
         </Section>
         <Section
           title="Programs"
-          buttons={(
+          buttons={
             <Fragment>
               <Button
-                icon={detail_view ? 'info' : 'list'}
-                content={detail_view ? 'Detailed' : 'Compact'}
-                onClick={() => act('toggle_details')} />
+                icon={detail_view ? "info" : "list"}
+                content={detail_view ? "Detailed" : "Compact"}
+                onClick={() => act("toggle_details")}
+              />
               <Button
                 icon="sync"
                 content="Sync Research"
-                onClick={() => act('refresh')} />
+                onClick={() => act("refresh")}
+              />
             </Fragment>
-          )}>
+          }>
           {programs !== null ? (
-            <Flex>
-              <Flex.Item minWidth="110px">
+            <Flex height={39}>
+              <Flex.Item minWidth="150px">
                 <Tabs vertical>
                   {map((cat_contents, category) => {
                     const progs = cat_contents || [];
                     // Backend was sending stupid data that would have been
                     // annoying to fix
-                    const tabLabel = category
-                      .substring(0, category.length - 8);
+                    const tabLabel = category.substring(0, category.length - 8);
                     return (
-                      <Tabs.Tab
+                      <Button
                         key={category}
                         selected={category === selectedCategory}
-                        onClick={() => setSelectedCategory(category)}>
-                        {tabLabel}
-                      </Tabs.Tab>
+                        onClick={() => setSelectedCategory(category)}
+                        mb={2}
+                        width="80%"
+                        height={2} >
+                        <Flex direction="row" justify="space-between">
+                          <Icon
+                            mt={1}
+                            color={
+                              category === selectedCategory
+                                ? "lime"
+                                : "lightskyblue"
+                            }
+                            name="microchip"
+                          />
+                          {tabLabel}
+                        </Flex>
+                      </Button>
                     );
                   })(programs)}
                 </Tabs>
               </Flex.Item>
-              <Flex.Item grow={1} basis={0}>
+              <Flex.Item grow width="100%" overflowY="scroll">
                 {detail_view ? (
                   programsInCategory.map(program => (
                     <Section
                       key={program.id}
                       title={program.name}
                       level={2}
-                      buttons={(
+                      width="100%"
+                      buttons={
                         <Button
-                          icon="download"
-                          content="Download"
+                          height={2}
                           disabled={!has_disk}
-                          onClick={() => act('download', {
-                            program_id: program.id,
-                          })} />
-                      )}>
+                          onClick={() =>
+                            act("download", {
+                              program_id: program.id,
+                            })}>
+                          <Icon mr={2} name="download" />
+                          Download
+                        </Button>
+                      }>
                       {program.desc}
                     </Section>
                   ))
@@ -121,24 +138,27 @@ export const NaniteProgramHub = (props, context) => {
                       <LabeledList.Item
                         key={program.id}
                         label={program.name}
-                        buttons={(
+                        buttons={
                           <Button
-                            icon="download"
-                            content="Download"
+                            mt={1}
+                            mr={2}
+                            height={2}
                             disabled={!has_disk}
-                            onClick={() => act('download', {
-                              program_id: program.id,
-                            })} />
-                        )} />
+                            onClick={() =>
+                              act("download", {
+                                program_id: program.id,
+                              })}>
+                            <Icon mr={2} name="download" />
+                            Download
+                          </Button>
+                        } />
                     ))}
                   </LabeledList>
                 )}
               </Flex.Item>
             </Flex>
           ) : (
-            <NoticeBox>
-              No nanite programs are currently researched.
-            </NoticeBox>
+            <NoticeBox>No nanite programs are currently researched.</NoticeBox>
           )}
         </Section>
       </Window.Content>
