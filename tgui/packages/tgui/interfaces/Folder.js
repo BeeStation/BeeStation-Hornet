29d9290b61d4f0b7126c79e2014f6326ed6a730a
Autistic <36102060+Autisem@users.noreply.github.com>
2021-03-03 13:08:38 +0100

    :joker:

--- tgui/packages/tgui/interfaces/Folder.js
+++ tgui/packages/tgui/interfaces/Folder.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 import { useBackend } from "../backend";
 import { Box, Button, Flex, Section } from "../components";
 import { Window } from "../layouts";
