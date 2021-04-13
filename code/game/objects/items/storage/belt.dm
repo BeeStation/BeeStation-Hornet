HolyTigerGod/Voidcrew

mining keys to open tendrills crates buyable with miner points (#56267)

code/game/objects/items/storage/belt.dm
@@ -308,7 +308,8 @@
 		/obj/item/organ/regenerative_core,
 		/obj/item/wormhole_jaunter,
 		/obj/item/stack/marker_beacon,
-		/obj/item/key/lasso
+		/obj/item/key/lasso,
+		/obj/item/skeleton_key
 		))
 
 

BeeStation/BeeStation-Hornet

code/game/objects/items/storage/belt.dm
@@ -355,7 +355,8 @@
 		/obj/item/storage/bag/plants,
 		/obj/item/stack/marker_beacon,
 		/obj/item/restraints/legcuffs/bola/watcher,
-		/obj/item/claymore/bone
+		/obj/item/claymore/bone,
+		/obj/item/skeleton_key
 		))
 
 