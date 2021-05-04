HolyTigerGod/Voidcrew

Update Dockerfile for CBT (#56175)
Follow-up to fix the failing Docker CI on master.

- Reorganize the entire Dockerfile to be more readable
- Inline the tgstation/byond Dockerfile into our own, so we can change the base distro at will
- Also allows us to trash the dependencies.sh<->Dockerfile hack
- Use 32-bit libs on a 64-bit distro so that we can download and run recent 64-bit Node binaries
- Call tools/build/build rather than DreamMaker directly

Dockerfile
@@ -1,62 +1,86 @@
-FROM tgstation/byond:513.1533 as base
+# base = ubuntu + full apt update
+FROM ubuntu:xenial AS base
 
-FROM base as rust_g
-
-RUN apt-get update \
+RUN dpkg --add-architecture i386 \
+    && apt-get update \
+    && apt-get upgrade -y \
+    && apt-get dist-upgrade -y \
     && apt-get install -y --no-install-recommends \
-    git \
-    ca-certificates
+        ca-certificates
 
+# byond = base + byond installed globally
+FROM base AS byond
+WORKDIR /byond
+
+RUN apt-get install -y --no-install-recommends \
+        curl \
+        unzip \
+        make \
+        libstdc++6:i386
+
+COPY dependencies.sh .
+
+RUN . ./dependencies.sh \
+    && curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip \
+    && unzip byond.zip \
+    && cd byond \
+    && sed -i 's|install:|&\n\tmkdir -p $(MAN_DIR)/man6|' Makefile \
+    && make install \
+    && chmod 644 /usr/local/byond/man/man6/* \
+    && apt-get purge -y --auto-remove curl unzip make \
+    && cd .. \
+    && rm -rf byond byond.zip
+
+# build = byond + tgstation compiled and deployed to /deploy
+FROM byond AS build
+WORKDIR /tgstation
+
+RUN apt-get install -y --no-install-recommends \
+        curl
+
+COPY . .
+
+RUN env TG_BOOTSTRAP_NODE_LINUX=1 tools/build/build \
+    && tools/deploy.sh /deploy \
+	&& rm /deploy/*.dll
+
+# rust = base + rustc and i686 target
+FROM base AS rust
+RUN apt-get install -y --no-install-recommends \
+        curl && \
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal \
+    && ~/.cargo/bin/rustup target add i686-unknown-linux-gnu
+
+# rust_g = base + rust_g compiled to /rust_g
+FROM rust AS rust_g
 WORKDIR /rust_g
 
 RUN apt-get install -y --no-install-recommends \
-    libssl-dev \
-    pkg-config \
-    curl \
-    gcc-multilib \
-    && curl https://sh.rustup.rs -sSf | sh -s -- -y --default-host i686-unknown-linux-gnu \
+        pkg-config:i386 \
+        libssl-dev:i386 \
+        gcc-multilib \
+        git \
     && git init \
     && git remote add origin https://github.com/tgstation/rust-g
 
 COPY dependencies.sh .
 
-RUN /bin/bash -c "source dependencies.sh \
-    && git fetch --depth 1 origin \$RUST_G_VERSION" \
+RUN . ./dependencies.sh \
+    && git fetch --depth 1 origin "${RUST_G_VERSION}" \
     && git checkout FETCH_HEAD \
-    && ~/.cargo/bin/cargo build --release
+    && env PKG_CONFIG_ALLOW_CROSS=1 ~/.cargo/bin/cargo build --release --target i686-unknown-linux-gnu
 
-FROM base as dm_base
-
+# final = byond + runtime deps + rust_g + build
+FROM byond
 WORKDIR /tgstation
 
-FROM dm_base as build
+RUN apt-get install -y --no-install-recommends \
+        libssl1.0.0:i386 \
+        zlib1g:i386
 
-COPY . .
-
-RUN DreamMaker -max_errors 0 tgstation.dme \
-    && tools/deploy.sh /deploy \
-	&& rm /deploy/*.dll
-
-FROM dm_base
-
-EXPOSE 1337
-
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends software-properties-common \
-    && add-apt-repository ppa:ubuntu-toolchain-r/test \
-    && apt-get update \
-    && apt-get upgrade -y \
-    && apt-get dist-upgrade -y \
-    && apt-get install -y --no-install-recommends \
-    libmariadb2 \
-    mariadb-client \
-    libssl1.0.0 \
-    && rm -rf /var/lib/apt/lists/* \
-    && mkdir -p /root/.byond/bin
-
-COPY --from=rust_g /rust_g/target/release/librust_g.so /root/.byond/bin/rust_g
 COPY --from=build /deploy ./
+COPY --from=rust_g /rust_g/target/i686-unknown-linux-gnu/release/librust_g.so ./librust_g.so
 
 VOLUME [ "/tgstation/config", "/tgstation/data" ]
-
 ENTRYPOINT [ "DreamDaemon", "tgstation.dmb", "-port", "1337", "-trusted", "-close", "-verbose" ]
+EXPOSE 1337

BeeStation/BeeStation-Hornet

Dockerfile
@@ -1,72 +1,86 @@
-FROM beestation/byond:513.1536 as base
-ONBUILD ENV BYOND_MAJOR=513
-ONBUILD ENV BYOND_MINOR=1536
+# base = ubuntu + full apt update
+FROM ubuntu:xenial AS base
 
-FROM base as build_base
-
-RUN apt-get update \
+RUN dpkg --add-architecture i386 \
+    && apt-get update \
+    && apt-get upgrade -y \
+    && apt-get dist-upgrade -y \
     && apt-get install -y --no-install-recommends \
-    git \
-	dos2unix\
-    ca-certificates
+        ca-certificates
 
-FROM build_base as rust_g
+# byond = base + byond installed globally
+FROM base AS byond
+WORKDIR /byond
 
+RUN apt-get install -y --no-install-recommends \
+        curl \
+        unzip \
+        make \
+        libstdc++6:i386
+
+COPY dependencies.sh .
+
+RUN . ./dependencies.sh \
+    && curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip \
+    && unzip byond.zip \
+    && cd byond \
+    && sed -i 's|install:|&\n\tmkdir -p $(MAN_DIR)/man6|' Makefile \
+    && make install \
+    && chmod 644 /usr/local/byond/man/man6/* \
+    && apt-get purge -y --auto-remove curl unzip make \
+    && cd .. \
+    && rm -rf byond byond.zip
+
+# build = byond + beestation compiled and deployed to /deploy
+FROM byond AS build
+WORKDIR /beestation
+
+RUN apt-get install -y --no-install-recommends \
+        curl
+
+COPY . .
+
+RUN env TG_BOOTSTRAP_NODE_LINUX=1 tools/build/build \
+    && tools/deploy.sh /deploy \
+	&& rm /deploy/*.dll
+
+# rust = base + rustc and i686 target
+FROM base AS rust
+RUN apt-get install -y --no-install-recommends \
+        curl && \
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal \
+    && ~/.cargo/bin/rustup target add i686-unknown-linux-gnu
+
+# rust_g = base + rust_g compiled to /rust_g
+FROM rust AS rust_g
 WORKDIR /rust_g
 
 RUN apt-get install -y --no-install-recommends \
-    libssl-dev \
-    pkg-config \
-    curl \
-    gcc-multilib \
-    && curl https://sh.rustup.rs -sSf | sh -s -- -y --default-host i686-unknown-linux-gnu \
+        pkg-config:i386 \
+        libssl-dev:i386 \
+        gcc-multilib \
+        git \
     && git init \
     && git remote add origin https://github.com/BeeStation/rust-g
 
 COPY dependencies.sh .
 
-RUN dos2unix dependencies.sh \
-	&& /bin/bash -c "source dependencies.sh \
-    && git fetch --depth 1 origin \$RUST_G_VERSION" \
+RUN . ./dependencies.sh \
+    && git fetch --depth 1 origin "${RUST_G_VERSION}" \
     && git checkout FETCH_HEAD \
-    && ~/.cargo/bin/cargo build --release --all-features \
-	&& apt-get --purge remove -y dos2unix
+    && env PKG_CONFIG_ALLOW_CROSS=1 ~/.cargo/bin/cargo build --release --target i686-unknown-linux-gnu
 
-FROM base as dm_base
-
+# final = byond + runtime deps + rust_g + build
+FROM byond
 WORKDIR /beestation
 
-FROM dm_base as build
+RUN apt-get install -y --no-install-recommends \
+        libssl1.0.0:i386 \
+        zlib1g:i386
 
-COPY . .
-
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends dos2unix \
-    && rm -rf /var/lib/apt/lists/* \
-    && DreamMaker -max_errors 0 beestation.dme && dos2unix tools/deploy.sh && tools/deploy.sh /deploy
-
-FROM dm_base
-
-EXPOSE 1337
-
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends software-properties-common \
-    && add-apt-repository ppa:ubuntu-toolchain-r/test \
-    && apt-get update \
-    && apt-get upgrade -y \
-    && apt-get dist-upgrade -y \
-    && apt-get install -y --no-install-recommends \
-    mariadb-client \
-    libssl1.0.0 \
-    && rm -rf /var/lib/apt/lists/* \
-    && mkdir -p /root/.byond/bin
-
-COPY --from=rust_g /rust_g/target/release/librust_g.so /root/.byond/bin/rust_g
 COPY --from=build /deploy ./
-
-#extools fexists memes
-RUN ln -s /beestation/libbyond-extools.so /root/.byond/bin/libbyond-extools.so
+COPY --from=rust_g /rust_g/target/i686-unknown-linux-gnu/release/librust_g.so ./librust_g.so
 
 VOLUME [ "/beestation/config", "/beestation/data" ]
-
 ENTRYPOINT [ "DreamDaemon", "beestation.dmb", "-port", "1337", "-trusted", "-close", "-verbose" ]
+EXPOSE 1337