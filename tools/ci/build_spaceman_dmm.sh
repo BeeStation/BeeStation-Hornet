070109d2cfc2bc70bf89563054dc63fa10834656
ike709 <ike709@users.noreply.github.com>
2021-05-10 20:25:23 -0500

    Missed a bit

--- tools/ci/build_spaceman_dmm.sh
+++ tools/ci/build_spaceman_dmm.sh
@@ -14,8 +14,4 @@ fi
 git fetch origin --depth=1 $SPACEMAN_DMM_COMMIT_HASH
 git reset --hard FETCH_HEAD
 
-cargo build --release --bin $1
-cp target/release/$1 ~
-~/$1 --version
-
 cargo build --release --bin dreamchecker
