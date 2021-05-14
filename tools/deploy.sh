79b87c5965076eef4aba6cb9bc919e9f2e24f503
Sinestia <zarth@mm.st>
2021-05-14 15:59:12 -0500

    getinthere

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -36,6 +36,5 @@ cp -r strings/* $1/strings/
 #find $1/_maps -name "*.dm" -type f -delete
 
 #dlls on windows
-if [ "$(uname -o)" = "Msys" ]; then
-	cp ./*.dll $1/
-fi
+cp rust_g* $1/ || true
+cp *byond-extools.* $1/ || true
