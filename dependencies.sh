591e333742d9507bd86e741fefe055120886a395
Froststahr <zarth@mm.st>
2021-06-10 21:39:00 -0500

    reverttoclassic

--- dependencies.sh
+++ dependencies.sh
@@ -4,15 +4,19 @@
 #Final authority on what's required to fully build the project
 
 # byond version
-export BYOND_MAJOR=513
-export BYOND_MINOR=1536
+# Extracted from the Dockerfile. Change by editing Dockerfile's ARG commands. Otherwise, this is set by Docker's envrionment variables
+if [[ -f "Dockerfile" ]]; then
+	LIST=($(sed -n 's/.*byond:\([0-9]\+\)\.\([0-9]\+\).*/\1 \2/p' Dockerfile))
+	export BYOND_MAJOR=${LIST[0]}
+	export BYOND_MINOR=${LIST[1]}
+	unset LIST
+fi
 
 #rust_g git tag
 export RUST_G_VERSION=0.4.7.1
 
 #node version
 export NODE_VERSION=12
-export NODE_VERSION_PRECISE=12.20.0
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.7
