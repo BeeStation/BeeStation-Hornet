02db88460e6f7eecbaa3a23a42cc91ace3a5255e
ike709 <ike709@beestation13.com>
2021-06-29 23:02:33 -0400

    unhardcode the version

--- tools/ci/install_auxmos.sh
+++ tools/ci/install_auxmos.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 source dependencies.sh
 
 mkdir -p ~/.byond/bin/auxtools
-wget -O ~/.byond/bin/auxtools/linda_libauxmos.so "https://github.com/BeeStation/auxmos/releases/download/0.1/linda_libauxmos.so"
+wget -O ~/.byond/bin/auxtools/linda_libauxmos.so "https://github.com/BeeStation/auxmos/releases/download/${AUXMOS_VERSION}/linda_libauxmos.so"
 chmod +x ~/.byond/bin/auxtools/linda_libauxmos.so
 ldd ~/.byond/bin/auxtools/linda_libauxmos.so
