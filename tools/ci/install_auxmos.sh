b6a98c6fbe052d2fee97fce7ed16acf6a2fe0e80
ike709 <ike709@beestation13.com>
2021-07-15 13:29:03 -0400

    God I hate linux

--- tools/ci/install_auxmos.sh
+++ tools/ci/install_auxmos.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 source dependencies.sh
 
 #mkdir -p ~/.byond/bin/auxtools
-wget -O ../../auxtools/libauxmos.so "https://github.com/BeeStation/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
-chmod +x ../../auxtools/libauxmos.so
-ldd ../../auxtools/libauxmos.so
+wget -O auxtools/libauxmos.so "https://github.com/BeeStation/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
+chmod +x auxtools/libauxmos.so
+ldd auxtools/libauxmos.so
