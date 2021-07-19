2fecb81cbc43783848ef0b44e02579c3fdb9f806
KubeRoot <kubinator4321@gmail.com>
2021-07-20 01:31:53 +0200

    Remove `resizable` from window

--- tgui/packages/tgui/interfaces/Stack.js
+++ tgui/packages/tgui/interfaces/Stack.js
@@ -47,8 +47,7 @@ export const Stack = (props, context) => {
   return (
     <Window
       width={400}
-      height={Math.min(height, 500)}
-      resizable>
+      height={Math.min(height, 500)}>
       <Window.Content scrollable>
         <Section
           title={"Amount: " + amount}
