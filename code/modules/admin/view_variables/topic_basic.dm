AzriAnDr/HowlingVoid

Allows proper application of arguments to components via dropdown (#47112)

code/modules/admin/view_variables/topic_basic.dm
@@ -66,12 +66,13 @@
 		if(!lst)
 			return
 		var/datumname = "error"
+		lst.Insert(1, result)
 		if(result in componentsubtypes)
 			datumname = "component"
-			target.AddComponent(result, lst)
+			target.AddComponent(arglist(lst))
 		else
 			datumname = "element"
-			target.AddElement(result, lst)
+			target.AddElement(arglist(lst))
 		log_admin("[key_name(usr)] has added [result] [datumname] to [key_name(src)].")
 		message_admins("<span class='notice'>[key_name_admin(usr)] has added [result] [datumname] to [key_name_admin(src)].</span>")
 	if(href_list[VV_HK_CALLPROC])

BeeStation/BeeStation-Hornet

(cherry picked from commit 159d2ec79d4f2177f40658a268ebbab8f5f19272)

code/modules/admin/view_variables/topic_basic.dm
@@ -81,10 +81,10 @@
 		lst.Insert(1, result)
 		if(result in componentsubtypes)
 			datumname = "component"
-			target._AddComponent(lst)
+			target._AddComponent(arglist(lst))
 		else
 			datumname = "element"
-			target._AddElement(lst)
+			target._AddElement(arglist(lst))
 		log_admin("[key_name(usr)] has added [result] [datumname] to [key_name(src)].")
 		message_admins("<span class='notice'>[key_name_admin(usr)] has added [result] [datumname] to [key_name_admin(src)].</span>")
 	if(href_list[VV_HK_CALLPROC])