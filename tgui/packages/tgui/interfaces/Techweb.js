f5301e85118b908d42f98fe377450516a8e82231
Katherine Kiefer <sterlite2902@gmail.com>
2021-09-07 10:39:03 +1000

    abbreviation

--- tgui/packages/tgui/interfaces/Techweb.js
+++ tgui/packages/tgui/interfaces/Techweb.js
@@ -74,6 +74,7 @@ const useRemappedBackend = context => {
 const abbreviations = {
   "General Research": "Gen. Res.",
   "Nanite Research": "Nanite Res.",
+  "Discovery Research": "Disc. Res.",
 };
 const abbreviateName = name => abbreviations[name] ?? name;
 
