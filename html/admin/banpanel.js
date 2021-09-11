1411244552d7bbc9c177beae2c95ac691e90db2f
Gallyus <5572280+francinum@users.noreply.github.com>
2021-09-11 16:45:33 -0400

    1984: Suppressed Bans (#5251)
    * Suppressor Permission
    Critical fix to ban notes
    
    * Confirmation alert
    
    * Allow Temp Bans
    Relevant terminology changes
    
    * I totally did not not do this at 3am
    
    * FUCK I FORGOT TO UPDATE THE UI
    TGUI SUCKS BUT HOLY SHIT THIS STUFF IS /AWFUL/
    
    * irc>tgs

--- html/admin/banpanel.js
+++ html/admin/banpanel.js
@@ -14,3 +14,19 @@ function toggle_checkboxes(source, ext) {
         }
     }
 }
+
+function suppression_lock(activator) {
+	var state = activator.checked
+	var restricted_elements = document.getElementsByClassName("redact_incompatible")
+	for (var i=0, n = restricted_elements.length; i < n; i++) {
+		restricted_elements[i].checked = false;
+		restricted_elements[i].disabled = state;
+	}
+	if(!state) {
+		return;
+	}
+	var force_enabled = document.getElementsByClassName("redact_force_checked");
+	for (var i=0, n = force_enabled.length; i < n; i++) {
+		force_enabled[i].checked = true;
+	}
+}
