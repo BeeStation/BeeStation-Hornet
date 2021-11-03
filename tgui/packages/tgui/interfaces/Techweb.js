3f85314a9ab6ff034fcd1d9da7c4c3f237561ccb
KubeRoot <kubinator4321@gmail.com>
2021-11-03 09:16:39 +0100

    Fix R&D Console's destructive analyzer interface (#5686)
    * Fix null reference
    
    * Hide the whole section if there are no reclaimable materials or research points

--- tgui/packages/tgui/interfaces/Techweb.js
+++ tgui/packages/tgui/interfaces/Techweb.js
@@ -498,7 +498,7 @@ const TechwebItemmaterials = (props, context) => {
   const { act, data } = useRemappedBackend(context);
   const { itemmats, itempoints } = data;
 
-  return (
+  return (itempoints || itemmats) && (
     <Section mt={1} className="Techweb__NodeContainer">
       {!!itempoints && (
         <>
@@ -514,16 +514,18 @@ const TechwebItemmaterials = (props, context) => {
           <Divider />
         </>
       )}
-      <Flex direction="column">
-        Reclaimable materials:
-        {itemmats.map(mats => {
-          return (
-            <Flex.Item key={mats}>
-              {mats}
-            </Flex.Item>
-          );
-        })}
-      </Flex>
+      {!!itemmats && (
+        <Flex direction="column">
+          Reclaimable materials:
+          {itemmats.map(mats => {
+            return (
+              <Flex.Item key={mats}>
+                {mats}
+              </Flex.Item>
+            );
+          })}
+        </Flex>
+      )}
     </Section>
   );
 };
