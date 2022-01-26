88af6ddd0a8b3eddd7d75a3aaff3689eb454bd11
Crossedfall <ping@crossedfall.io>
2022-01-25 20:13:20 -0600

    Updates the test script to run through all of the stuff we care about

--- bin/test.cmd
+++ bin/test.cmd
@@ -1,2 +1,4 @@
 @echo off
+call "%~dp0\..\tools\build\build.bat" --wait-on-error lint %*
+call "%~dp0\..\tools\build\build.bat" --wait-on-error tgui-test %*
 call "%~dp0\..\tools\build\build.bat" --wait-on-error dm-test %*
