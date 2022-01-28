db64f582f42b42ca7aa2379f83c53aa901abfa5f
Crossedfall <ping@crossedfall.io>
2022-01-28 12:24:41 -0600

    Has the test script also compile maps

--- bin/test.cmd
+++ bin/test.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "%~dp0\..\tools\build\build.bat" --wait-on-error lint %*
 call "%~dp0\..\tools\build\build.bat" --wait-on-error tgui-test %*
-call "%~dp0\..\tools\build\build.bat" --wait-on-error dm-test %*
+call "%~dp0\..\tools\build\build.bat" --wait-on-error dm-test -DCITESTING -DALL_MAPS %*
