HolyTigerGod/Voidcrew

Refactors tile reskinning (#54233)
* Refactors tile reskinning

* revert maxhealth changes

* what's with the extra spaces

* Include floors in the name changes

* tile fix and delete and create instead of change

* damn you github auto-resolve

* more atomization

code/game/objects/items/stacks/tiles/tile_mineral.dm
@@ -72,12 +72,87 @@
 /obj/item/stack/tile/mineral/titanium
 	name = "titanium tile"
 	singular_name = "titanium floor tile"
-	desc = "A tile made of titanium, used for shuttles. Use while in your hand to change what type of titanium tiles you want."
+	desc = "Sleek titanium tiles, used for shuttles. Use while in your hand to change what type of titanium tiles you want."
 	icon_state = "tile_shuttle"
 	inhand_icon_state = "tile-shuttle"
 	turf_type = /turf/open/floor/mineral/titanium
 	mineralType = "titanium"
 	custom_materials = list(/datum/material/titanium=500)
+	tile_reskin_types = list(
+		/obj/item/stack/tile/mineral/titanium,
+		/obj/item/stack/tile/mineral/titanium/yellow,
+		/obj/item/stack/tile/mineral/titanium/blue,
+		/obj/item/stack/tile/mineral/titanium/white,
+		/obj/item/stack/tile/mineral/titanium/purple,
+		/obj/item/stack/tile/mineral/titanium/tiled,
+		/obj/item/stack/tile/mineral/titanium/tiled/yellow,
+		/obj/item/stack/tile/mineral/titanium/tiled/blue,
+		/obj/item/stack/tile/mineral/titanium/tiled/white,
+		/obj/item/stack/tile/mineral/titanium/tiled/purple,
+		)
+
+/obj/item/stack/tile/mineral/titanium/yellow
+	name = "yellow titanium tile"
+	singular_name = "yellow titanium floor tile"
+	desc = "Sleek yellow titanium tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/yellow
+	icon_state = "tile_titanium_yellow"
+
+/obj/item/stack/tile/mineral/titanium/blue
+	name = "blue titanium tile"
+	singular_name = "blue titanium floor tile"
+	desc = "Sleek blue titanium tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/blue
+	icon_state = "tile_titanium_blue"
+
+/obj/item/stack/tile/mineral/titanium/white
+	name = "white titanium tile"
+	singular_name = "white titanium floor tile"
+	desc = "Sleek white titanium tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/white
+	icon_state = "tile_titanium_white"
+
+/obj/item/stack/tile/mineral/titanium/purple
+	name = "purple titanium tile"
+	singular_name = "purple titanium floor tile"
+	desc = "Sleek purple titanium tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/purple
+	icon_state = "tile_titanium_purple"
+
+/obj/item/stack/tile/mineral/titanium/tiled
+	name = "tiled titanium tile"
+	singular_name = "tiled titanium floor tile"
+	desc = "Titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/tiled
+	icon_state = "tile_titanium_tiled"
+
+/obj/item/stack/tile/mineral/titanium/tiled/yellow
+	name = "yellow titanium tile"
+	singular_name = "yellow titanium floor tile"
+	desc = "Yellow titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/tiled/yellow
+	icon_state = "tile_titanium_tiled_yellow"
+
+/obj/item/stack/tile/mineral/titanium/tiled/blue
+	name = "blue titanium tile"
+	singular_name = "blue titanium floor tile"
+	desc = "Blue titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/tiled/blue
+	icon_state = "tile_titanium_tiled_blue"
+
+/obj/item/stack/tile/mineral/titanium/tiled/white
+	name = "white titanium tile"
+	singular_name = "white titanium floor tile"
+	desc = "White titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/tiled/white
+	icon_state = "tile_titanium_tiled_white"
+
+/obj/item/stack/tile/mineral/titanium/tiled/purple
+	name = "purple titanium tile"
+	singular_name = "purple titanium floor tile"
+	desc = "Purple titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/tiled/purple
+	icon_state = "tile_titanium_tiled_purple"
 
 /obj/item/stack/tile/mineral/plastitanium
 	name = "plastitanium tile"

code/game/turfs/open/floor/mineral_floor.dm
@@ -97,24 +97,28 @@
 
 /turf/open/floor/mineral/titanium/yellow
 	icon_state = "titanium_yellow"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/yellow
 
 /turf/open/floor/mineral/titanium/yellow/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/blue
 	icon_state = "titanium_blue"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/blue
 
 /turf/open/floor/mineral/titanium/blue/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/white
 	icon_state = "titanium_white"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/white
 
 /turf/open/floor/mineral/titanium/white/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/purple
 	icon_state = "titanium_purple"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/purple
 
 /turf/open/floor/mineral/titanium/purple/airless
 	initial_gas_mix = AIRLESS_ATMOS
@@ -122,32 +126,37 @@
 // OLD TITANIUM (titanium floor tiles before PR #50454)
 /turf/open/floor/mineral/titanium/tiled
 	name = "titanium tile"
-	icon_state = "titanium_old"
+	icon_state = "titanium_tiled"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/tiled
 	broken_states = list("titanium_dam1_old","titanium_dam2_old","titanium_dam3_old","titanium_dam4_old","titanium_dam5_old")
 
 /turf/open/floor/mineral/titanium/tiled/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/tiled/yellow
-	icon_state = "titanium_yellow_old"
+	icon_state = "titanium_tiled_yellow"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/tiled/yellow
 
 /turf/open/floor/mineral/titanium/tiled/yellow/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/tiled/blue
-	icon_state = "titanium_blue_old"
+	icon_state = "titanium_tiled_blue"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/tiled/blue
 
 /turf/open/floor/mineral/titanium/tiled/blue/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/tiled/white
-	icon_state = "titanium_white_old"
+	icon_state = "titanium_tiled_white"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/tiled/white
 
 /turf/open/floor/mineral/titanium/tiled/white/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/tiled/purple
-	icon_state = "titanium_purple_old"
+	icon_state = "titanium_tiled_purple"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/tiled/purple
 
 /turf/open/floor/mineral/titanium/tiled/purple/airless
 	initial_gas_mix = AIRLESS_ATMOS

BeeStation/BeeStation-Hornet

code/game/objects/items/stacks/tiles/tile_mineral.dm
@@ -86,6 +86,81 @@
 	turf_type = /turf/open/floor/mineral/titanium
 	mineralType = "titanium"
 	materials = list(/datum/material/titanium=500)
+	tile_reskin_types = list(
+		/obj/item/stack/tile/mineral/titanium,
+		/obj/item/stack/tile/mineral/titanium/yellow,
+		/obj/item/stack/tile/mineral/titanium/blue,
+		/obj/item/stack/tile/mineral/titanium/white,
+		/obj/item/stack/tile/mineral/titanium/purple,
+		/obj/item/stack/tile/mineral/titanium/alt,
+		/obj/item/stack/tile/mineral/titanium/alt/yellow,
+		/obj/item/stack/tile/mineral/titanium/alt/blue,
+		/obj/item/stack/tile/mineral/titanium/alt/white,
+		/obj/item/stack/tile/mineral/titanium/alt/purple,
+		)
+
+/obj/item/stack/tile/mineral/titanium/yellow
+	name = "yellow titanium tile"
+	singular_name = "yellow titanium floor tile"
+	desc = "Yellow titanium tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/yellow
+	icon_state = "tile_titanium_yellow"
+
+/obj/item/stack/tile/mineral/titanium/blue
+	name = "blue titanium tile"
+	singular_name = "blue titanium floor tile"
+	desc = "Blue titanium tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/blue
+	icon_state = "tile_titanium_blue"
+
+/obj/item/stack/tile/mineral/titanium/white
+	name = "white titanium tile"
+	singular_name = "white titanium floor tile"
+	desc = "White titanium tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/white
+	icon_state = "tile_titanium_white"
+
+/obj/item/stack/tile/mineral/titanium/purple
+	name = "purple titanium tile"
+	singular_name = "purple titanium floor tile"
+	desc = "Purple titanium tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/purple
+	icon_state = "tile_titanium_purple"
+
+/obj/item/stack/tile/mineral/titanium/alt
+	name = "sleek titanium tile"
+	singular_name = "sleek titanium floor tile"
+	desc = "Sleek titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/alt
+	icon_state = "tile_titanium_alt"
+
+/obj/item/stack/tile/mineral/titanium/alt/yellow
+	name = "sleek yellow titanium tile"
+	singular_name = "sleek yellow titanium floor tile"
+	desc = "Sleek yellow titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/alt/yellow
+	icon_state = "tile_titanium_yellow_alt"
+
+/obj/item/stack/tile/mineral/titanium/alt/blue
+	name = "sleek blue titanium tile"
+	singular_name = "sleek blue titanium floor tile"
+	desc = "Sleek blue titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/alt/blue
+	icon_state = "tile_titanium_blue_alt"
+
+/obj/item/stack/tile/mineral/titanium/alt/white
+	name = "sleek white titanium tile"
+	singular_name = "sleek white titanium floor tile"
+	desc = "Sleek white titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/alt/white
+	icon_state = "tile_titanium_white_alt"
+
+/obj/item/stack/tile/mineral/titanium/alt/purple
+	name = "sleek purple titanium tile"
+	singular_name = "sleek purple titanium floor tile"
+	desc = "Sleek purple titanium floor tiles. Use while in your hand to change what type of titanium tiles you want."
+	turf_type = /turf/open/floor/mineral/titanium/alt/purple
+	icon_state = "tile_titanium_purple_alt"
 
 /obj/item/stack/tile/mineral/plastitanium
 	name = "plastitanium tile"

code/game/turfs/open/floor/mineral_floor.dm
@@ -97,32 +97,36 @@
 
 /turf/open/floor/mineral/titanium/yellow
 	icon_state = "titanium_yellow"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/yellow
 
 /turf/open/floor/mineral/titanium/yellow/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/blue
 	icon_state = "titanium_blue"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/blue
 
 /turf/open/floor/mineral/titanium/blue/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/white
 	icon_state = "titanium_white"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/white
 
 /turf/open/floor/mineral/titanium/white/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/purple
 	icon_state = "titanium_purple"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/purple
 
 /turf/open/floor/mineral/titanium/purple/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
-// OLD TITANIUM (titanium floor tiles before PR #7318)
 /turf/open/floor/mineral/titanium/alt
 	name = "titanium floor"
 	icon_state = "titanium_alt"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/alt
 	broken_states = list("titanium_dam1_alt","titanium_dam2_alt","titanium_dam3_alt","titanium_dam4_alt","titanium_dam5_alt")
 
 /turf/open/floor/mineral/titanium/alt/airless
@@ -130,24 +134,28 @@
 
 /turf/open/floor/mineral/titanium/alt/yellow
 	icon_state = "titanium_yellow_alt"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/alt/yellow
 
 /turf/open/floor/mineral/titanium/alt/yellow/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/alt/blue
 	icon_state = "titanium_blue_alt"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/alt/yellow
 
 /turf/open/floor/mineral/titanium/alt/blue/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/alt/white
 	icon_state = "titanium_white_alt"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/alt/yellow
 
 /turf/open/floor/mineral/titanium/alt/white/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/mineral/titanium/alt/purple
 	icon_state = "titanium_purple_alt"
+	floor_tile = /obj/item/stack/tile/mineral/titanium/alt/yellow
 
 /turf/open/floor/mineral/titanium/alt/purple/airless
 	initial_gas_mix = AIRLESS_ATMOS