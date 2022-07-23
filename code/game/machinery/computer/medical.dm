AzriAnDr/HowlingVoid

Console alt-clicking tweaks (#45193)
* * reworked consoles so you can:
 * use attackby to insert id
 * use alt-click to eject id
 * examine to see if alt-click is available
 * moved eject_id and insert_id procs to _computer.dm
 * added some sound effects

* * prisoner management console new features: insert id with attackby and eject with altclick

* * prisoner management console can't have multiple prisoner id's inserted

* removed unnecessary src. and changed usr --> user

* made gulag teleporter consistent with attackby, altclick and examine.
simplified attackby on card.dm

* equipment reclaimer station consistent with id attackby, altclick-eject and examine.
point claim console consistent with id attackby, altclick-eject and examine.

* ore redemption machine and mining equipment vendor made consistent with id attackby, altclick-eject and examine.

* * reworked all eject/insert ID copypastacodes into procs in _machinery.dm

* * 0's to FALSE

* hopefully i didnt mess up any more things in the resolve

* everything now uses id_insert-proc and doesn't work.

* compiles but doesn't work

* works

code/game/machinery/computer/medical.dm
@@ -7,8 +7,6 @@
 	icon_keyboard = "med_key"
 	req_one_access = list(ACCESS_MEDICAL, ACCESS_FORENSICS_LOCKERS)
 	circuit = /obj/item/circuitboard/computer/med_data
-	var/obj/item/card/id/scan = null
-	var/authenticated = null
 	var/rank = null
 	var/screen = null
 	var/datum/data/record/active1
@@ -25,22 +23,14 @@
 /obj/machinery/computer/med_data/syndie
 	icon_keyboard = "syndie_key"
 
-/obj/machinery/computer/med_data/attackby(obj/item/O, mob/user, params)
-	if(istype(O, /obj/item/card/id) && !scan)
-		if(!user.transferItemToLoc(O, src))
-			return
-		scan = O
-		to_chat(user, "<span class='notice'>You insert [O].</span>")
-	else
-		return ..()
-
 /obj/machinery/computer/med_data/ui_interact(mob/user)
 	. = ..()
+	playsound(src, 'sound/machines/terminal_prompt_confirm.ogg', 50, FALSE)
 	var/dat
 	if(temp)
 		dat = text("<TT>[temp]</TT><BR><BR><A href='?src=[REF(src)];temp=1'>Clear Screen</A>")
 	else
-		dat = text("Confirm Identity: <A href='?src=[REF(src)];scan=1'>[]</A><HR>", (src.scan ? text("[]", src.scan.name) : "----------"))
+		dat = text("Confirm Identity: <A href='?src=[REF(src)];inserted_scan_id=1'>[]</A><HR>", (src.inserted_scan_id ? text("[]", src.inserted_scan_id.name) : "----------"))
 		if(src.authenticated)
 			switch(src.screen)
 				if(1)
@@ -205,21 +195,16 @@
 		usr.set_machine(src)
 		if(href_list["temp"])
 			src.temp = null
-		if(href_list["scan"])
-			if(src.scan)
-				usr.put_in_hands(scan)
-				scan = null
-			else
-				var/obj/item/I = usr.is_holding_item_of_type(/obj/item/card/id)
-				if(I)
-					if(!usr.transferItemToLoc(I, src))
-						return
-					src.scan = I
+		if(href_list["inserted_scan_id"])
+			if(inserted_scan_id)
+				id_eject(usr, inserted_scan_id)
+				inserted_scan_id = null
 		else if(href_list["logout"])
 			src.authenticated = null
 			src.screen = null
 			src.active1 = null
 			src.active2 = null
+			playsound(src, 'sound/machines/terminal_off.ogg', 50, FALSE)
 		else if(href_list["choice"])
 			// SORTING!
 			if(href_list["choice"] == "Sorting")
@@ -246,13 +231,16 @@
 				src.authenticated = 1
 				src.rank = "Central Command"
 				src.screen = 1
-			else if(istype(src.scan, /obj/item/card/id))
+			else if(istype(src.inserted_scan_id, /obj/item/card/id))
 				src.active1 = null
 				src.active2 = null
-				if(src.check_access(src.scan))
-					src.authenticated = src.scan.registered_name
-					src.rank = src.scan.assignment
+				if(src.check_access(src.inserted_scan_id))
+					src.authenticated = src.inserted_scan_id.registered_name
+					src.rank = src.inserted_scan_id.assignment
 					src.screen = 1
+				else
+					to_chat(usr, "<span class='danger'>Unauthorized access.</span>")
+			playsound(src, 'sound/machines/terminal_on.ogg', 50, FALSE)
 		if(src.authenticated)
 
 			if(href_list["screen"])

BeeStation/BeeStation-Hornet

code/game/machinery/computer/medical.dm
@@ -7,8 +7,6 @@
 	icon_keyboard = "med_key"
 	req_one_access = list(ACCESS_MEDICAL, ACCESS_FORENSICS_LOCKERS)
 	circuit = /obj/item/circuitboard/computer/med_data
-	var/obj/item/card/id/scan = null
-	var/authenticated = null
 	var/rank = null
 	var/screen = null
 	var/datum/data/record/active1
@@ -25,22 +23,13 @@
 /obj/machinery/computer/med_data/syndie
 	icon_keyboard = "syndie_key"
 
-/obj/machinery/computer/med_data/attackby(obj/item/O, mob/user, params)
-	if(istype(O, /obj/item/card/id) && !scan)
-		if(!user.transferItemToLoc(O, src))
-			return
-		scan = O
-		to_chat(user, "<span class='notice'>You insert [O].</span>")
-	else
-		return ..()
-
 /obj/machinery/computer/med_data/ui_interact(mob/user)
 	. = ..()
 	var/dat
 	if(temp)
 		dat = text("<TT>[temp]</TT><BR><BR><A href='?src=[REF(src)];temp=1'>Clear Screen</A>")
 	else
-		dat = text("Confirm Identity: <A href='?src=[REF(src)];scan=1'>[]</A><HR>", (src.scan ? text("[]", src.scan.name) : "----------"))
+		dat = text("Confirm Identity: <A href='?src=[REF(src)];scan=1'>[]</A><HR>", (src.inserted_scan_id ? text("[]", src.inserted_scan_id.name) : "----------"))
 		if(src.authenticated)
 			switch(src.screen)
 				if(1)
@@ -199,16 +188,10 @@
 		usr.set_machine(src)
 		if(href_list["temp"])
 			src.temp = null
-		if(href_list["scan"])
-			if(src.scan)
-				usr.put_in_hands(scan)
-				scan = null
-			else
-				var/obj/item/I = usr.is_holding_item_of_type(/obj/item/card/id)
-				if(I)
-					if(!usr.transferItemToLoc(I, src))
-						return
-					src.scan = I
+		if(href_list["inserted_scan_id"])
+			if(inserted_scan_id)
+				id_eject(usr, inserted_scan_id)
+				inserted_scan_id = null
 		else if(href_list["logout"])
 			src.authenticated = null
 			src.screen = null
@@ -240,13 +223,16 @@
 				src.authenticated = 1
 				src.rank = "Central Command"
 				src.screen = 1
-			else if(istype(src.scan, /obj/item/card/id))
+			else if(istype(src.inserted_scan_id, /obj/item/card/id))
 				src.active1 = null
 				src.active2 = null
-				if(src.check_access(src.scan))
-					src.authenticated = src.scan.registered_name
-					src.rank = src.scan.assignment
+				if(src.check_access(src.inserted_scan_id))
+					src.authenticated = src.inserted_scan_id.registered_name
+					src.rank = src.inserted_scan_id.assignment
 					src.screen = 1
+				else
+					to_chat(usr, "<span class='danger'>Unauthorized access.</span>")
+			playsound(src, 'sound/machines/terminal_on.ogg', 50, FALSE)
 		if(src.authenticated)
 
 			if(href_list["screen"])