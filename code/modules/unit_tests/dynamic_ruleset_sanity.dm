HolyTigerGod/Voidcrew

Dynamic 2022, Part 1 - Redesigning midround rolls (Midrounds happen far more frequently now, and scale their power over time) (#65997)
Implements the new midround roll changes.

1. Split midround rulesets into heavy/light impact
First, midround rulesets will be split into light/heavy impact categories. For example, sleeper agent and midround thief would be classified as light impact, while blob/ninja would be classified as heavy impact.

This split will then be used to spawn lighter impact antagonists earlier into the round, and higher impact antagonists later into the round.

Midrounds before 25 minutes = 100% light impact / 0% heavy impact
Midrounds from 25-60 minutes = Varying chances, increasing in favor of heavy impact after enough time
Midrounds from 60+ minutes = 0% light impact / 100% heavy impact
Low impact threat rulesets are guaranteed to roll if there is enough threat. High impact threat rulesets will carry the same % chance as they do now. If a heavy impact threat ruleset cannot be rolled, but there is enough threat, a light impact threat ruleset will roll in its place. In the future, this can potentially be changed into not spawning any ruleset if the station is deemed extremely hostile, or even be tweaked into spawning protagonists like ERTs.

Alongside this, rulesets should be able to determine their minimum time required. For example, nuclear assault has terrible numbers--it's very high cost, and very low weight, which are basically the only variables it can configure. In Dynamic 2022, it should be able to have low cost, still low weight (though maybe nto as much), while only being rollable after 70 minutes or so.

2. Midround antagonists now roll as intervals, determined by midround threat
In order to make sure 50 midround threat spawns more antagonists than 10 midround threat, the intervals at which midround threats are spawned will change from 15-45 minutes to increments determined by midround threat. Larger midround threat = more,

code/modules/unit_tests/dynamic_ruleset_sanity.dm
@@ -2,9 +2,7 @@
 /datum/unit_test/dynamic_roundstart_ruleset_sanity
 
 /datum/unit_test/dynamic_roundstart_ruleset_sanity/Run()
-	for (var/_ruleset in subtypesof(/datum/dynamic_ruleset/roundstart))
-		var/datum/dynamic_ruleset/roundstart/ruleset = _ruleset
-
+	for (var/datum/dynamic_ruleset/roundstart/ruleset as anything in subtypesof(/datum/dynamic_ruleset/roundstart))
 		var/has_scaling_cost = initial(ruleset.scaling_cost)
 		var/is_lone = initial(ruleset.flags) & (LONE_RULESET | HIGH_IMPACT_RULESET)
 
@@ -12,6 +10,11 @@
 			TEST_FAIL("[ruleset] has a scaling_cost, but is also a lone/highlander ruleset.")
 		else if (!has_scaling_cost && !is_lone)
 			TEST_FAIL("[ruleset] has no scaling cost, but is also not a lone/highlander ruleset.")
+
+	for (var/datum/dynamic_ruleset/midround/ruleset as anything in subtypesof(/datum/dynamic_ruleset/midround) - /datum/dynamic_ruleset/midround/from_ghosts)
+		var/midround_ruleset_style = initial(ruleset.midround_ruleset_style)
+		if (midround_ruleset_style != MIDROUND_RULESET_STYLE_HEAVY && midround_ruleset_style != MIDROUND_RULESET_STYLE_LIGHT)
+			TEST_FAIL("[ruleset] has an invalid midround_ruleset_style, it should be MIDROUND_RULESET_STYLE_HEAVY or MIDROUND_RULESET_STYLE_LIGHT")
 
 /// Verifies that dynamic rulesets have unique antag_flag.
 /datum/unit_test/dynamic_unique_antag_flags

BeeStation/BeeStation-Hornet

code/modules/unit_tests/dynamic_ruleset_sanity.dm
@@ -2,9 +2,7 @@
 /datum/unit_test/dynamic_roundstart_ruleset_sanity
 
 /datum/unit_test/dynamic_roundstart_ruleset_sanity/Run()
-	for (var/_ruleset in subtypesof(/datum/dynamic_ruleset/roundstart))
-		var/datum/dynamic_ruleset/roundstart/ruleset = _ruleset
-
+	for (var/datum/dynamic_ruleset/roundstart/ruleset as anything in subtypesof(/datum/dynamic_ruleset/roundstart))
 		var/has_scaling_cost = initial(ruleset.scaling_cost)
 		var/is_lone = initial(ruleset.flags) & (LONE_RULESET | HIGH_IMPACT_RULESET)
 
@@ -12,3 +10,8 @@
 			Fail("[ruleset] has a scaling_cost, but is also a lone/highlander ruleset.")
 		else if (!has_scaling_cost && !is_lone)
 			Fail("[ruleset] has no scaling cost, but is also not a lone/highlander ruleset.")
+
+	for (var/datum/dynamic_ruleset/midround/ruleset as anything in subtypesof(/datum/dynamic_ruleset/midround) - /datum/dynamic_ruleset/midround/from_ghosts)
+		var/midround_ruleset_style = initial(ruleset.midround_ruleset_style)
+		if (midround_ruleset_style != MIDROUND_RULESET_STYLE_HEAVY && midround_ruleset_style != MIDROUND_RULESET_STYLE_LIGHT)
+			Fail("[ruleset] has an invalid midround_ruleset_style, it should be MIDROUND_RULESET_STYLE_HEAVY or MIDROUND_RULESET_STYLE_LIGHT")