ee221e6ae486a30a58b879ce1586027f878cf95e
DrDuckedGoose <Hisphilosophy.comics@gmail.com>
2022-08-20 12:36:15 +1000

    Update XenoartifactConsole.js

--- tgui/packages/tgui/interfaces/XenoartifactConsole.js
+++ tgui/packages/tgui/interfaces/XenoartifactConsole.js
@@ -148,7 +148,6 @@ export const XenoartifactSell = (props, context) => {
           {sold_artifacts.map(item => (
             <Section key={item}>
               <BlockQuote>
-                {/*eslint-ignore-next-line react/no-danger*/}
                 <div dangerouslySetInnerHTML={{ __html: sanitize(item) }} />
               </BlockQuote>
             </Section>))}
