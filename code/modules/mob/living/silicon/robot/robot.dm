HolyTigerGod/Voidcrew

Fixes cyborg light overlay not showing when the light is off (#64279)
Borgs have a lightbulb overlay. The plane of the overlay depends on the status of the lights, so as to glow when on. The plane being used for disabled lights no longer shows above mobs for reasons I do not know. But this plane (-2) works.

code/modules/mob/living/silicon/robot/robot.dm
@@ -350,7 +350,7 @@
 		else
 			eye_lights.icon_state = "[model.special_light_key ? "[model.special_light_key]":"[model.cyborg_base_icon]"]_e"
 			eye_lights.color = COLOR_WHITE
-			eye_lights.plane = GAME_PLANE
+			eye_lights.plane = ABOVE_GAME_PLANE
 		eye_lights.icon = icon
 		add_overlay(eye_lights)
 

BeeStation/BeeStation-Hornet

code/modules/mob/living/silicon/robot/robot.dm
@@ -662,7 +662,7 @@
 		else
 			eye_lights.icon_state = "[module.special_light_key ? "[module.special_light_key]":"[module.cyborg_base_icon]"]_e[ratvar ? "_r" : ""]"
 			eye_lights.color = COLOR_WHITE
-			eye_lights.plane = GAME_PLANE
+			eye_lights.plane = OBJ_LAYER
 		eye_lights.icon = icon
 		add_overlay(eye_lights)
 