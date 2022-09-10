HolyTigerGod/Voidcrew

Fix: #41250 You can't use a tablet while inside a locker (#68171)

code/modules/mob/mob.dm
@@ -937,8 +937,13 @@
 	return 9
 
 ///Can the mob interact() with an atom?
-/mob/proc/can_interact_with(atom/A)
-	if(isAdminGhostAI(src) || Adjacent(A))
+/mob/proc/can_interact_with(atom/A, treat_mob_as_adjacent)
+	if(isAdminGhostAI(src))
+		return TRUE
+	//Return early. we do not need to check that we are on adjacent turfs (i.e we are inside a closet)
+	if (treat_mob_as_adjacent && src == A.loc)
+		return TRUE
+	if (Adjacent(A))
 		return TRUE
 	var/datum/dna/mob_dna = has_dna()
 	if(mob_dna?.check_mutation(/datum/mutation/human/telekinesis) && tkMaxRangeCheck(src, A))

BeeStation/BeeStation-Hornet

code/modules/mob/mob.dm
@@ -955,8 +955,12 @@
 	return 1
 
 ///Can the mob interact() with an atom?
-/mob/proc/can_interact_with(atom/A)
-	return IsAdminGhost(src) || Adjacent(A)
+/mob/proc/can_interact_with(atom/A, treat_mob_as_adjacent)
+	if(IsAdminGhost(src))
+		return TRUE
+	if(treat_mob_as_adjacent && src == A.loc)
+		return TRUE
+	return Adjacent(A)
 
 ///Can the mob use Topic to interact with machines
 /mob/proc/canUseTopic(atom/movable/M, be_close=FALSE, no_dextery=FALSE, no_tk=FALSE)