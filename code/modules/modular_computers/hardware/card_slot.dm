HolyTigerGod/Voidcrew

Updates tablet computer sechud logic (#57351)

code/modules/modular_computers/hardware/card_slot.dm
@@ -61,10 +61,12 @@
 	stored_card = I
 	to_chat(user, "<span class='notice'>You insert \the [I] into \the [expansion_hw ? "secondary":"primary"] [src].</span>")
 	playsound(src, 'sound/machines/terminal_insert_disc.ogg', 50, FALSE)
-	if(ishuman(user))
-		var/mob/living/carbon/human/H = user
-		H.sec_hud_set_ID()
 
+	var/holder_loc = holder.loc
+	if(ishuman(holder_loc))
+		var/mob/living/carbon/human/human_wearer = holder_loc
+		if(human_wearer.wear_id == holder)
+			human_wearer.sec_hud_set_ID()
 	holder.update_slot_icon()
 
 	return TRUE
@@ -91,9 +93,11 @@
 
 		holder.update_slot_icon()
 
-	if(ishuman(user))
-		var/mob/living/carbon/human/human_user = user
-		human_user.sec_hud_set_ID()
+	var/holder_loc = holder.loc
+	if(ishuman(holder_loc))
+		var/mob/living/carbon/human/human_wearer = holder_loc
+		if(human_wearer.wear_id == holder)
+			human_wearer.sec_hud_set_ID()
 
 	to_chat(user, "<span class='notice'>You remove the card from \the [src].</span>")
 	playsound(src, 'sound/machines/terminal_insert_disc.ogg', 50, FALSE)

BeeStation/BeeStation-Hornet

code/modules/modular_computers/hardware/card_slot.dm
@@ -92,8 +92,9 @@
 			var/datum/computer_file/program/computer_program = p
 			computer_program.event_idremoved(1)
 	if(ishuman(user))
-		var/mob/living/carbon/human/human_user = user
-		human_user.sec_hud_set_ID()
+		var/mob/living/carbon/human/human_wearer = user
+		if(human_wearer.wear_id == holder)
+			human_wearer.sec_hud_set_ID()
 	to_chat(user, "<span class='notice'>You remove the card from \the [src].</span>")
 	playsound(src, 'sound/machines/terminal_insert_disc.ogg', 50, FALSE)
 	stored_card = null