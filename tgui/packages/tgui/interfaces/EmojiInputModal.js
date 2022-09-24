c7b316c00d60ba4fb5921315d15fc11368b6b152
itsmeow <itsmeow@itsmeow.dev>
2022-09-24 17:19:41 -0500

    Mime emoji input improvements

--- tgui/packages/tgui/interfaces/EmojiInputModal.js
+++ tgui/packages/tgui/interfaces/EmojiInputModal.js
@@ -1,3 +1,5 @@
+
+import { KEY_BACKSPACE, KEY_ENTER, KEY_LEFT, KEY_RIGHT } from 'common/keycodes';
 import { useBackend, useLocalState } from '../backend';
 import { Box, Section, Button } from '../components';
 import { Window } from '../layouts';
@@ -22,6 +24,15 @@ export const EmojiInputModal = (_, context) => {
     ...arr.slice(0, index - 1),
     ...arr.slice(index),
   ];
+  const submit = () => act('submit', { entry: emojis.map(emoji => `:${emoji}:`).join(" ") });
+  const backspace = () => {
+    if (cursor_pos > 0 && cursor_pos <= emojis.length) {
+      setEmojis(remove(emojis, cursor_pos));
+      setCursorPos(cursor_pos - 1);
+    }
+  };
+  const left = () => setCursorPos(clamp(cursor_pos - 1, 0, emojis.length));
+  const right = () => setCursorPos(clamp(cursor_pos + 1, 0, emojis.length));
   return (
     <Window title={title} width={400} height={500}>
       <style>
@@ -30,70 +41,101 @@ export const EmojiInputModal = (_, context) => {
           50%  { opacity: 0; }
           100% { opacity: 1; }
         }
+        #cursor {
+          display: inline-block;
+          height: 40px;
+          width: 0px;
+          border-radius: 0;
+          border: 1px solid #aaaaaa;
+          animation: flickerAnimation 1s infinite;
+        }
         .EmojiButton {
+          width: 48px;
+          height: 48px;
           transition: outline linear 0.25s;
           outline: 1px solid transparent;
           outline-offset: -4px;
-          &:hover {
-            outline-color: white;
-            transition-duration: 0;
-          }
-        }`}
+        }
+        .EmojiButton:hover {
+          outline-color: white;
+          transition-duration: 0;
+        }
+        #emoji-input {
+          border: 1px solid white;
+          border-radius: 2px;
+          min-height: 54px;
+          transition: 1s linear border-color;
+        }
+        .emoji-contents {
+          width: 100%;
+          padding: 0;
+          max-height: 270px;
+          overflow-y: scroll;
+        }
+        `}
       </style>
-      <Window.Content scrollable>
+      <Window.Content
+        scrollable
+        onKeyDown={event => {
+          const keyCode = window.event ? event.which : event.code;
+          const e = window.event ? window.event : event;
+          switch (keyCode) {
+            case KEY_ENTER:
+              submit();
+              e.preventDefault();
+              break;
+            case KEY_BACKSPACE:
+              backspace();
+              e.preventDefault();
+              break;
+            case KEY_LEFT:
+              left();
+              e.preventDefault();
+              break;
+            case KEY_RIGHT:
+              right();
+              e.preventDefault();
+              break;
+          }
+        }}>
         <Section title="Entry">
           <Button
             icon="arrow-left"
-            onClick={() => {
-              setCursorPos(clamp(cursor_pos - 1, 0, emojis.length));
-            }} />
+            onClick={left} />
           <Button
             icon="arrow-right"
-            onClick={() => {
-              setCursorPos(clamp(cursor_pos + 1, 0, emojis.length));
-            }} />
+            onClick={right} />
           <Button
             color="red"
             content="Backspace"
-            onClick={() => {
-              if (cursor_pos > 0 && cursor_pos <= emojis.length) {
-                setEmojis(remove(emojis, cursor_pos));
-                setCursorPos(cursor_pos - 1);
-              }
-            }} />
-          <Box style={{ border: "1px solid white", "border-radius": "5px", "min-height": "50px" }}>
+            onClick={backspace} />
+          <div id="emoji-input">
             {insert(emojis.map(emoji => (<div
               key={emoji}
-              className={`emoji48x48 ${emoji}`} />)), cursor_pos, <div style={
-              { height: "40px",
-                width: "0px",
-                borderRadius: 0,
-                border: "1px solid #aaaaaa",
-                display: "inline-block",
-                animation: "flickerAnimation 1s infinite",
-              }
-            } />)}
-          </Box>
+              className={`emoji48x48 ${emoji}`} />)), cursor_pos, <div id="cursor" />)}
+          </div>
           <Button
             color="green"
             content="Submit"
-            onClick={() => act('submit', { entry: emojis.map(emoji => `:${emoji}:`).join(" ") })} />
+            onClick={submit} />
           <Button
             color="red"
             content="Cancel"
             onClick={() => act('cancel')} />
         </Section>
         <Section fitted title="Emojis">
-          {Object.keys(all_emojis).slice(1).map(emoji => (<div
-            key={emoji}
-            value={emoji}
-            className={`EmojiButton emoji48x48 ${emoji}`}
-            onClick={event => {
-              setEmojis(insert(emojis, cursor_pos,
-                event.target.value));
-              setCursorPos(cursor_pos + 1);
-            }}
-          />))}
+          <div className="emoji-contents">
+            {Object.keys(all_emojis).slice(1).map(emoji => (<Box
+              key={emoji}
+              value={emoji}
+              className={`EmojiButton emoji48x48 ${emoji}`}
+              onClick={event => {
+                setEmojis(insert(emojis, cursor_pos,
+                  event.target.value));
+                setCursorPos(cursor_pos + 1);
+              }}
+            />))}
+          </div>
         </Section>
       </Window.Content>
     </Window>
