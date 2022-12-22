48633a50e5fd20fa1460a0cec8bd3b9ab55a9c76
Bobbanz1 <59128051+Bobbanz1@users.noreply.github.com>
2022-12-22 10:36:22 +0100

    I have done things that make the code cry (#8230)

---
+++ tgui/packages/tgui/interfaces/NtosDatabank.tsx
@@ -0,0 +1,29 @@
+import { NtosWindow } from '../layouts';
+import { useBackend } from '../backend';
+import { Stack, Section } from '../components';
+
+type Data = {
+  src?: string
+};
+
+export const NtosDatabank = (props, context) => {
+  const { data } = useBackend<Data>(context);
+  const { src } = data;
+
+  return (
+    <NtosWindow width={600} height={800}>
+      <NtosWindow.Content>
+        <Stack fill vertical justify="space-between">
+          <Section fill>
+            <iframe
+              src={src}
+              height="100%"
+              width="100%"
+              frameBorder="0"
+            />
+          </Section>
+        </Stack>
+      </NtosWindow.Content>
+    </NtosWindow>
+  );
+};
