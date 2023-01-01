2cec61eb5d387e4233070c057ca679ca2801c2db
JobScholten <cooljob.js@gmail.com>
2023-01-01 21:03:11 +0100

    Imagine not being able to use a mix of tabs and spaces

--- tgui/packages/tgui/interfaces/NtosViroSymptoms.js
+++ tgui/packages/tgui/interfaces/NtosViroSymptoms.js
@@ -27,8 +27,8 @@ export const NtosViroSymptoms = (props, context) => {
           onChange={(e, value) => setSearchTerm(value)}
         />
         {filteredSymptoms.map(symptomName => (
-		<Section key={symptomName}>
-		  <Box fontWeight="bold">{symptomName}</Box>
+        <Section key={symptomName}>
+          <Box fontWeight="bold">{symptomName}</Box>
           <Box>{symptoms[symptomName]["desc"]}</Box>
           <Box>Stealth: {symptoms[symptomName]["stealth"]}</Box>
           <Box>Resistance: {symptoms[symptomName]["resistance"]}</Box>
@@ -36,17 +36,17 @@ export const NtosViroSymptoms = (props, context) => {
           <Box>Transmission: {symptoms[symptomName]["transmission"]}</Box>
           <Box>Level: {symptoms[symptomName]["level"]}</Box>
           <Box>Threshold Description:
-		    <>
-			  {symptoms[symptomName]["threshold_desc"]
-			    .split('<br>')
-			    .map((line, i) => (
-				<>
-				  {line.replace(/<b>/g, ' ')
-					.replace(/<\/b>/g, '')}
-					<br />
-				</>
-			  ))}
-			</>
+            <>
+              {symptoms[symptomName]["threshold_desc"]
+                .split('<br>')
+                .map((line, i) => (
+                  <>
+                    {line.replace(/<b>/g, ' ')
+                      .replace(/<\/b>/g, '')}
+                  <br />
+                  </>
+              ))}
+            </>
           </Box>
           <Box>Severity: {symptoms[symptomName]["severity"]}</Box>
         </Section>
