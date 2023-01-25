d8f3faf5e5d0df3c7715d5bf7b90aa98ce09056a
itsmeow <itsmeow@itsmeow.dev>
2023-01-25 16:09:07 -0600

    Add searching (tg/72824)

--- tgui/packages/tgui/interfaces/NtosMessenger.js
+++ tgui/packages/tgui/interfaces/NtosMessenger.js
@@ -1,5 +1,6 @@
-import { useBackend } from '../backend';
-import { Box, Button, Dimmer, Icon, Section, Stack } from '../components';
+import { useBackend, useLocalState } from '../backend';
+import { createSearch } from 'common/string';
+import { Box, Button, Dimmer, Icon, Section, Stack, Input } from '../components';
 import { NtosWindow } from '../layouts';
 
 export const NtosMessenger = (_, context) => {
@@ -120,120 +121,133 @@ const ContactsScreen = (props, context) => {
     virus_attach,
     sending_virus,
   } = data;
+  const [searchUser, setSearchUser] = useLocalState(context, 'searchUser', '');
+  const search = createSearch(
+    searchUser,
+    (messengers) => messengers.name + messengers.job
+  );
+  let users
+    = searchUser.length > 0 ? data.messengers.filter(search) : messengers;
   return (
-    <NtosWindow width={400} height={600}>
-      <NtosWindow.Content scrollable>
-        <Stack vertical>
-          <Section fill textAlign="center">
-            <Box bold>
-              <Icon name="address-card" mr={1} />
-              SpaceMessenger V8.5.3
-            </Box>
-            <Box italic opacity={0.3}>
-              Bringing you spy-proof communications since 2467.
-            </Box>
-          </Section>
-        </Stack>
-        <Stack vertical>
-          <Section fill textAlign="center">
-            <Box>
-              <Button
-                icon="bell"
-                content={ringer_status ? 'Ringer: On' : 'Ringer: Off'}
-                onClick={() => act('PDA_ringer_status')}
-              />
-              <Button
-                icon="address-card"
-                content={sending_and_receiving ? "Send / Receive: On" : "Send / Receive: Off"}
-                onClick={() => act('PDA_sAndR')}
-              />
-              <Button
-                icon="bell"
-                content="Set Ringtone"
-                onClick={() => act('PDA_ringSet')}
-              />
-              <Button
-                icon="comment"
-                content="View Messages"
-                onClick={() => act('PDA_viewMessages')}
-              />
-              <Button
-                icon="sort"
-                content={`Sort by: ${sortByJob ? "Job" : "Name"}`}
-                onClick={() => act('PDA_changeSortStyle')}
-              />
-              {!!isSilicon && (
-                <Button
-                  icon="camera"
-                  content="Attach Photo"
-                  onClick={() => act('PDA_selectPhoto')} />
-              )}
-              {!!virus_attach && (
-                <Button
-                  icon="bug"
-                  color={sending_virus ? "bad" : null}
-                  content={`Send Virus: ${sending_virus ? "Yes" : "No"}`}
-                  onClick={() => act('PDA_toggleVirus')}
-                />
-              )}
-            </Box>
-          </Section>
-        </Stack>
-        {!!photo && (
-          <Stack vertical mt={1}>
-            <Section fill textAlign="center">
-              <Icon name="camera" mr={1} />
-              Current Photo
-            </Section>
-            <Section align="center">
-              <Button
-                onClick={() => act('PDA_clearPhoto')}>
-                <Box
-                  mt={1}
-                  as="img"
-                  src={photo ? photo : null}
-                />
-              </Button>
-            </Section>
-          </Stack>
-        )}
-        <Stack vertical mt={1}>
-          <Section fill textAlign="center">
+    <>
+      <Stack vertical>
+        <Section fill textAlign="center">
+          <Box bold>
             <Icon name="address-card" mr={1} />
-            Detected Messengers
-          </Section>
-        </Stack>
-        <Stack vertical mt={1}>
-          <Section fill>
-            <Stack vertical>
-              {messengers.map(messenger => (
-                <Button
-                  key={messenger.ref}
-                  fluid
-                  onClick={() => act('PDA_sendMessage', {
-                    name: messenger.name,
-                    job: messenger.job,
-                    ref: messenger.ref,
-                  })}>
-                  {messenger.name} ({messenger.job})
-                </Button>
-              ))}
-            </Stack>
-            {!!canSpam && (
+            SpaceMessenger V8.5.3
+          </Box>
+          <Box italic opacity={0.3}>
+            Bringing you spy-proof communications since 2467.
+          </Box>
+        </Section>
+      </Stack>
+      <Stack vertical>
+        <Section fill textAlign="center">
+          <Box>
+            <Button
+              icon="bell"
+              content={ringer_status ? 'Ringer: On' : 'Ringer: Off'}
+              onClick={() => act('PDA_ringer_status')}
+            />
+            <Button
+              icon="address-card"
+              content={sending_and_receiving ? "Send / Receive: On" : "Send / Receive: Off"}
+              onClick={() => act('PDA_sAndR')}
+            />
+            <Button
+              icon="bell"
+              content="Set Ringtone"
+              onClick={() => act('PDA_ringSet')}
+            />
+            <Button
+              icon="comment"
+              content="View Messages"
+              onClick={() => act('PDA_viewMessages')}
+            />
+            <Button
+              icon="sort"
+              content={`Sort by: ${sortByJob ? "Job" : "Name"}`}
+              onClick={() => act('PDA_changeSortStyle')}
+            />
+            {!!isSilicon && (
               <Button
-                fluid
-                mt={1}
-                content="Send to all..."
-                onClick={() => act('PDA_sendEveryone')}
+                icon="camera"
+                content="Attach Photo"
+                onClick={() => act('PDA_selectPhoto')} />
+            )}
+            {!!virus_attach && (
+              <Button
+                icon="bug"
+                color={sending_virus ? "bad" : null}
+                content={`Send Virus: ${sending_virus ? "Yes" : "No"}`}
+                onClick={() => act('PDA_toggleVirus')}
               />
             )}
+          </Box>
+        </Section>
+      </Stack>
+      {!!photo && (
+        <Stack vertical mt={1}>
+          <Section fill textAlign="center">
+            <Icon name="camera" mr={1} />
+            Current Photo
+          </Section>
+          <Section align="center">
+            <Button
+              onClick={() => act('PDA_clearPhoto')}>
+              <Box
+                mt={1}
+                as="img"
+                src={photo ? photo : null}
+              />
+            </Button>
           </Section>
         </Stack>
-        {(!owner && !isSilicon) && (
-          <NoIDDimmer />
-        )}
-      </NtosWindow.Content>
-    </NtosWindow>
+      )}
+      <Stack vertical mt={1}>
+        <Section fill textAlign="center">
+          <Icon name="address-card" mr={1} />
+          Detected Messengers <br />
+          <Input
+            mt={1}
+            width="300px"
+            placeholder="Search by name or job"
+            value={searchUser}
+            onInput={(e, value) => setSearchUser(value)}
+          />
+        </Section>
+      </Stack>
+      <Stack vertical mt={1}>
+        <Section fill>
+          <Stack vertical>
+            {users.length === 0 && 'No users found'}
+            {users.map(messenger => (
+              <Button
+                key={messenger.ref}
+                fluid
+                onClick={() => act('PDA_sendMessage', {
+                  name: messenger.name,
+                  job: messenger.job,
+                  ref: messenger.ref,
+                })}>
+                {messenger.name} ({messenger.job})
+              </Button>
+            ))}
+          </Stack>
+          {!!canSpam && (
+            <Button
+              fluid
+              mt={1}
+              content="Send to all..."
+              onClick={() => act('PDA_sendEveryone')}
+            />
+          )}
+        </Section>
+      </Stack>
+      {(!owner && !isSilicon) && (
+        <NoIDDimmer />
+      )}
+    </>
   );
 };
 
