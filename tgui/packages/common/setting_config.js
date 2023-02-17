924c10675f7b66243a1686b68d7101c1693d8ae1
Evildragon <draco@evildragon.co.kr>
2023-02-17 10:43:03 +0900

    comment2

--- tgui/packages/common/setting_config.js
+++ tgui/packages/common/setting_config.js
@@ -8,8 +8,10 @@
  */
 
 export const PREF_ADDITION_KEY = 'beestation';
-// players will not have the same setting on different codebase as long as this key isn't the same to the key from other codebases
-// Currently, TG doesn't have this, so making it blank will let all players on Beestation codebase use TG setting (which is very default)
+// players will not have the same setting on different codebase
+// as long as this key isn't the same to the key from other codebases
+// Currently, TG doesn't have this, so making it blank will let
+// all players on Beestation codebase use TG setting (which is very default)
 // Key as blank means:
 //     PREF_ADDITION_KEY = '';
 
@@ -18,7 +20,8 @@ export const PREF_ADDITION_KEY = 'beestation';
 
    Things you might need to know:
       I don't think we would ever get a system that should share across all codebases,
-      but if that happens, Backend methods should have additional parameter which determines if it should refer a key to the backend
+      but if that happens, Backend methods should have additional parameter
+      which determines if it should refer a key to the backend
       For example:
       --------------------------------------
           set(key, value, ignore_config) {
@@ -26,7 +29,7 @@ export const PREF_ADDITION_KEY = 'beestation';
           }
       --------------------------------------
       ignore_config should be newly added to 9 methods that use key, and check if it's necessary
-      
+
    Why does this work?:
       Let's say you get chat logs.
       `store["chat_log"] = "some chat logs"` will be where it is stored.
@@ -35,8 +38,10 @@ export const PREF_ADDITION_KEY = 'beestation';
 
    Which additional effect will be broght?:
       Everything that uses backend storage system will be affected.
-      This is why I explained why the additional modification (ignore_config) might be needed in the future.
-      Currently, other minor stuff (i.e. TGUI windows geometry) is only affected by this, so there won't be any unwanted effect of the change.
+      This is why I explained why the additional modification (ignore_config) might be
+      needed in the future.
+      Currently, other minor stuff (i.e. TGUI windows geometry) is
+      only affected by this, so there won't be any unwanted effect of the change.
 */
 
 
