4015192188b15107557ac945aa31b4fc2c96efed
DrDuckedGoose <Hisphilosophy.comics@gmail.com>
2023-02-21 12:59:50 +1100

    Update Elevator.js

--- tgui/packages/tgui/interfaces/Elevator.js
+++ tgui/packages/tgui/interfaces/Elevator.js
@@ -6,25 +6,25 @@ export const Elevator = (props, context) => {
   const { act, data } = useBackend(context);
   const {
     current_z,
-    available_levels
+    available_levels,
   } = data;
   return (
     <Window        
-    width={240}
-    height={500}
-    theme="retro">
+      width={240}
+      height={500}
+      theme="retro">
       <Window.Content scrollable={1}>
-        <Stack direction="row" wrap="wrap" grow = {1} 
-        align = "center" 
-        spacing = {`0%`}>
+        <Stack direction="row" wrap="wrap" grow={1} 
+          align="center" 
+          spacing={`0%`}>
           {
             available_levels.map(level => (
-            <Stack.Item basis = {`100px`}>
-              <Button disabled = {level == current_z}
-              onClick={() => act(`${level}`)}
-              fontSize = {`50px`}
-              bold = {1}
-              m = {`5%`}>
+            <Stack.Item basis={`100px`}>
+              <Button disabled={level==current_z}
+                onClick={() => act(`${level}`)}
+                fontSize={`50px`}
+                bold={1}
+                m={`5%`}>
                 {level}
               </Button>
             </Stack.Item>))
