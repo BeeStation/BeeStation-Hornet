d0f7af05e71e3aaac790b8df0d5b515a1d25bc9f
DrDuckedGoose <Hisphilosophy.comics@gmail.com>
2023-02-21 12:54:04 +1100

    Update Elevator.js

--- tgui/packages/tgui/interfaces/Elevator.js
+++ tgui/packages/tgui/interfaces/Elevator.js
@@ -9,28 +9,28 @@ export const Elevator = (props, context) => {
     available_levels
   } = data;
   return (
-  <Window        
-  width={240}
-  height={500}
-  theme="retro">
-    <Window.Content scrollable={1}>
-      <Stack direction="row" wrap="wrap" grow = {1} 
-      align = "center" 
-      spacing = {`0%`}>
-        {
-          available_levels.map(level => (
-          <Stack.Item basis = {`100px`}>
-            <Button disabled = {level == current_z}
-            onClick={() => act(`${level}`)}
-            fontSize = {`50px`}
-            bold = {1}
-            m = {`5%`}>
-              {level}
-            </Button>
-          </Stack.Item>))
-        }
-      </Stack>
-    </Window.Content>
-  </Window>
+    <Window        
+    width={240}
+    height={500}
+    theme="retro">
+      <Window.Content scrollable={1}>
+        <Stack direction="row" wrap="wrap" grow = {1} 
+        align = "center" 
+        spacing = {`0%`}>
+          {
+            available_levels.map(level => (
+            <Stack.Item basis = {`100px`}>
+              <Button disabled = {level == current_z}
+              onClick={() => act(`${level}`)}
+              fontSize = {`50px`}
+              bold = {1}
+              m = {`5%`}>
+                {level}
+              </Button>
+            </Stack.Item>))
+          }
+        </Stack>
+      </Window.Content>
+    </Window>
   );
 };
