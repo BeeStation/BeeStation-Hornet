BeeStation/BeeStation-Hornet

commit a62e0cdba99986070f50d466edaf6490dff3d8a2

Overhauls/adjusts solid plasma ignition in many things (#8541)
* Initial commit

/atom/proc/plasma_ignition() added for use by all atoms made of plasma which can be burned

Applied to loose plasma sheets, plasma airlocks, plasma walls and false walls, plasma statues and plasma doors.

* Improves qdel logic

Particularly for things leaving debris behind

* Consistency in reactions

All affected objects explode when shot with lasers, exposed to flame, or attacked with a hot object.

* now includes cigarettes

* unnecessary src.

* Adjusts reagent plasma part 1

Also adds admin alerts to synthesis of Blackpowder and Phlogiston.

* Powdered plasma is now very volatile

Expanded on admin alerts and logging to include the last fingerprint if there is no user.

* update documentation

* plasma coin + minor tweaks

reduced cig power
adds a minimum threshold to plasma_ignite. Under threshold just makes a fire on the turf.

Adds fire sound to plasma ignition.

* now including rigged lights

* and power cells too now

They can still have explosions for reasons other than plasma though.

* unnecessary prior change

* some random whitespace I added

and it's gone now.

* updates explosion names

* Fixes the conflict caused by renaming files

* Remove redundant proc

* removes redundant moles checks

And also fixes an oversight in girder placement

* Capitalizes reaction names

Consistency is good.

* Update airlock_types.dm

code/game/atoms.dm
@@ -1596,3 +1596,42 @@
 /atom/proc/InitializeAIController()
 	if(ai_controller)
 		ai_controller = new ai_controller(src)
+
+/*
+* Called when something made out of plasma is exposed to high temperatures. 
+* Intended for use only with plasma that is ignited outside of some form of containment
+* Contained plasma ignitions (such as power cells or light fixtures) should explode with proper force
+*/
+/atom/proc/plasma_ignition(strength, mob/user)
+	var/turf/T = get_turf(src)
+	var/datum/gas_mixture/environment = T.return_air()
+	if(environment.get_moles(GAS_O2) >= PLASMA_MINIMUM_OXYGEN_NEEDED) //Flashpoint ignition can only occur with at least this much oxygen present
+		//no reason to alert admins or create an explosion if there's not enough power to actually make an explosion
+		if(strength > 1) 
+			if(user)
+				message_admins("[src] ignited by [ADMIN_LOOKUPFLW(user)] in [ADMIN_VERBOSEJMP(T)]")
+				log_game("[src] ignited by [key_name(user)] in [AREACOORD(T)]")
+			else
+				//if we can't get a direct source for ignition, we'll take the last person who touched what is blowing up
+				var/mob/toucher = get_mob_by_ckey(fingerprintslast)
+				if(toucher)
+					message_admins("[src] ignited in [ADMIN_VERBOSEJMP(T)], last touched by [ADMIN_LOOKUPFLW(toucher)]")
+					log_game("[src] ignited in [AREACOORD(T)], last touched by [key_name(toucher)]")
+				else
+					//Nobody directly touched the source of ignition or what ignited. Probably caused by burning atmos.
+					message_admins("[src] ignited by unidentified causes in [ADMIN_VERBOSEJMP(T)]")
+					log_game("[src] ignited by unidentified causes in [AREACOORD(T)]")
+			explosion(T, 0, 0, light_impact_range = strength/4, flash_range = strength/2, flame_range = strength, silent = TRUE)
+		else
+			new /obj/effect/hotspot(T)
+		//Regardless of power, whatever is burning will go up in a brilliant flash with at least a fizzle
+		playsound(T,'sound/magic/fireball.ogg', max(strength*20, 20), 1)
+		T.visible_message("<b><span class='userdanger'>[src] ignites in a brilliant flash!</span></b>") 
+		if(isturf(src))
+			var/turf/srcTurf = src
+			srcTurf.ScrapeAway() //Can't just qdel turfs
+		else
+			qdel(src)
+		return TRUE
+	return FALSE
+

code/modules/reagents/chemistry/recipes/pyrotechnics.dm
@@ -10,19 +10,8 @@
 /datum/chemical_reaction/reagent_explosion/proc/explode(datum/reagents/holder, created_volume)
 	var/power = modifier + round(created_volume/strengthdiv, 1)
 	if(power > 0)
+		reaction_alert_admins(holder)
 		var/turf/T = get_turf(holder.my_atom)
-		var/inside_msg
-		if(ismob(holder.my_atom))
-			var/mob/M = holder.my_atom
-			inside_msg = " inside [ADMIN_LOOKUPFLW(M)]"
-		var/lastkey = holder.my_atom?.fingerprintslast
-		var/touch_msg = "N/A"
-		if(lastkey)
-			var/mob/toucher = get_mob_by_ckey(lastkey)
-			touch_msg = "[ADMIN_LOOKUPFLW(toucher)]"
-		if(!istype(holder.my_atom, /obj/machinery/plumbing)) //excludes standard plumbing equipment from spamming admins with this shit
-			message_admins("Reagent explosion reaction occurred at [ADMIN_VERBOSEJMP(T)][inside_msg]. Last Fingerprint: [touch_msg].")
-		log_game("Reagent explosion reaction occurred at [AREACOORD(T)]. Last Fingerprint: [lastkey ? lastkey : "N/A"]." )
 		var/datum/effect_system/reagents_explosion/e = new()
 		if(istype(holder.my_atom, /obj/item/grenade/chem_grenade))
 			e.explosion_sizes = list(0, 1, 1, 1)
@@ -30,6 +19,20 @@
 		e.start()
 		holder.clear_reagents()
 
+/datum/chemical_reaction/proc/reaction_alert_admins(datum/reagents/holder)
+	var/turf/T = get_turf(holder.my_atom)
+	var/inside_msg
+	if(ismob(holder.my_atom))
+		var/mob/M = holder.my_atom
+		inside_msg = " inside [ADMIN_LOOKUPFLW(M)]"
+	var/lastkey = holder.my_atom?.fingerprintslast
+	var/touch_msg = "N/A"
+	if(lastkey)
+		var/mob/toucher = get_mob_by_ckey(lastkey)
+		touch_msg = "[ADMIN_LOOKUPFLW(toucher)]"
+	if(!istype(holder.my_atom, /obj/machinery/plumbing)) //excludes standard plumbing equipment from spamming admins with this shit
+		message_admins("[src] created at [ADMIN_VERBOSEJMP(T)][inside_msg]. Last Fingerprint: [touch_msg].")
+	log_game("[src] created at [AREACOORD(T)]. Last Fingerprint: [lastkey ? lastkey : "N/A"]." )
 
 /datum/chemical_reaction/reagent_explosion/nitroglycerin
 	name = "Nitroglycerin"
@@ -53,7 +56,7 @@
 
 
 /datum/chemical_reaction/reagent_explosion/potassium_explosion
-	name = "Explosion"
+	name = "Potassium Water explosion"
 	id = "potassium_explosion"
 	required_reagents = list(/datum/reagent/water = 1, /datum/reagent/potassium = 1)
 	strengthdiv = 10
@@ -84,14 +87,27 @@
 			C.adjust_fire_stacks(5)
 			C.IgniteMob()
 
+/datum/chemical_reaction/plasma
+	name = "Plasma Flash"
+	id = /datum/reagent/toxin/plasma
+	required_reagents = list(/datum/reagent/toxin/plasma = 1)
+	required_temp = 301 //extremely volatile
+
+/datum/chemical_reaction/plasma/on_reaction(datum/reagents/holder, created_volume)
+	holder.my_atom.plasma_ignition(created_volume/30)
+	holder.clear_reagents()
+
 /datum/chemical_reaction/blackpowder
 	name = "Black Powder"
 	id = /datum/reagent/blackpowder
 	results = list(/datum/reagent/blackpowder = 3)
 	required_reagents = list(/datum/reagent/saltpetre = 1, /datum/reagent/medicine/charcoal = 1, /datum/reagent/sulfur = 1)
 
+/datum/chemical_reaction/blackpowder/on_reaction(datum/reagents/holder, created_volume)
+	reaction_alert_admins(holder)
+
 /datum/chemical_reaction/reagent_explosion/blackpowder_explosion
-	name = "Black Powder Kaboom"
+	name = "Black Powder explosion"
 	id = "blackpowder_explosion"
 	required_reagents = list(/datum/reagent/blackpowder = 1)
 	required_temp = 474
@@ -165,7 +181,7 @@
 	holder.chem_temp = 1000 // hot as shit
 
 /datum/chemical_reaction/reagent_explosion/methsplosion
-	name = "Meth explosion"
+	name = "Strong meth explosion"
 	id = "methboom1"
 	required_temp = 380 //slightly above the meth mix time.
 	required_reagents = list(/datum/reagent/drug/methamphetamine = 1)
@@ -182,6 +198,7 @@
 	..()
 
 /datum/chemical_reaction/reagent_explosion/methsplosion/methboom2
+	name = "Weak meth explosion"
 	id = "methboom2"
 	required_reagents = list(/datum/reagent/diethylamine = 1, /datum/reagent/iodine = 1, /datum/reagent/phosphorus = 1, /datum/reagent/hydrogen = 1) //diethylamine is often left over from mixing the ephedrine.
 	required_temp = 300 //room temperature, chilling it even a little will prevent the explosion
@@ -201,7 +218,7 @@
 	goonchem_vortex(T, 1, range)
 
 /datum/chemical_reaction/sorium_vortex
-	name = "sorium_vortex"
+	name = "Sorium vortex"
 	id = "sorium_vortex"
 	required_reagents = list(/datum/reagent/sorium = 1)
 	required_temp = 474
@@ -301,7 +318,7 @@
 		holder.clear_reagents()
 
 /datum/chemical_reaction/smoke_powder_smoke
-	name = "smoke_powder_smoke"
+	name = "Smoke powder smoke"
 	id = "smoke_powder_smoke"
 	required_reagents = list(/datum/reagent/smoke_powder = 1)
 	required_temp = 374
@@ -335,7 +352,7 @@
 		C.soundbang_act(1, 100, rand(0, 5))
 
 /datum/chemical_reaction/sonic_powder_deafen
-	name = "sonic_powder_deafen"
+	name = "Sonic powder deafen"
 	id = "sonic_powder_deafen"
 	required_reagents = list(/datum/reagent/sonic_powder = 1)
 	required_temp = 374
@@ -347,12 +364,13 @@
 		C.soundbang_act(1, 100, rand(0, 5))
 
 /datum/chemical_reaction/phlogiston
-	name = /datum/reagent/phlogiston
+	name = "Phlogiston"
 	id = /datum/reagent/phlogiston
 	results = list(/datum/reagent/phlogiston = 3)
 	required_reagents = list(/datum/reagent/phosphorus = 1, /datum/reagent/toxin/acid = 1, /datum/reagent/stable_plasma = 1)
 
 /datum/chemical_reaction/phlogiston/on_reaction(datum/reagents/holder, created_volume)
+	reaction_alert_admins(holder)
 	if(holder.has_reagent(/datum/reagent/stabilizing_agent))
 		return
 	var/turf/open/T = get_turf(holder.my_atom)

code/game/machinery/doors/airlock_types.dm
@@ -223,14 +223,16 @@
 
 /obj/machinery/door/airlock/plasma/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	if(exposed_temperature > 300)
-		PlasmaBurn(exposed_temperature)
+		if(plasma_ignition(6))
+			PlasmaBurn()
 
-/obj/machinery/door/airlock/plasma/proc/ignite(exposed_temperature)
-	if(exposed_temperature > 300)
-		PlasmaBurn(exposed_temperature)
+/obj/machinery/door/airlock/plasma/bullet_act(obj/item/projectile/Proj)
+	if(!(Proj.nodamage) && Proj.damage_type == BURN)
+		if(plasma_ignition(6, Proj?.firer))
+			PlasmaBurn()
+	. = ..()
 
-/obj/machinery/door/airlock/plasma/proc/PlasmaBurn(temperature)
-	atmos_spawn_air("plasma=500;TEMP=1000")
+/obj/machinery/door/airlock/plasma/proc/PlasmaBurn()
 	var/obj/structure/door_assembly/DA
 	DA = new /obj/structure/door_assembly(loc)
 	if(glass)
@@ -239,16 +241,14 @@
 		DA.heat_proof_finished = TRUE
 	DA.update_icon()
 	DA.update_name()
-	qdel(src)
 
 /obj/machinery/door/airlock/plasma/BlockThermalConductivity() //we don't stop the heat~
 	return 0
 
 /obj/machinery/door/airlock/plasma/attackby(obj/item/C, mob/user, params)
 	if(C.is_hot() > 300)//If the temperature of the object is over 300, then ignite
-		message_admins("Plasma airlock ignited by [ADMIN_LOOKUPFLW(user)] in [ADMIN_VERBOSEJMP(src)]")
-		log_game("Plasma airlock ignited by [key_name(user)] in [AREACOORD(src)]")
-		ignite(C.is_hot())
+		if(plasma_ignition(6, user))
+			PlasmaBurn()
 	else
 		return ..()
 

code/game/objects/items/cigs_lighters.dm
@@ -202,16 +202,13 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 	hitsound = 'sound/items/welder.ogg'
 	damtype = "fire"
 	force = 4
+	var/turf/T = get_turf(src)
 	if(reagents.get_reagent_amount(/datum/reagent/toxin/plasma)) // the plasma explodes when exposed to fire
-		var/datum/effect_system/reagents_explosion/e = new()
-		e.set_up(round(reagents.get_reagent_amount(/datum/reagent/toxin/plasma) / 2.5, 1), get_turf(src), 0, 0)
-		e.start()
-		qdel(src)
+		plasma_ignition(reagents.get_reagent_amount(/datum/reagent/toxin/plasma)/10)
 		return
-	if(reagents.get_reagent_amount(/datum/reagent/fuel)) // the fuel explodes, too, but much less violently
-		var/datum/effect_system/reagents_explosion/e = new()
-		e.set_up(round(reagents.get_reagent_amount(/datum/reagent/fuel) / 5, 1), get_turf(src), 0, 0)
-		e.start()
+	if(reagents.get_reagent_amount(/datum/reagent/fuel)) // the fuel explodes too, but much less violently
+		T.visible_message("<b><span class='userdanger'>[src] violently explodes!</span></b>")
+		explosion(src, 0, 0, 1, 0, flame_range = 1)
 		qdel(src)
 		return
 	// allowing reagents to react after being lit
@@ -220,7 +217,6 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 	icon_state = icon_on
 	item_state = icon_on
 	if(flavor_text)
-		var/turf/T = get_turf(src)
 		T.visible_message(flavor_text)
 	START_PROCESSING(SSobj, src)
 

code/game/objects/items/devices/lightreplacer.dm
@@ -212,7 +212,7 @@
 			qdel(L2)
 
 			if(target.on && target.rigged)
-				target.explode()
+				target.plasma_ignition(4)
 			return
 
 		else

code/game/objects/items/stacks/sheets/mineral/materials.dm
@@ -97,16 +97,18 @@ Mineral Sheets
 
 /obj/item/stack/sheet/mineral/plasma/attackby(obj/item/W as obj, mob/user as mob, params)
 	if(W.is_hot() > 300)//If the temperature of the object is over 300, then ignite
-		var/turf/T = get_turf(src)
-		message_admins("Plasma sheets ignited by [ADMIN_LOOKUPFLW(user)] in [ADMIN_VERBOSEJMP(T)]")
-		log_game("Plasma sheets ignited by [key_name(user)] in [AREACOORD(T)]")
-		fire_act(W.is_hot())
+		plasma_ignition(amount/5, user)
 	else
 		return ..()
 
-/obj/item/stack/sheet/mineral/plasma/fire_act(exposed_temperature, exposed_volume)
-	atmos_spawn_air("plasma=[amount*10];TEMP=[exposed_temperature]")
-	qdel(src)
+/obj/item/stack/sheet/mineral/plasma/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
+	if(exposed_temperature > 300)
+		plasma_ignition(amount/5)
+
+/obj/item/stack/sheet/mineral/plasma/bullet_act(obj/item/projectile/Proj)
+	if(!(Proj.nodamage) && Proj.damage_type == BURN)
+		plasma_ignition(amount/5, Proj?.firer)
+	. = ..()
 
 /* Gold */
 

code/game/objects/structures/false_walls.dm
@@ -262,22 +262,23 @@
 
 /obj/structure/falsewall/plasma/attackby(obj/item/W, mob/user, params)
 	if(W.is_hot() > 300)
-		var/turf/T = get_turf(src)
-		message_admins("Plasma falsewall ignited by [ADMIN_LOOKUPFLW(user)] in [ADMIN_VERBOSEJMP(T)]")
-		log_game("Plasma falsewall ignited by [key_name(user)] in [AREACOORD(T)]")
-		burnbabyburn()
+		if(plasma_ignition(6, user))
+			new /obj/structure/girder/displaced(loc)
+			
 	else
 		return ..()
 
-/obj/structure/falsewall/plasma/proc/burnbabyburn(user)
-	playsound(src, 'sound/items/welder.ogg', 100, 1)
-	atmos_spawn_air("plasma=400;TEMP=1000")
-	new /obj/structure/girder/displaced(loc)
-	qdel(src)
-
 /obj/structure/falsewall/plasma/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	if(exposed_temperature > 300)
-		burnbabyburn()
+		if(plasma_ignition(6))
+			new /obj/structure/girder/displaced(loc)
+			
+
+/obj/structure/falsewall/plasma/bullet_act(obj/item/projectile/Proj)
+	if(!(Proj.nodamage) && Proj.damage_type == BURN)
+		if(plasma_ignition(6, Proj?.firer))
+			new /obj/structure/girder/displaced(loc)
+	. = ..()
 
 /obj/structure/falsewall/bananium
 	name = "bananium wall"

code/game/objects/structures/mineral_doors.dm
@@ -252,21 +252,19 @@
 	return
 
 /obj/structure/mineral_door/transparent/plasma/attackby(obj/item/W, mob/user, params)
-	if(W.is_hot())
-		var/turf/T = get_turf(src)
-		message_admins("Plasma mineral door ignited by [ADMIN_LOOKUPFLW(user)] in [ADMIN_VERBOSEJMP(T)]")
-		log_game("Plasma mineral door ignited by [key_name(user)] in [AREACOORD(T)]")
-		TemperatureAct()
+	if(W.is_hot() > 300)
+		plasma_ignition(6, user)
 	else
 		return ..()
 
 /obj/structure/mineral_door/transparent/plasma/temperature_expose(datum/gas_mixture/air, exposed_temperature, exposed_volume)
 	if(exposed_temperature > 300)
-		TemperatureAct()
+		plasma_ignition(6)
 
-/obj/structure/mineral_door/transparent/plasma/proc/TemperatureAct()
-	atmos_spawn_air("plasma=500;TEMP=1000")
-	deconstruct(FALSE)
+/obj/structure/mineral_door/transparent/plasma/bullet_act(obj/item/projectile/Proj)
+	if(!(Proj.nodamage) && Proj.damage_type == BURN)
+		plasma_ignition(6, Proj?.firer)
+	. = ..()
 
 /obj/structure/mineral_door/transparent/diamond
 	name = "diamond door"

commit 0cf1c9d1bc7c192ea1ea602a2a0e567f54fd1331

Fixes gamebreaking oversight introduced in #8541 (#8693)
* fixes plasma reactions

* better fix

* Update code/game/atoms.dm

---------

Co-authored-by: ike709 <ike709@users.noreply.github.com>

code/game/atoms.dm
@@ -1602,7 +1602,7 @@
 * Intended for use only with plasma that is ignited outside of some form of containment
 * Contained plasma ignitions (such as power cells or light fixtures) should explode with proper force
 */
-/atom/proc/plasma_ignition(strength, mob/user)
+/atom/proc/plasma_ignition(strength, mob/user, reagent_reaction)
 	var/turf/T = get_turf(src)
 	var/datum/gas_mixture/environment = T.return_air()
 	if(environment.get_moles(GAS_O2) >= PLASMA_MINIMUM_OXYGEN_NEEDED) //Flashpoint ignition can only occur with at least this much oxygen present
@@ -1627,7 +1627,9 @@
 		//Regardless of power, whatever is burning will go up in a brilliant flash with at least a fizzle
 		playsound(T,'sound/magic/fireball.ogg', max(strength*20, 20), 1)
 		T.visible_message("<b><span class='userdanger'>[src] ignites in a brilliant flash!</span></b>") 
-		if(isturf(src))
+		if(reagent_reaction) // Don't qdel(src). It's a reaction inside of something (or someone) important.
+			return TRUE
+		else if(isturf(src))
 			var/turf/srcTurf = src
 			srcTurf.ScrapeAway() //Can't just qdel turfs
 		else

code/modules/reagents/chemistry/recipes/pyrotechnics.dm
@@ -91,10 +91,10 @@
 	name = "Plasma Flash"
 	id = /datum/reagent/toxin/plasma
 	required_reagents = list(/datum/reagent/toxin/plasma = 1)
-	required_temp = 301 //extremely volatile
+	required_temp = 320 //extremely volatile
 
 /datum/chemical_reaction/plasma/on_reaction(datum/reagents/holder, created_volume)
-	holder.my_atom.plasma_ignition(created_volume/30)
+	holder.my_atom.plasma_ignition(created_volume/30, reagent_reaction = TRUE)
 	holder.clear_reagents()
 
 /datum/chemical_reaction/blackpowder