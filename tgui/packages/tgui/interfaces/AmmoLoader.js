4e8cc28fc8532266f87d927e7c10105e52c083c7
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2023-03-25 19:56:54 +0000

    Fixes ammo loader not being able to eject and also bluescreening

--- tgui/packages/tgui/interfaces/AmmoLoader.js
+++ tgui/packages/tgui/interfaces/AmmoLoader.js
@@ -1,8 +1,9 @@
+import { useBackend } from 'tgui/backend';
 import { Button, Flex } from '../components';
 import { Window } from '../layouts';
 
 export const AmmoLoader = (props, context) => {
-  const { act, data } = context;
+  const { act, data } = useBackend(context);
   const {
     loaded = [],
   } = data;
@@ -11,12 +12,12 @@ export const AmmoLoader = (props, context) => {
       width={380}
       height={240}>
       <Window.Content>
-        <Flex direction="row" overflowY="scroll">
+        <Flex direction="column" overflowY="scroll">
           {loaded.map(element => (
             <Flex.Item key={element.id}>
-              <Flex direction="width">
-                <Flex.Item grow={1}>element.name</Flex.Item>
-                <Flex.Item>element.count</Flex.Item>
+              <Flex direction="row">
+                <Flex.Item grow={1}>{element.name}</Flex.Item>
+                <Flex.Item mr="20px">{element.count}</Flex.Item>
                 <Flex.Item>
                   <Button
                     content="Eject"
