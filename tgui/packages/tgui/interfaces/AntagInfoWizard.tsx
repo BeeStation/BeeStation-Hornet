176ab6c4d8b4cca9354f206c802334bd867d01d1
Lucy <lucy@absolucy.moe>
2023-06-09 01:23:13 -0400

    Reformat AntagInfoWizard.

--- tgui/packages/tgui/interfaces/AntagInfoWizard.tsx
+++ tgui/packages/tgui/interfaces/AntagInfoWizard.tsx
@@ -90,6 +90,55 @@ const ObjectivesSection = (_props, context) => {
   );
 };
 
+const SpellbookSection = (_props, _context) => {
+  return (
+    <Section fill title="Spellbook">
+      <Stack vertical fill>
+        <Stack.Item>
+          You have a spellbook which is bound to you. You can use it to choose a magical arsenal.
+          <br />
+          <span style={destructionstyle}>The deadly page has the offensive spells, to destroy your enemies.</span>
+          <br />
+          <span style={defensestyle}>
+            The defensive page has defensive spells, to keep yourself alive. Remember, you may be powerful, but you are still
+            only human.
+          </span>
+          <br />
+          <span style={transportstyle}>
+            The transport page has mobility spells, very important aspect of staying alive and getting things done.
+          </span>
+          <br />
+          <span style={summonstyle}>
+            The summoning page has summoning and other helpful spells for not fighting alone. Careful, not every summon is on
+            your side.
+          </span>
+          <br />
+          <span style={ritualstyle}>
+            The rituals page has powerful global effects, that will pit the station against itself. Do mind that these are
+            either expensive, or just for panache.
+          </span>
+        </Stack.Item>
+      </Stack>
+    </Section>
+  );
+};
+
+const MiscGearSection = (_props, _context) => {
+  return (
+    <Section title="Misc Gear">
+      <Stack>
+        <Stack.Item>
+          <span style={teleportstyle}>Teleport scroll:</span> 4 uses to teleport wherever you want. You will not be able to come
+          back to the den, so be sure you have everything ready before departing.
+          <br />
+          <span style={robestyle}>Wizard robes:</span> Used to cast most spells. Your spellbook will let you know which spells
+          cannot be cast without a garb.
+        </Stack.Item>
+      </Stack>
+    </Section>
+  );
+};
+
 export const AntagInfoWizard = (_props, _context) => {
   return (
     <Window width={620} height={620} theme="wizard">
@@ -102,47 +151,10 @@ export const AntagInfoWizard = (_props, _context) => {
             <ObjectivesSection />
           </Stack.Item>
           <Stack.Item>
-            <Section fill title="Spellbook">
-              <Stack vertical fill>
-                <Stack.Item>
-                  You have a spellbook which is bound to you. You can use it to choose a magical arsenal.
-                  <br />
-                  <span style={destructionstyle}>The deadly page has the offensive spells, to destroy your enemies.</span>
-                  <br />
-                  <span style={defensestyle}>
-                    The defensive page has defensive spells, to keep yourself alive. Remember, you may be powerful, but you are
-                    still only human.
-                  </span>
-                  <br />
-                  <span style={transportstyle}>
-                    The transport page has mobility spells, very important aspect of staying alive and getting things done.
-                  </span>
-                  <br />
-                  <span style={summonstyle}>
-                    The summoning page has summoning and other helpful spells for not fighting alone. Careful, not every summon
-                    is on your side.
-                  </span>
-                  <br />
-                  <span style={ritualstyle}>
-                    The rituals page has powerful global effects, that will pit the station against itself. Do mind that these
-                    are either expensive, or just for panache.
-                  </span>
-                </Stack.Item>
-              </Stack>
-            </Section>
+            <SpellbookSection />
           </Stack.Item>
           <Stack.Item>
-            <Section title="Misc Gear">
-              <Stack>
-                <Stack.Item>
-                  <span style={teleportstyle}>Teleport scroll:</span> 4 uses to teleport wherever you want. You will not be able
-                  to come back to the den, so be sure you have everything ready before departing.
-                  <br />
-                  <span style={robestyle}>Wizard robes:</span> Used to cast most spells. Your spellbook will let you know which
-                  spells cannot be cast without a garb.
-                </Stack.Item>
-              </Stack>
-            </Section>
+            <MiscGearSection />
           </Stack.Item>
           <Stack.Item>
             <Section textAlign="center" textColor="red" fontSize="20px">
@@ -152,23 +164,5 @@ export const AntagInfoWizard = (_props, _context) => {
         </Stack>
       </Window.Content>
     </Window>
-  );
-};
-
-const ObjectivePrintout = (props, context) => {
-  const { data } = useBackend<Info>(context);
-  const { objectives } = data;
-  return (
-    <Stack vertical>
-      <Stack.Item bold>The Space Wizards Federation has given you the following tasks:</Stack.Item>
-      <Stack.Item>
-        {(!objectives && 'None!') ||
-          objectives.map((objective) => (
-            <Stack.Item key={objective.count}>
-              #{objective.count}: {objective.explanation}
-            </Stack.Item>
-          ))}
-      </Stack.Item>
-    </Stack>
   );
 };
