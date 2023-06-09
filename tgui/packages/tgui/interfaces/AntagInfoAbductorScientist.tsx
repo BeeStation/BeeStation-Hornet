642e65ec7dd831f01bc39f70828c80c3e006d73d
Lucy <lucy@absolucy.moe>
2023-06-09 13:36:16 -0400

    Work on scientist info.

--- tgui/packages/tgui/interfaces/AntagInfoAbductorScientist.tsx
+++ tgui/packages/tgui/interfaces/AntagInfoAbductorScientist.tsx
@@ -30,8 +30,8 @@ const IntroSection = (_props, context) => {
           <Box inline textColor="purple">
             Abductor
           </Box>{' '}
-          <Box inline textColor="red">
-            Agent
+          <Box inline textColor="blue">
+            Scientist
           </Box>{' '}
           of{' '}
           <Box inline textColor="purple">
@@ -49,19 +49,84 @@ const BasicLoreSection = (_props, _context) => {
     <Section>
       <BlockQuote>
         You are the{' '}
-        <Box inline textColor="red">
-          agent
-        </Box>{' '}
-        of this abductor team!
-        <br />
-        You&apos;re the <b>brawn</b> of the abductors, it is your job to work with your{' '}
         <Box inline textColor="blue">
           scientist
         </Box>{' '}
-        to capture test subjects and bring them back to your ship!
+        of this abductor team!
+        <br />
+        You&apos;re the <b>brains</b> of the abductors, it is your job to work with your{' '}
+        <Box inline textColor="red">
+          agent
+        </Box>{' '}
+        to capture test subjects and bring them back to your ship for experimentation!
         <br />
         As an abductor, you have a telepathic link with your partner, and have no method of verbal communication.
       </BlockQuote>
+    </Section>
+  );
+};
+
+const SurgerySubsection = (_props, _context) => {
+  return (
+    <Section name="Experimentation">
+      Whenever you have successfully abducted a target to your mothership, you experiment on them with an{' '}
+      <Box inline textColor="purple">
+        experimental organ replacement
+      </Box>{' '}
+      surgery. This surgery does not require you to strip the target, and consists of the following steps:
+      <br />
+      <b>1.</b>: Targeting their chest, click on the subject with{' '}
+      <Box inline textColor="blue">
+        surgical drapes
+      </Box>{' '}
+      and select{' '}
+      <Box inline textColor="purple">
+        experimental organ manipulation
+      </Box>
+      .
+      <br />
+      <b>2.</b>: Make an incision in the subject using a{' '}
+      <Box inline textColor="blue">
+        scalpel
+      </Box>
+      .
+      <br />
+      <b>3.</b>: Clamp bleeders into the subject using a{' '}
+      <Box inline textColor="blue">
+        hemostat
+      </Box>
+      .
+      <br />
+      <b>4.</b>: Retract the subject&apos;s skin using{' '}
+      <Box inline textColor="blue">
+        retractors
+      </Box>
+      .
+      <br />
+      <b>5.</b>: Make another incision in the subject using a{' '}
+      <Box inline textColor="blue">
+        scalpel
+      </Box>
+      .
+      <br />
+      <b>6.</b>: Extract the subject&apos; organs using an{' '}
+      <Box inline textColor="blue">
+        empty hand
+      </Box>
+      .
+      <br />
+      <b>7.</b>: Insert an experimental{' '}
+      <Box inline textColor="blue">
+        alien organ
+      </Box>{' '}
+      into the subject.
+      <br />
+      <b>8.</b>: Move the target into the{' '}
+      <Box inline textColor="purple">
+        experimentation machine
+      </Box>{' '}
+      via drag-clicking, ensuring they are unbuckled from your operating table. Select either Probe, Analyze, or Dissect once
+      they are in there.
     </Section>
   );
 };
@@ -76,41 +141,25 @@ const EquipmentSection = (_props, _context) => {
               <Box
                 inline
                 as="img"
-                src={resolveAsset('abaton.png')}
+                src={resolveAsset('scitool.png')}
                 width="32px"
                 style={{ '-ms-interpolation-mode': 'nearest-neighbor' }}
               />
               Your{' '}
               <Box inline textColor="purple">
-                advanced baton
+                science tool
               </Box>{' '}
-              is a multi-purpose baton that does just about everything you need to subdue someone. It has four modes:
-              <br />
-              <Box inline textColor="yellow">
-                Stun
-              </Box>
-              : Stuns whoever you hit it with.
+              is essential for abducting test subjects for experimentation! It has two modes:
               <br />
               <Box inline textColor="blue">
-                Sleep
+                Mark
               </Box>
-              : Knocks someone out, as long as they&apos;re stunned. Only one person can be asleep at a time, however!
-              <br />
+              : Marks a subject, allowing them to be beamed up by your camera console. You can mark your agent instantly from
+              any range (even from the cameras), however test subjects require you to beam down to them and mark them yourself!
               <Box inline textColor="red">
-                Cuff
+                Scan
               </Box>
-              : Cuffs someone with hard-light energy cuffs.
-              <br />
-              <Box inline textColor="purple">
-                Probe
-              </Box>
-              : Probes whether an organism is suitable for experimentation or not.
-              <br />
-              People wearing{' '}
-              <Box inline textColor="label">
-                tinfoil hats
-              </Box>{' '}
-              are immune to the baton&apos;s effects, however.
+              : Scans someone, adding their appearance to the potential disguises for your agent. This works from the cameras.
             </Stack.Item>
             <Stack.Divider />
             <Stack.Item>
@@ -123,60 +172,34 @@ const EquipmentSection = (_props, _context) => {
               />
               You have{' '}
               <Box inline textColor="purple">
-                alien tools
+                alien surgical tools
               </Box>{' '}
               , which are capable of working extremely fast, faster than any tool available to lesser lifeforms.
               <br />
-              You can use these to{' '}
-              <Box inline textColor="yellow">
-                hack machinery
+              These are essential to performing experimental surgeries on abducted test subjects!
+              <br />
+              In addition, you are capable of doing other high-tech surgeries, such as{' '}
+              <Box inline textColor="purple">
+                brainwashing
               </Box>{' '}
               or{' '}
-              <Box inline textColor="orange">
-                weld doors shut
+              <Box inline textColor="green">
+                max-tier wound tending
               </Box>
-              , for example.
+              .
             </Stack.Item>
           </Stack>
         </Stack.Item>
         <Stack.Divider />
         <Stack.Item>
-          <Stack.Item>
-            <Box
-              inline
-              as="img"
-              src={resolveAsset('apistol.png')}
-              width="32px"
-              style={{ '-ms-interpolation-mode': 'nearest-neighbor' }}
-            />
-            Your{' '}
-            <Box inline textColor="purple">
-              alien pistol
-            </Box>{' '}
-            is an effective self-defense weapon, with three different fire modes:
-            <br />
-            <Box inline textColor="green">
-              Declone
-            </Box>
-            : Deals 20 cellular damage to whatever it hits.
-            <br />
-            <Box inline textColor="blue">
-              Ion
-            </Box>
-            : EMPs whatever it hits.
-            <br />
-            <Box inline textColor="teal">
-              Freeze
-            </Box>
-            : Lowers the temperature of whatever it hits.
-          </Stack.Item>
+          <SurgerySubsection />
         </Stack.Item>
       </Stack>
     </Section>
   );
 };
 
-export const AntagInfoAbductorAgent = (_props, context) => {
+export const AntagInfoAbductorScientist = (_props, context) => {
   const { data } = useBackend<Info>(context);
   const { objectives } = data;
   return (
