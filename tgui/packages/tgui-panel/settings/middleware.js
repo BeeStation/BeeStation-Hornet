c7cecd927202c1f9bac8cd906b70d918a06b7eb6
Lucy <lucy@absolucy.moe>
2023-06-17 11:38:40 -0400

    Port tgui-panel chat and settings updates from /tg/

--- tgui/packages/tgui-panel/settings/middleware.js
+++ tgui/packages/tgui-panel/settings/middleware.js
@@ -5,9 +5,8 @@
  */
 
 import { storage } from 'common/storage';
-import { chatMiddleware } from '../chat';
 import { setClientTheme } from '../themes';
-import { loadSettings, updateSettings } from './actions';
+import { loadSettings, updateSettings, addHighlightSetting, removeHighlightSetting, updateHighlightSetting } from './actions';
 import { selectSettings } from './selectors';
 import { FONTS_DISABLED } from './constants';
 
@@ -33,22 +32,23 @@ export const settingsMiddleware = (store) => {
         store.dispatch(loadSettings(settings));
       });
     }
-    if (type === updateSettings.type || type === loadSettings.type) {
+    if (
+      type === updateSettings.type ||
+      type === loadSettings.type ||
+      type === addHighlightSetting.type ||
+      type === removeHighlightSetting.type ||
+      type === updateHighlightSetting.type
+    ) {
       // Set client theme
       const theme = payload?.theme;
       if (theme) {
         setClientTheme(theme);
-      } else if (type === loadSettings.type) {
-        updateSettings({
-          theme: 'dark',
-        });
       }
       // Pass action to get an updated state
       next(action);
       const settings = selectSettings(store.getState());
       // Update global UI font size
       setGlobalFontSize(settings.fontSize);
-      // Update global UI font family
       setGlobalFontFamily(settings.fontFamily);
       // Save settings to the web storage
       storage.set('panel-settings', settings);
