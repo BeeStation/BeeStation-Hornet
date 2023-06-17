bb0889b966e49a35bf431f3efe452e0697fadfd0
itsmeow <itsmeow@itsmeow.dev>
2023-06-17 19:23:51 -0400

    Remove the last of legacy toggles for good, add deadmin forced preview

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/deadmin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/deadmin.tsx
@@ -1,36 +1,63 @@
-import { FeatureToggle, CheckboxInput } from '../base';
+import { BooleanLike } from 'common/react';
+import { Button } from '../../../../../components';
+import { Feature, FeatureValueProps } from '../base';
 
-export const deadmin_always: FeatureToggle = {
+type FeatureToggleDeadminServerData = {
+  forced: BooleanLike;
+};
+
+type FeatureToggleDeadmin = Feature<BooleanLike, boolean, FeatureToggleDeadminServerData>;
+
+const DeadminCheckboxInput = (props: FeatureValueProps<BooleanLike, boolean, FeatureToggleDeadminServerData>) => {
+  const forced = props.serverData?.forced;
+  return (
+    <Button
+      color="transparent"
+      style={forced ? { 'background-color': '#cc0000' } : null}
+      tooltip={forced ? 'Forced by server config' : null}
+      tooltipPosition="right"
+      icon={forced ? 'minus-square-o' : props.value ? 'check-square-o' : 'square-o'}
+      selected={!!forced || !!props.value}
+      onClick={() => {
+        if (!forced) {
+          props.handleSetValue(!props.value);
+        }
+      }}
+    />
+  );
+};
+
+export const deadmin_always: FeatureToggleDeadmin = {
   name: 'Always Deadmin',
   category: 'ADMIN',
   description: 'Whether you will always deadmin when joining a round.',
-  component: CheckboxInput,
+  component: DeadminCheckboxInput,
 };
 
-export const deadmin_antagonist: FeatureToggle = {
+export const deadmin_antagonist: FeatureToggleDeadmin = {
   name: 'Deadmin As Antagonist',
   category: 'ADMIN',
   description: 'Whether you will always deadmin when joining a round as an antagonist.',
-  component: CheckboxInput,
+  component: DeadminCheckboxInput,
 };
 
-export const deadmin_position_head: FeatureToggle = {
+export const deadmin_position_head: FeatureToggleDeadmin = {
   name: 'Deadmin As Head of Staff',
   category: 'ADMIN',
   description: 'Whether you will always deadmin when joining a round as a head of staff.',
-  component: CheckboxInput,
+  component: DeadminCheckboxInput,
 };
 
-export const deadmin_position_security: FeatureToggle = {
+export const deadmin_position_security: FeatureToggleDeadmin = {
   name: 'Deadmin As Security',
   category: 'ADMIN',
   description: 'Whether you will always deadmin when joining a round as security.',
-  component: CheckboxInput,
+  component: DeadminCheckboxInput,
 };
 
-export const deadmin_position_silicon: FeatureToggle = {
+export const deadmin_position_silicon: FeatureToggleDeadmin = {
   name: 'Deadmin As Silicon',
   category: 'ADMIN',
   description: 'Whether you will always deadmin when joining a round as a silicon.',
-  component: CheckboxInput,
+  component: DeadminCheckboxInput,
 };
