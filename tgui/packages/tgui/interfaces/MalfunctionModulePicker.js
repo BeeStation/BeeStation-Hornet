4c0a0c73fa0e5e41ef5d4131ef7004e2083e31ea
RKz <falldownftw@gmail.com>
2023-07-07 00:25:18 -0400

    prettierx fix?

--- tgui/packages/tgui/interfaces/MalfunctionModulePicker.js
+++ tgui/packages/tgui/interfaces/MalfunctionModulePicker.js
@@ -4,14 +4,9 @@ import { GenericUplink } from './Uplink';
 
 export const MalfunctionModulePicker = (props, context) => {
   const { act, data } = useBackend(context);
-  const {
-    processingTime,
-  } = data;
+  const { processingTime } = data;
   return (
-    <Window
-      width={620}
-      height={525}
-      theme="malfunction">
+    <Window width={620} height={525} theme="malfunction">
       <Window.Content scrollable>
         <GenericUplink
           currencyAmount={processingTime}
