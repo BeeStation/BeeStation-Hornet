tgstation/tgstation

It turns out bodypart reloading relied entirely on the regenerate_icons call that eyes did when they were inserted/removed. Easy fix though, just really funny

code/modules/client/preferences.dm
@@ -487,6 +487,7 @@ INITIALIZE_IMMEDIATE(/atom/movable/screen/character_preview_view)
 	character.dna.real_name = character.real_name
 
 	if(icon_updates)
+		character.icon_render_key = null //turns out if you don't set this to null update_body_parts does nothing, since it assumes the operation was cached
 		character.update_body()
 		character.update_hair()
 		character.update_body_parts()

BeeStation/BeeStation-Hornet

code/modules/client/preferences/preferences.dm
@@ -383,6 +383,7 @@ GLOBAL_LIST_EMPTY(preferences_datums)
 	character.dna.real_name = character.real_name
 
 	if(icon_updates)
+		character.icon_render_keys = null // turns out if you don't set this to null update_body_parts does nothing, since it assumes the operation was cached
 		character.update_body()
 		character.update_hair()
 		character.update_body_parts()