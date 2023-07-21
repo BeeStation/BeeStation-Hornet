HolyTigerGod/Voidcrew

Macro optimizes SSmapping saving 50%  (#69632)
* 'optimizes' space transitions by like 0.06 seconds, makes them easier to read tho, so that's an upside

* ''''optimizes'''' parsed map loading

I'm honestly not sure how big a difference this makes, looked like small
percentage points if anything
It's a bit more internally concistent at least, which is nice. Also I
understand the system now.

I'd like to think it helped but I think this is kinda a "do you think
it's easier to read" sort of situation. if it did help it was by the
skin of its teeth

* Saves 0.6 seconds off loading meta and lavaland's map files

This is just a lot of micro stuff.
1: Bound checks don't need to be inside for loops, we can instead bound the iteration counts
2: TGM and DMM are parsed differently. in dmm a grid_set is one z level,
   in tgm it's one collumn. Realizing this allows you to skip copytexts and
   other such silly in the tgm implemenentation, saving a good bit of time
3: Min/max bounds do not need to be checked inside for loops, and can
   instead be handled outside of them, because we know the order of x
   and y iteration. This saves 0.2 seconds

I may or may not have made the code harder to read, if so let me know
and I'll check it over.

* Micro ops key caching significantly. Fixes macros bug

inserting \ into a dmm with no valid target would just less then loop
the string. Dumb

Anyway, optimizations. I save a LOT of time by not needing to call
find_next_delimiter_position for every entry and var set. (like maybe 0.5
seconds, not totally sure)
I save this by using splittext, which is significantly faster. this
would cause parsing issues if you could embed \n into dmms, but you
can't, so I'm safe.

Lemme see uh, lots of little things, stuff that's suboptimal or could be
done cheaper. Some "hey you and I both know a \" is 2 chars long sort of
stuff

I removed trim_text because the quote trimming was never actually used,
and the space t

code/game/turfs/open/river.dm
@@ -8,6 +8,7 @@
 	var/list/river_nodes = list()
 	var/num_spawned = 0
 	var/list/possible_locs = block(locate(min_x, min_y, target_z), locate(max_x, max_y, target_z))
+	new_baseturfs = baseturfs_string_list(new_baseturfs, pick(possible_locs))
 	while(num_spawned < nodes && possible_locs.len)
 		var/turf/T = pick(possible_locs)
 		var/area/A = get_area(T)
@@ -23,8 +24,11 @@
 		if (W.z != target_z || W.connected)
 			continue
 		W.connected = TRUE
+		// Workaround around ChangeTurf that's safe because of when this proc is called
 		var/turf/cur_turf = get_turf(W)
-		cur_turf.ChangeTurf(turf_type, new_baseturfs, CHANGETURF_IGNORE_AIR)
+		cur_turf = new turf_type(cur_turf)
+		if(new_baseturfs)
+			cur_turf.baseturfs = new_baseturfs
 		var/turf/target_turf = get_turf(pick(river_nodes - W))
 		if(!target_turf)
 			break
@@ -53,7 +57,10 @@
 				cur_turf = get_step(cur_turf, cur_dir)
 				continue
 			else
-				var/turf/river_turf = cur_turf.ChangeTurf(turf_type, new_baseturfs, CHANGETURF_IGNORE_AIR)
+				// Workaround around ChangeTurf that's safe because of when this proc is called
+				var/turf/river_turf = new turf_type(cur_turf)
+				if(new_baseturfs)
+					river_turf.baseturfs = new_baseturfs
 				river_turf.Spread(25, 11, whitelist_area)
 
 	for(var/WP in river_nodes)
@@ -72,35 +79,42 @@
 	var/list/cardinal_turfs = list()
 	var/list/diagonal_turfs = list()
 	var/logged_turf_type
-	for(var/F in RANGE_TURFS(1, src) - src)
-		var/turf/T = F
-		var/area/new_area = get_area(T)
-		if(!T || (T.density && !ismineralturf(T)) || isindestructiblefloor(T) || (whitelisted_area && !istype(new_area, whitelisted_area)) || (T.turf_flags & NO_LAVA_GEN) )
+	for(var/turf/canidate as anything in RANGE_TURFS(1, src) - src)
+		if(!canidate || (canidate.density && !ismineralturf(canidate)) || isindestructiblefloor(canidate))
 			continue
 
-		if(!logged_turf_type && ismineralturf(T))
-			var/turf/closed/mineral/M = T
-			logged_turf_type = M.turf_type
+		var/area/new_area = get_area(canidate)
+		if((!istype(new_area, whitelisted_area) && whitelisted_area) || (canidate.turf_flags & NO_LAVA_GEN))
+			continue
 
-		if(get_dir(src, F) in GLOB.cardinals)
-			cardinal_turfs += F
+		if(!logged_turf_type && ismineralturf(canidate))
+			var/turf/closed/mineral/mineral_canidate = canidate
+			logged_turf_type = mineral_canidate.turf_type
+
+		if(get_dir(src, canidate) in GLOB.cardinals)
+			cardinal_turfs += canidate
 		else
-			diagonal_turfs += F
+			diagonal_turfs += canidate
 
-	for(var/F in cardinal_turfs) //cardinal turfs are always changed but don't always spread
-		var/turf/T = F
-		if(!istype(T, logged_turf_type) && T.ChangeTurf(type, baseturfs, CHANGETURF_IGNORE_AIR) && prob(probability))
-			T.Spread(probability - prob_loss, prob_loss, whitelisted_area)
+	for(var/turf/cardinal_canidate as anything in cardinal_turfs) //cardinal turfs are always changed but don't always spread
+		// NOTE: WE ARE SKIPPING CHANGETURF HERE
+		// The calls in this proc only serve to provide a satisfactory (if it's not ALREADY this) check. They do not actually call changeturf
+		// This is safe because this proc can only be run during mapload, and nothing has initialized by now so there's nothing to inherit or delete
+		if(!istype(cardinal_canidate, logged_turf_type) && cardinal_canidate.ChangeTurf(type, baseturfs, CHANGETURF_SKIP) && prob(probability))
+			if(baseturfs)
+				cardinal_canidate.baseturfs = baseturfs
+			cardinal_canidate.Spread(probability - prob_loss, prob_loss, whitelisted_area)
 
-	for(var/F in diagonal_turfs) //diagonal turfs only sometimes change, but will always spread if changed
-		var/turf/T = F
-		if(!istype(T, logged_turf_type) && prob(probability) && T.ChangeTurf(type, baseturfs, CHANGETURF_IGNORE_AIR))
-			T.Spread(probability - prob_loss, prob_loss, whitelisted_area)
-		else if(ismineralturf(T))
-			var/turf/closed/mineral/M = T
-			M.ChangeTurf(M.turf_type, M.baseturfs, CHANGETURF_IGNORE_AIR)
-
-
+	for(var/turf/diagonal_canidate as anything in diagonal_turfs) //diagonal turfs only sometimes change, but will always spread if changed
+		// Important NOTE: SEE ABOVE
+		if(!istype(diagonal_canidate, logged_turf_type) && prob(probability) && diagonal_canidate.ChangeTurf(type, baseturfs, CHANGETURF_SKIP))
+			if(baseturfs)
+				diagonal_canidate.baseturfs = baseturfs
+			diagonal_canidate.Spread(probability - prob_loss, prob_loss, whitelisted_area)
+		else if(ismineralturf(diagonal_canidate))
+			var/turf/closed/mineral/diagonal_mineral = diagonal_canidate
+			// SEE ABOVE, THIS IS ONLY VERY RARELY SAFE
+			new diagonal_mineral.turf_type(diagonal_mineral)
 
 #undef RANDOM_UPPER_X
 #undef RANDOM_UPPER_Y

BeeStation/BeeStation-Hornet

code/game/turfs/open/river.dm
@@ -8,6 +8,7 @@
 	var/list/river_nodes = list()
 	var/num_spawned = 0
 	var/list/possible_locs = block(locate(min_x, min_y, target_z), locate(max_x, max_y, target_z))
+	new_baseturfs = baseturfs_string_list(new_baseturfs, pick(possible_locs))
 	while(num_spawned < nodes && possible_locs.len)
 		var/turf/T = pick(possible_locs)
 		var/area/A = get_area(T)
@@ -22,9 +23,12 @@
 		var/obj/effect/landmark/river_waypoint/W = A
 		if (W.z != target_z || W.connected)
 			continue
-		W.connected = 1
+		W.connected = TRUE
+		// Workaround around ChangeTurf that's safe because of when this proc is called
 		var/turf/cur_turf = get_turf(W)
-		cur_turf.ChangeTurf(turf_type, new_baseturfs, CHANGETURF_IGNORE_AIR)
+		cur_turf = new turf_type(cur_turf)
+		if(new_baseturfs)
+			cur_turf.baseturfs = new_baseturfs
 		var/turf/target_turf = get_turf(pick(river_nodes - W))
 		if(!target_turf)
 			break
@@ -53,7 +57,10 @@
 				cur_turf = get_step(cur_turf, cur_dir)
 				continue
 			else
-				var/turf/river_turf = cur_turf.ChangeTurf(turf_type, new_baseturfs, CHANGETURF_IGNORE_AIR)
+				// Workaround around ChangeTurf that's safe because of when this proc is called
+				var/turf/river_turf = new turf_type(cur_turf)
+				if(new_baseturfs)
+					river_turf.baseturfs = new_baseturfs
 				river_turf.Spread(25, 11, whitelist_area)
 
 	for(var/WP in river_nodes)
@@ -72,32 +79,42 @@
 	var/list/cardinal_turfs = list()
 	var/list/diagonal_turfs = list()
 	var/logged_turf_type
-	for(var/turf/T as() in RANGE_TURFS(1, src) - src)
-		var/area/new_area = get_area(T)
-		if(!T || (T.density && !ismineralturf(T)) || istype(T, /turf/open/indestructible) || (whitelisted_area && !istype(new_area, whitelisted_area)) || (T.flags_1 & NO_LAVA_GEN_1) )
+	for(var/turf/canidate as anything in RANGE_TURFS(1, src) - src)
+		if(!canidate || (canidate.density && !ismineralturf(canidate)) || istype(canidate, /turf/open/indestructible))
 			continue
 
-		if(!logged_turf_type && ismineralturf(T))
-			var/turf/closed/mineral/M = T
-			logged_turf_type = M.turf_type
+		var/area/new_area = get_area(canidate)
+		if((!istype(new_area, whitelisted_area) && whitelisted_area) || (canidate.flags_1 & NO_LAVA_GEN_1))
+			continue
 
-		if(get_dir(src, T) in GLOB.cardinals)
-			cardinal_turfs += T
+		if(!logged_turf_type && ismineralturf(canidate))
+			var/turf/closed/mineral/mineral_canidate = canidate
+			logged_turf_type = mineral_canidate.turf_type
+
+		if(get_dir(src, canidate) in GLOB.cardinals)
+			cardinal_turfs += canidate
 		else
-			diagonal_turfs += T
+			diagonal_turfs += canidate
 
-	for(var/turf/T as() in cardinal_turfs) //cardinal turfs are always changed but don't always spread
-		if(!istype(T, logged_turf_type) && T.ChangeTurf(type, baseturfs, CHANGETURF_IGNORE_AIR) && prob(probability))
-			T.Spread(probability - prob_loss, prob_loss, whitelisted_area)
+	for(var/turf/cardinal_canidate as anything in cardinal_turfs) //cardinal turfs are always changed but don't always spread
+		// NOTE: WE ARE SKIPPING CHANGETURF HERE
+		// The calls in this proc only serve to provide a satisfactory (if it's not ALREADY this) check. They do not actually call changeturf
+		// This is safe because this proc can only be run during mapload, and nothing has initialized by now so there's nothing to inherit or delete
+		if(!istype(cardinal_canidate, logged_turf_type) && cardinal_canidate.ChangeTurf(type, baseturfs, CHANGETURF_SKIP) && prob(probability))
+			if(baseturfs)
+				cardinal_canidate.baseturfs = baseturfs
+			cardinal_canidate.Spread(probability - prob_loss, prob_loss, whitelisted_area)
 
-	for(var/turf/T as() in diagonal_turfs) //diagonal turfs only sometimes change, but will always spread if changed
-		if(!istype(T, logged_turf_type) && prob(probability) && T.ChangeTurf(type, baseturfs, CHANGETURF_IGNORE_AIR))
-			T.Spread(probability - prob_loss, prob_loss, whitelisted_area)
-		else if(ismineralturf(T))
-			var/turf/closed/mineral/M = T
-			M.ChangeTurf(M.turf_type, M.baseturfs, CHANGETURF_IGNORE_AIR)
-
-
+	for(var/turf/diagonal_canidate as anything in diagonal_turfs) //diagonal turfs only sometimes change, but will always spread if changed
+		// Important NOTE: SEE ABOVE
+		if(!istype(diagonal_canidate, logged_turf_type) && prob(probability) && diagonal_canidate.ChangeTurf(type, baseturfs, CHANGETURF_SKIP))
+			if(baseturfs)
+				diagonal_canidate.baseturfs = baseturfs
+			diagonal_canidate.Spread(probability - prob_loss, prob_loss, whitelisted_area)
+		else if(ismineralturf(diagonal_canidate))
+			var/turf/closed/mineral/diagonal_mineral = diagonal_canidate
+			// SEE ABOVE, THIS IS ONLY VERY RARELY SAFE
+			new diagonal_mineral.turf_type(diagonal_mineral)
 
 #undef RANDOM_UPPER_X
 #undef RANDOM_UPPER_Y