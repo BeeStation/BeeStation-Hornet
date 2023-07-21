HolyTigerGod/Voidcrew

Makes the ChangeTurf same turf optimization work properly again, and converts baseturfs into a string_list (#54277)
I've converted baseturfs into a string list, I had to add a helper proc for baseturf stringlistifying, as the system expects single length baseturfs to not be a list, and I needed to support that. I added a length check of 100 to the helper proc, to help prevent more stuff like what got us into this mess in the first place, the kilo oom bug.

Makes ChangeTurf a lot faster in some cases, as it should be, and saves a lot of memory with cached lists.

code/datums/shuttles.dm
@@ -68,7 +68,9 @@
 			continue
 		if(length(place.baseturfs) < 2) // Some snowflake shuttle shit
 			continue
-		place.baseturfs.Insert(3, /turf/baseturf_skipover/shuttle)
+		var/list/sanity = place.baseturfs.Copy()
+		sanity.Insert(3, /turf/baseturf_skipover/shuttle)
+		place.baseturfs = baseturfs_string_list(sanity, place)
 
 		for(var/obj/docking_port/mobile/port in place)
 			if(register)

code/game/turfs/change_turf.dm
@@ -104,9 +104,9 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 		W.TakeComponent(i)
 
 	if(new_baseturfs)
-		W.baseturfs = new_baseturfs
+		W.baseturfs = baseturfs_string_list(new_baseturfs, W)
 	else
-		W.baseturfs = old_baseturfs
+		W.baseturfs = baseturfs_string_list(old_baseturfs, W) //Just to be safe
 
 	W.explosion_id = old_exi
 	W.explosion_level = old_exl
@@ -189,8 +189,7 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 			assemble_baseturfs(fake_turf_type)
 			if(!length(baseturfs))
 				baseturfs = list(baseturfs)
-			baseturfs -= baseturfs & GLOB.blacklisted_automated_baseturfs
-			baseturfs += old_baseturfs
+			baseturfs = baseturfs_string_list((baseturfs - (baseturfs & GLOB.blacklisted_automated_baseturfs)) + old_baseturfs, src)
 			return
 		else if(!length(new_baseturfs))
 			new_baseturfs = list(new_baseturfs, fake_turf_type)
@@ -198,6 +197,7 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 			new_baseturfs += fake_turf_type
 	if(!length(baseturfs))
 		baseturfs = list(baseturfs)
+	baseturfs = baseturfs_string_list(new_baseturfs + baseturfs, src)
 	baseturfs.Insert(1, new_baseturfs)
 
 // Make a new turf and put it on top
@@ -226,25 +226,26 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 			newT.assemble_baseturfs(initial(fake_turf_type.baseturfs)) // The baseturfs list is created like roundstart
 			if(!length(newT.baseturfs))
 				newT.baseturfs = list(baseturfs)
-			newT.baseturfs -= GLOB.blacklisted_automated_baseturfs
-			newT.baseturfs.Insert(1, old_baseturfs) // The old baseturfs are put underneath
+			// The old baseturfs are put underneath, and we sort out the unwanted ones
+			newT.baseturfs = baseturfs_string_list(old_baseturfs + (newT.baseturfs - GLOB.blacklisted_automated_baseturfs), newT)
 			return newT
 		if(!length(baseturfs))
 			baseturfs = list(baseturfs)
 		if(!istype(src, /turf/closed))
-			baseturfs += type
-		baseturfs += new_baseturfs
+			new_baseturfs = list(type) + new_baseturfs
+		baseturfs = baseturfs_string_list(baseturfs + new_baseturfs, src)
 		return ChangeTurf(fake_turf_type, null, flags)
 	if(!length(baseturfs))
 		baseturfs = list(baseturfs)
+	var/list/temp_baseturfs = list()
 	if(!istype(src, /turf/closed))
-		baseturfs += type
+		temp_baseturfs += type
 	var/turf/change_type
 	if(length(new_baseturfs))
 		change_type = new_baseturfs[new_baseturfs.len]
 		new_baseturfs.len--
 		if(new_baseturfs.len)
-			baseturfs += new_baseturfs
+			baseturfs = baseturfs_string_list(baseturfs + temp_baseturfs + new_baseturfs, src)
 	else
 		change_type = new_baseturfs
 	return ChangeTurf(change_type, null, flags)
@@ -269,7 +270,7 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 			new_baseturfs += target_baseturfs
 
 	var/turf/newT = copytarget.copyTurf(src, copy_air)
-	newT.baseturfs = new_baseturfs
+	newT.baseturfs = baseturfs_string_list(new_baseturfs, newT)
 	return newT
 
 

BeeStation/BeeStation-Hornet

code/datums/shuttles.dm
@@ -158,7 +158,10 @@
 
 		if(!islist(shuttle_turf.baseturfs))
 			shuttle_turf.baseturfs = list(shuttle_turf.baseturfs)
-		shuttle_turf.baseturfs.Insert(shuttle_turf.baseturfs.len + 1 - baseturf_length, /turf/baseturf_skipover/shuttle)
+
+		var/list/sanity = shuttle_turf.baseturfs.Copy()
+		sanity.Insert(shuttle_turf.baseturfs.len + 1 - baseturf_length, /turf/baseturf_skipover/shuttle)
+		shuttle_turf.baseturfs = baseturfs_string_list(sanity, shuttle_turf)
 
 	//If this is a superfunction call, we don't want to initialize atoms here, let the subfunction handle that
 	if(finalize)

code/game/turfs/change_turf.dm
@@ -115,9 +115,9 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 		callback.InvokeAsync(new_turf)
 
 	if(new_baseturfs)
-		new_turf.baseturfs = new_baseturfs
+		new_turf.baseturfs = baseturfs_string_list(new_baseturfs, new_turf)
 	else
-		new_turf.baseturfs = old_baseturfs
+		new_turf.baseturfs = baseturfs_string_list(old_baseturfs, new_turf) //Just to be safe
 
 	new_turf.explosion_id = old_exi
 	new_turf.explosion_level = old_exl
@@ -209,8 +209,7 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 			assemble_baseturfs(fake_turf_type)
 			if(!length(baseturfs))
 				baseturfs = list(baseturfs)
-			baseturfs -= baseturfs & GLOB.blacklisted_automated_baseturfs
-			baseturfs += old_baseturfs
+			baseturfs = baseturfs_string_list((baseturfs - (baseturfs & GLOB.blacklisted_automated_baseturfs)) + old_baseturfs, src)
 			return
 		else if(!length(new_baseturfs))
 			new_baseturfs = list(new_baseturfs, fake_turf_type)
@@ -218,6 +217,7 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 			new_baseturfs += fake_turf_type
 	if(!length(baseturfs))
 		baseturfs = list(baseturfs)
+	baseturfs = baseturfs_string_list(new_baseturfs + baseturfs, src)
 	baseturfs.Insert(1, new_baseturfs)
 
 // Make a new turf and put it on top
@@ -246,25 +246,26 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 			newT.assemble_baseturfs(initial(fake_turf_type.baseturfs)) // The baseturfs list is created like roundstart
 			if(!length(newT.baseturfs))
 				newT.baseturfs = list(baseturfs)
-			newT.baseturfs -= GLOB.blacklisted_automated_baseturfs
-			newT.baseturfs.Insert(1, old_baseturfs) // The old baseturfs are put underneath
+			// The old baseturfs are put underneath, and we sort out the unwanted ones
+			newT.baseturfs = baseturfs_string_list(old_baseturfs + (newT.baseturfs - GLOB.blacklisted_automated_baseturfs), newT)
 			return newT
 		if(!length(baseturfs))
 			baseturfs = list(baseturfs)
 		if(!istype(src, /turf/closed))
-			baseturfs += type
-		baseturfs += new_baseturfs
+			new_baseturfs = list(type) + new_baseturfs
+		baseturfs = baseturfs_string_list(baseturfs + new_baseturfs, src)
 		return ChangeTurf(fake_turf_type, null, flags)
 	if(!length(baseturfs))
 		baseturfs = list(baseturfs)
+	var/list/temp_baseturfs = list()
 	if(!istype(src, /turf/closed))
-		baseturfs += type
+		temp_baseturfs += type
 	var/turf/change_type
 	if(length(new_baseturfs))
 		change_type = new_baseturfs[new_baseturfs.len]
 		new_baseturfs.len--
 		if(new_baseturfs.len)
-			baseturfs += new_baseturfs
+			baseturfs = baseturfs_string_list(baseturfs + temp_baseturfs + new_baseturfs, src)
 	else
 		change_type = new_baseturfs
 	return ChangeTurf(change_type, null, flags)
@@ -289,7 +290,7 @@ GLOBAL_LIST_INIT(blacklisted_automated_baseturfs, typecacheof(list(
 			new_baseturfs += target_baseturfs
 
 	var/turf/newT = copytarget.copyTurf(src, copy_air)
-	newT.baseturfs = new_baseturfs
+	newT.baseturfs = baseturfs_string_list(new_baseturfs, newT)
 	return newT
 
 