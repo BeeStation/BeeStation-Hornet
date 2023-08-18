63f4fa8c2b841a4aa0f74d87d38c71aa4b8ee5aa
Lucy <lucy@absolucy.moe>
2023-08-18 17:33:27 -0400

    Remove preference; use default `is_afk` activity period.

--- code/modules/client/preferences/entries/player/afk.dm
+++
@@ -1,9 +0,0 @@
-/datum/preference/numeric/afk_time
-	category = PREFERENCE_CATEGORY_GAME_PREFERENCES
-	db_key = "afk_time"
-	preference_type = PREFERENCE_PLAYER
-	minimum = 3
-	maximum = 10
-
-/datum/preference/numeric/afk_time/create_default_value()
-	return 5
--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/afk.tsx
+++
@@ -1,8 +0,0 @@
-import { FeatureNumberInput, FeatureNumeric } from '../base';
-
-export const afk_time: FeatureNumeric = {
-  name: 'Time until AFK (in minutes)',
-  category: 'GAMEPLAY',
-  description: 'How long you can be inactive for before you are shown as SSD when examined.',
-  component: FeatureNumberInput,
-};
