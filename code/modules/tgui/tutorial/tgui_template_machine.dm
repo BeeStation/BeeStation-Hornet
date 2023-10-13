8e16dddcbc1cd754737844a08c64cdbd6f4daf0d
Evildragon <draco@evildragon.co.kr>
2023-10-13 09:41:56 +0900

    store

--- code/modules/tgui/tutorial/tgui_template_machine.dm
+++ code/modules/tgui/tutorial/tgui_template_machine.dm
@@ -5,55 +5,71 @@
 	icon_state = "sextractor_open"
 
 	var/list/options
-	var/ui_message
 	var/current_option
+	var/ui_message
 
 /obj/machinery/tgui_template/Initialize(mapload)
 	. = ..()
 
 	options = list("Love", "Bravery", "Honor")
-	ui_message = "Hello, world!"
 	current_option = options[1]
+	ui_message = "Hello, world!"
 
 /obj/machinery/tgui_template/ui_interact(mob/user, datum/tgui/ui)
 	ui = SStgui.try_update_ui(user, src, ui)
 	if(!ui)
 		ui = new(user, src, "TGUITemplate")
 		ui.open()
-
+		/* ui.set_autoupdate(TRUE) */
+		//! It does literally auto-update. Uncomment this if you need auto-update for your UI.
 
 /obj/machinery/tgui_template/ui_data(mob/user)
 	var/list/data = list()
-	. = data
+	/* . = data */ //! Don't use implicit return(the dot one). You should use explicit return.
 
 	data["options"] = options
 	data["ui_message"] = ui_message
 	data["current_option"] = current_option
 
+	return data //! this is explicit return.
+
 /obj/machinery/tgui_template/ui_static_data(mob/user)
 	var/list/data = list()
-	. = data
+	/* . = data */
 
 	data["something_static"] = "This is static string. This is never changed."
-	// if you really need update, use update_static_data() or update_static_data_for_all_viewers() procs
+	//! if you really need update, use update_static_data() or update_static_data_for_all_viewers() procs
+
+	return data
 
 /obj/machinery/tgui_template/ui_assets(mob/user)
 	return list(
-		get_asset_datum(/datum/asset/spritesheet/research_designs)) // sample
+		get_asset_datum(/datum/asset/spritesheet/research_designs)
+	) //! sample code to explain how asset works.
 
 /obj/machinery/tgui_template/ui_act(action, params)
-	if(..())
+	. = ..() //! It's important to catch implicit return value first from parent.
+	if(.)	 //! Thus, `if(..())` is not recommended. You need to store a value into the dot.
 		return
 	if(action)
 		to_chat(usr, "<span class='notice'>You took action: [action]</span>")
 	switch(action)
 		if("button_clicked")
-			var/new_button = params["chosen_option"]
-			for(var/each_button in options)
-				if(new_button == each_button)
-					to_chat(usr, "<span class='notice'>Your clicked button is [each_button]</span>")
-					current_option = each_button
-					ui_update()
+			var/new_button = sanitize(params["chosen_option"])
+			if(new_button in options) //! This is important. This is checking if the chosen option is really available.
+				to_chat(usr, "<span class='notice'>Your clicked button is [each_button]</span>")
+				current_option = each_button
+				/* ui_update() */
+				SStgui.update_uis(src)
+			/*	<-- why SStgui.update_uis(src) over ui_update() ?? -->
+					> ui_update()
+						This does force-update your TGUI window regardless of server status.
+						This can cause lags if a user can spam this. Avoid using this if user can spam this.
+					> SStgui.update_uis(src)
+						This is used to prevent spamming
+
+					But, setting `ui.set_autoupdate(TRUE)` works at most times.         */
+
 		if("change_message")
 			var/new_message = params["new_message"]
 			if(!new_message)
--- tgui/packages/tgui/interfaces/TGUITemplate.tsx
+++ tgui/packages/tgui/interfaces/TGUITemplate.tsx
@@ -1,6 +1,6 @@
 import { classes } from 'common/react';
 import { useBackend, useLocalState } from '../backend';
-import { Input, Button, Stack, Box } from '../components';
+import { Section, Input, Button, Stack, Box } from '../components';
 import { Window } from '../layouts';
 
 type TGUITemplateData = {
@@ -17,12 +17,10 @@ export const TGUITemplate = (props, context) => {
   return (
     <Window width={400} height={400} title="TGUI Template">
       <Window.Content>
-        <Stack vertical>
-          <MessageShower />
-          <ButtonClicker />
-          <ShowsStaticData />
-          <ShowsAssetImage />
-        </Stack>
+        <MessageShower />
+        <ButtonClicker />
+        <ShowsStaticData />
+        <ShowsAssetImage />
       </Window.Content>
     </Window>
   );
@@ -33,8 +31,7 @@ const MessageShower = (props, context) => {
   const { ui_message } = data;
 
   return (
-    <Stack.Item>
-      {'Current text: '}
+    <Section title="Message Shower">
       <Input
         align="right"
         value={ui_message}
@@ -42,7 +39,7 @@ const MessageShower = (props, context) => {
           act('change_message', { 'new_message': value });
         }}
       />
-    </Stack.Item>
+    </Section>
   );
 };
 
@@ -51,33 +48,69 @@ const ButtonClicker = (props, context) => {
   const { options } = data;
 
   return (
-    <>
-      <Stack.Item>
-        {'Buttons: '}
-        {options.map((each) => {
-          return <ButtonForChange key={each} chosen_option={each} />;
-        })}
-      </Stack.Item>
-      <Stack.Item>
-        {'Buttons as index: '}
-        {Object.keys(options).map((each) => {
-          return <ButtonForChange key={each} chosen_option={options[each]} button_content={options[each] + ', idx ' + each} />;
-        })}
-      </Stack.Item>
-    </>
+    <Section title="Button clicker">
+      <Stack vertical>
+        <Stack.Item>
+          {'Buttons: '}
+          {options.map((each) => {
+            return <ButtonForChange key={each} chosen_option={each} />;
+          })}
+        </Stack.Item>
+        <Stack.Item>
+          {'Buttons as index: '}
+          {Object.keys(options).map((each) => {
+            return (
+              <ButtonForChange key={each} chosen_option={options[each]} button_content={options[each] + ', idx ' + each} />
+            );
+          })}
+        </Stack.Item>
+      </Stack>
+    </Section>
+  );
+};
+
+const ButtonClickerLocalState = (props, context) => {
+  const { act, data } = useBackend<TGUITemplateData>(context);
+  const { options } = data;
+
+  return (
+    <Section title="Button clicker">
+      This is the same code above, but it uses LocalState instead of saving it to DM side. This is faster than sending data to
+      DM then updating.
+      <Stack vertical>
+        <Stack.Item>
+          {'Buttons: '}
+          {options.map((each_item, each_index) => {
+            return <ButtonForChange key={each_item} chosen_option={each_item} />;
+          })}
+        </Stack.Item>
+        <Stack.Item>
+          {'Buttons as index: '}
+          {Object.keys(options).map((each_index) => {
+            return (
+              <ButtonForChange
+                key={each_index}
+                chosen_option={options[each_index]}
+                button_content={options[each_index] + ', idx ' + each_index}
+              />
+            );
+          })}
+        </Stack.Item>
+      </Stack>
+    </Section>
   );
 };
 
 const ButtonForChange = (props, context) => {
   const { act, data } = useBackend<TGUITemplateData>(context);
-  const { key, chosen_option, button_content = null } = props;
-  const { current_option } = data;
+  const { chosen_option, button_content = null } = props;
+  const [chosenOption, setChosenOption] = useLocalState(context, 'tgui_template_chosen_option', chosen_option);
 
   return (
     <Button
-      key={key}
-      disabled={chosen_option === current_option}
-      onClick={(e) => act('button_clicked', { 'chosen_option': chosen_option })}>
+      key={chosen_option}
+      disabled={chosen_option === chosenOption}
+      onClick={() => act('button_clicked', { 'chosen_option': chosen_option })}>
       {button_content ? button_content : chosen_option}
     </Button>
   );
@@ -87,16 +120,14 @@ const ShowsStaticData = (props, context) => {
   const { act, data } = useBackend<TGUITemplateData>(context);
   const { something_static } = data;
 
-  return <Stack.Item>{'Static string: ' + something_static}</Stack.Item>;
+  return <Section title="Static data">{'Static string: ' + something_static}</Section>;
 };
 
-const ShowsAssetImage = (props, context) => {
-  // const { act, data } = useBackend<TGUITemplateData>(context); // no need
-
+const ShowsAssetImage = () => {
   return (
-    <Stack.Item>
+    <Section title="Sprite asset sample">
       {'Asset image sample '}
       <Box className={classes(['design32x32', 'analyzer'])} />
-    </Stack.Item>
+    </Section>
   );
 };
