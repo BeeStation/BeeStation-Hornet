de77f968ff6f5be630758823faa7792b0594f636
RKz <falldownftw@gmail.com>
2023-10-19 19:32:11 -0400

    ran prettier

--- tgui/packages/tgui/interfaces/MinigamesMenu.js
+++ tgui/packages/tgui/interfaces/MinigamesMenu.js
@@ -5,22 +5,12 @@ import { Window } from '../layouts';
 export const MinigamesMenu = (props, context) => {
   const { act } = useBackend(context);
   return (
-    <Window
-      title="Minigames Menu"
-      width={450}
-      height={200}>
+    <Window title="Minigames Menu" width={450} height={200}>
       <Window.Content>
         <Section title="Select Minigame" textAlign="center">
           <Stack>
             <Stack.Item grow>
-              <Button
-                content="CTF"
-                fluid={1}
-                fontSize={3}
-                textAlign="center"
-                lineHeight="3"
-                onClick={() => act('ctf')}
-              />
+              <Button content="CTF" fluid={1} fontSize={3} textAlign="center" lineHeight="3" onClick={() => act('ctf')} />
             </Stack.Item>
           </Stack>
         </Section>
