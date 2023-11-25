89406a93a80119e3d02fcc37438365e6af4b0e40
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2023-11-25 00:46:41 +0000

    Executes prettification

--- tgui/packages/tgui/interfaces/GenPop.js
+++ tgui/packages/tgui/interfaces/GenPop.js
@@ -9,17 +9,10 @@ import { Window } from '../layouts';
 export const GenPop = (props, context) => {
   const { act, data } = useBackend(context);
 
-  const {
-    desired_name = "",
-    desired_details = "",
-    crime_list = {},
-  } = data;
+  const { desired_name = '', desired_details = '', crime_list = {} } = data;
 
   // Local state for the time of the prisoner.
-  const [
-    time,
-    setTime,
-  ] = useSharedState(context, 'time', 0);
+  const [time, setTime] = useSharedState(context, 'time', 0);
 
   // Local state to determine which modifiers we have active
   const [resistedMod, setResistedMod] = useSharedState(context, 'resisted', false);
@@ -27,41 +20,38 @@ export const GenPop = (props, context) => {
   const [elevatedMod, setElevatedMod] = useSharedState(context, 'repeat_offender', false);
 
   // Local state for the name of the crime being issued
-  const [
-    crimeName,
-    setCrimeName,
-  ] = useSharedState(context, 'crimeName', 'No crime');
+  const [crimeName, setCrimeName] = useSharedState(context, 'crimeName', 'No crime');
 
   // Local state for the name of the crime details
-  const [
-    crimeDetails,
-    setCrimeDetails,
-  ] = useSharedState(context, 'crimeDetails', 'No details provided');
+  const [crimeDetails, setCrimeDetails] = useSharedState(context, 'crimeDetails', 'No details provided');
 
   // Local state for the current category that we are browsing
-  const [
-    crimeCategory,
-    setCrimeCategory,
-  ] = useSharedState(context, 'crimeCategory', (crime_list && crime_list.length > 0) ? Object.keys(crime_list)[0] : null);
+  const [crimeCategory, setCrimeCategory] = useSharedState(
+    context,
+    'crimeCategory',
+    crime_list && crime_list.length > 0 ? Object.keys(crime_list)[0] : null
+  );
 
   const resetLocalState = () => {
     // Set back to the default crime name
-    setCrimeName("No crime");
+    setCrimeName('No crime');
     setTime(3000);
-    setCrimeDetails("No details provided");
+    setCrimeDetails('No details provided');
     setResistedMod(false);
     setAttemptedMod(false);
     setElevatedMod(false);
   };
 
   const getProcessedTime = () => {
-    return (time
-      // Back to major crime (9000)
-      + (attemptedMod ? (time >= 36000 ? -27000 : -3000) : 0)
-      // Set to 36000 if a crime over 900 deciseconds was repeated.
-      + (elevatedMod ? (time >= 9000 ? 360000 - time : 3000) : 0))
+    return (
+      (time +
+        // Back to major crime (9000)
+        (attemptedMod ? (time >= 36000 ? -27000 : -3000) : 0) +
+        // Set to 36000 if a crime over 900 deciseconds was repeated.
+        (elevatedMod ? (time >= 9000 ? 360000 - time : 3000) : 0)) *
       // Resisting arrest applies to everything at the end
-      * (resistedMod ? 1.2 : 1);
+      (resistedMod ? 1.2 : 1)
+    );
   };
 
   return (
@@ -79,32 +69,40 @@ export const GenPop = (props, context) => {
               <Button
                 icon="print"
                 content="Finalize ID"
-                tooltip={(attemptedMod && time <= 3000) ? "Attempted minor crimes must be met with fines!" : (crimeName === 'No crime' ? "You have not selected a crime." : (!data.desired_name ? "The prisoner requires an identifier for their card." : null))}
+                tooltip={
+                  attemptedMod && time <= 3000
+                    ? 'Attempted minor crimes must be met with fines!'
+                    : crimeName === 'No crime'
+                      ? 'You have not selected a crime.'
+                      : !data.desired_name
+                        ? 'The prisoner requires an identifier for their card.'
+                        : null
+                }
                 color="good"
                 disabled={!data.canPrint || (attemptedMod && time <= 3000) || crimeName === 'No crime' || !data.desired_name}
                 onClick={() => {
                   act('print', {
                     desired_sentence: getProcessedTime(),
-                    desired_crime: (attemptedMod ? "Attempted " : "") + crimeName + (elevatedMod ? " (Repeat offender)" : ""),
-                });
-                // Reset to the default state
-                resetLocalState();
-              }} />
+                    desired_crime: (attemptedMod ? 'Attempted ' : '') + crimeName + (elevatedMod ? ' (Repeat offender)' : ''),
+                  });
+                  // Reset to the default state
+                  resetLocalState();
+                }}
+              />
             </Fragment>
           }>
           <Button icon="fast-backward" onClick={() => setTime(clamp(time - 1200, 0, 36000))} />
           <Button icon="backward" onClick={() => setTime(clamp(time - 600, 0, 36000))} />
-          {String(getProcessedTime() / 600)} min:{' '}
-          <Button icon="forward" onClick={() => setTime(clamp(time + 600, 0, 36000))} />
+          {String(getProcessedTime() / 600)} min: <Button icon="forward" onClick={() => setTime(clamp(time + 600, 0, 36000))} />
           <Button icon="fast-forward" onClick={() => setTime(clamp(time + 1200, 0, 36000))} />
           <br />
           {Object.keys(crime_list)
             // Remove any categories with no crimes so we don't bluescreen in the event that a category has no crimes.
-            .filter(crime => crime_list[crime].length > 0)
-            .map(category => (
+            .filter((crime) => crime_list[crime].length > 0)
+            .map((category) => (
               <Button
                 key={category}
-                icon={category === "Capital" ? "exclamation-triangle" : "hourglass-start"}
+                icon={category === 'Capital' ? 'exclamation-triangle' : 'hourglass-start'}
                 content={category}
                 color={crime_list[category][0].colour}
                 onClick={() => {
@@ -112,7 +110,8 @@ export const GenPop = (props, context) => {
                   setCrimeCategory(category);
                   // Set the default crime length
                   setTime(crime_list[category][0].sentence);
-                }} />
+                }}
+              />
             ))}
           <br />
         </Section>
@@ -129,7 +128,7 @@ export const GenPop = (props, context) => {
                 selected={crimeName === value.name}
                 onClick={() => {
                   if (crimeName === value.name) {
-                    setCrimeName("No crime");
+                    setCrimeName('No crime');
                     setTime(3000);
                     return;
                   }
@@ -170,10 +169,12 @@ export const GenPop = (props, context) => {
           />
         </Section>
         <Section title="Preview">
-          Identity: {String(data.desired_name || "No name entered")} <br />
+          Identity: {String(data.desired_name || 'No name entered')} <br />
           Crime: {String(crimeName)} <br />
           Sentence: {String(getProcessedTime() / 600)} min <br />
-          <Section title="Crime details" buttons={<Button content="Edit" icon="pen" onClick={() => act('edit_details')} />}>{desired_details}</Section>
+          <Section title="Crime details" buttons={<Button content="Edit" icon="pen" onClick={() => act('edit_details')} />}>
+            {desired_details}
+          </Section>
         </Section>
         <Section title="Prison Management:">
           {Object.keys(data.allPrisoners).map((key) => {
