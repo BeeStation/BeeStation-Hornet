347bf8b17f00215f822e86ebbfcf85d967876b2e
Crossedfall <ping@crossedfall.io>
2023-12-10 22:46:10 -0600

    Reverting back to 43f695e

--- dependencies.sh
+++ dependencies.sh
@@ -5,7 +5,7 @@
 
 # byond version
 export BYOND_MAJOR=515
-export BYOND_MINOR=1620
+export BYOND_MINOR=1608
 
 #rust version
 export RUST_VERSION=1.67.1
@@ -21,7 +21,7 @@ export NODE_VERSION_PRECISE=18.14.2
 export SPACEMAN_DMM_VERSION=suite-1.7.1
 
 #auxmos version
-export AUXMOS_VERSION=2.2.3
+export AUXMOS_VERSION=2.2.2
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.11.2
