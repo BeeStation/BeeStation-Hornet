HolyTigerGod/Voidcrew

Defer creation of emergency power lights until needed -- Saves ~0.2s of init time (#71729)
Every light was creating a power cell. Creating a power cell creates
reagents, creates appearances, updates the appearances to reflect its
charge, etc etc. It's very expensive. Of the 293ms, 1,652 calls of
cell/Initialize, 196ms of it was emergency_light/Initialize.

Defers it until necessary, `get_cell()` will produce the cell if it
doesn't exist.

Closes https://github.com/tgstation/dev-cycles-initiative/issues/21

code/modules/power/lighting/light.dm
@@ -38,6 +38,9 @@
 	var/switchcount = 0
 	///Cell reference
 	var/obj/item/stock_parts/cell/cell
+	/// If TRUE, then cell is null, but one is pretending to exist.
+	/// This is to defer emergency cell creation unless necessary, as it is very expensive.
+	var/has_mock_cell = TRUE
 	///If true, this fixture generates a very weak cell at roundstart
 	var/start_with_cell = TRUE
 	///Currently in night shift mode?
@@ -85,8 +88,8 @@
 		var/obj/machinery/power/apc/temp_apc = our_area.apc
 		nightshift_enabled = temp_apc?.nightshift_lights
 
-	if(start_with_cell && !no_low_power)
-		cell = new/obj/item/stock_parts/cell/emergency_light(src)
+	if(!start_with_cell || no_low_power)
+		has_mock_cell = FALSE
 
 	RegisterSignal(src, COMSIG_LIGHT_EATER_ACT, PROC_REF(on_light_eater))
 	AddElement(/datum/element/atmos_sensitive, mapload)
@@ -264,6 +267,10 @@
 	update()
 
 /obj/machinery/light/get_cell()
+	if (has_mock_cell)
+		cell = new /obj/item/stock_parts/cell/emergency_light(src)
+		has_mock_cell = FALSE
+
 	return cell
 
 // examine verb
@@ -278,8 +285,8 @@
 			. += "The [fitting] is burnt out."
 		if(LIGHT_BROKEN)
 			. += "The [fitting] has been smashed."
-	if(cell)
-		. += "Its backup power charge meter reads [round((cell.charge / cell.maxcharge) * 100, 0.1)]%."
+	if(cell || has_mock_cell)
+		. += "Its backup power charge meter reads [has_mock_cell ? 100 : round((cell.charge / cell.maxcharge) * 100, 0.1)]%."
 
 
 
@@ -366,9 +373,11 @@
 			drop_light_tube()
 		new /obj/item/stack/cable_coil(loc, 1, "red")
 	transfer_fingerprints_to(new_light)
-	if(!QDELETED(cell))
-		new_light.cell = cell
-		cell.forceMove(new_light)
+
+	var/obj/item/stock_parts/cell/real_cell = get_cell()
+	if(!QDELETED(real_cell))
+		new_light.cell = real_cell
+		real_cell.forceMove(new_light)
 		cell = null
 	qdel(src)
 
@@ -415,23 +424,27 @@
 // returns whether this light has emergency power
 // can also return if it has access to a certain amount of that power
 /obj/machinery/light/proc/has_emergency_power(power_usage_amount)
-	if(no_low_power || !cell)
+	if(no_low_power || (!cell && !has_mock_cell))
 		return FALSE
+	if (has_mock_cell)
+		return status == LIGHT_OK
 	if(power_usage_amount ? cell.charge >= power_usage_amount : cell.charge)
 		return status == LIGHT_OK
+	return FALSE
 
 // attempts to use power from the installed emergency cell, returns true if it does and false if it doesn't
 /obj/machinery/light/proc/use_emergency_power(power_usage_amount = LIGHT_EMERGENCY_POWER_USE)
 	if(!has_emergency_power(power_usage_amount))
 		return FALSE
-	if(cell.charge > 300) //it's meant to handle 120 W, ya doofus
+	var/obj/item/stock_parts/cell/real_cell = get_cell()
+	if(real_cell.charge > 300) //it's meant to handle 120 W, ya doofus
 		visible_message(span_warning("[src] short-circuits from too powerful of a power cell!"))
 		burn_out()
 		return FALSE
-	cell.use(power_usage_amount)
+	real_cell.use(power_usage_amount)
 	set_light(
 		l_range = brightness * bulb_low_power_brightness_mul,
-		l_power = max(bulb_low_power_pow_min, bulb_low_power_pow_mul * (cell.charge / cell.maxcharge)),
+		l_power = max(bulb_low_power_pow_min, bulb_low_power_pow_mul * (real_cell.charge / real_cell.maxcharge)),
 		l_color = bulb_low_power_colour
 		)
 	return TRUE

BeeStation/BeeStation-Hornet

code/modules/power/lighting/light.dm
@@ -30,6 +30,9 @@
 	var/rigged = FALSE			// true if rigged to explode
 
 	var/obj/item/stock_parts/cell/cell
+	/// If TRUE, then cell is null, but one is pretending to exist.
+	/// This is to defer emergency cell creation unless necessary, as it is very expensive.
+	var/has_mock_cell = TRUE
 	var/start_with_cell = TRUE	// if true, this fixture generates a very weak cell at roundstart
 
 	var/nightshift_enabled = FALSE	//Currently in night shift mode?
@@ -132,8 +135,10 @@
 	if(!mapload) //sync up nightshift lighting for player made lights
 		var/obj/machinery/power/apc/temp_apc = A.apc
 		nightshift_enabled = temp_apc?.nightshift_lights
-	if(start_with_cell && !no_emergency)
-		store_cell(new/obj/item/stock_parts/cell/emergency_light(src))
+
+	if(!start_with_cell || no_emergency)
+		has_mock_cell = FALSE
+
 	spawn(2)
 		switch(fitting)
 			if("tube")
@@ -290,12 +295,11 @@
 		addtimer(CALLBACK(src, PROC_REF(broken_sparks)), delay, TIMER_UNIQUE | TIMER_NO_HASH_WAIT)
 
 /obj/machinery/light/process()
-	if (!cell)
-		return PROCESS_KILL
 	if(has_power())
-		if (cell.charge == cell.maxcharge)
-			return PROCESS_KILL
-		cell.charge = min(cell.maxcharge, cell.charge + LIGHT_EMERGENCY_POWER_USE) //Recharge emergency power automatically while not using it
+		if(cell)
+			if(cell.charge == cell.maxcharge)
+				return PROCESS_KILL
+			cell.charge = min(cell.maxcharge, cell.charge + LIGHT_EMERGENCY_POWER_USE) //Recharge emergency power automatically while not using it
 	if(emergency_mode && !use_emergency_power(LIGHT_EMERGENCY_POWER_USE))
 		update(FALSE) //Disables emergency mode and sets the color to normal
 
@@ -315,6 +319,10 @@
 	update(FALSE)
 
 /obj/machinery/light/get_cell()
+	if (has_mock_cell)
+		store_cell(new /obj/item/stock_parts/cell/emergency_light(src))
+		has_mock_cell = FALSE
+
 	return cell
 
 // examine verb
@@ -329,8 +337,8 @@
 			. += "The [fitting] is burnt out."
 		if(LIGHT_BROKEN)
 			. += "The [fitting] has been smashed."
-	if(cell)
-		. += "Its backup power charge meter reads [round((cell.charge / cell.maxcharge) * 100, 0.1)]%."
+	if(cell || has_mock_cell)
+		. += "Its backup power charge meter reads [has_mock_cell ? 100 : round((cell.charge / cell.maxcharge) * 100, 0.1)]%."
 
 
 
@@ -414,9 +422,10 @@
 				drop_light_tube()
 			new /obj/item/stack/cable_coil(loc, 1, "red")
 		transfer_fingerprints_to(newlight)
-		if(!QDELETED(cell))
-			newlight.store_cell(cell)
-			cell.forceMove(newlight)
+		var/obj/item/stock_parts/cell/real_cell = get_cell()
+		if(!QDELETED(real_cell))
+			newlight.store_cell(real_cell)
+			real_cell.forceMove(newlight)
 			remove_cell()
 	qdel(src)
 
@@ -464,21 +473,25 @@
 // returns whether this light has emergency power
 // can also return if it has access to a certain amount of that power
 /obj/machinery/light/proc/has_emergency_power(pwr)
-	if(no_emergency || !cell)
+	if(no_emergency || (!cell && !has_mock_cell))
 		return FALSE
+	if (has_mock_cell)
+		return status == LIGHT_OK
 	if(pwr ? cell.charge >= pwr : cell.charge)
 		return status == LIGHT_OK
+	return FALSE
 
 // attempts to use power from the installed emergency cell, returns true if it does and false if it doesn't
 /obj/machinery/light/proc/use_emergency_power(pwr = LIGHT_EMERGENCY_POWER_USE)
 	if(!has_emergency_power(pwr))
 		return FALSE
-	if(cell.charge > 300) //it's meant to handle 120 W, ya doofus
+	var/obj/item/stock_parts/cell/real_cell = get_cell()
+	if(real_cell.charge > 300) // it's meant to handle 120 W, ya doofus
 		visible_message("<span class='warning'>[src] short-circuits from too powerful of a power cell!</span>")
 		burn_out()
 		return FALSE
-	cell.use(pwr)
-	set_light(brightness * bulb_emergency_brightness_mul, max(bulb_emergency_pow_min, bulb_emergency_pow_mul * (cell.charge / cell.maxcharge)), bulb_emergency_colour)
+	real_cell.use(pwr)
+	set_light(brightness * bulb_emergency_brightness_mul, max(bulb_emergency_pow_min, bulb_emergency_pow_mul * (real_cell.charge / real_cell.maxcharge)), bulb_emergency_colour)
 	return TRUE
 
 