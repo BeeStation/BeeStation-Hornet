14a612edac508c12bc24cffb1058bb6d38633da1
itsmeow <itsmeow@itsmeow.dev>
2024-01-12 01:45:51 -0600

    Fix tests

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -13,8 +13,7 @@ mkdir -p \
     $1/_maps \
 	$1/auxtools \
 	$1/code/datums/greyscale/json_configs \
-	$1/icons/obj/food \
-    $1/icons/runtime \
+	$1/icons \
     $1/sound/runtime \
     $1/strings \
     $1/tgui/public \
@@ -27,9 +26,8 @@ fi
 
 cp beestation.dmb beestation.rsc $1/
 cp -r _maps/* $1/_maps/
-cp -r code/datums/greyscale/json_configs/* $1/code/datums/greyscale/json_configs
-cp -r icons/obj/food/* $1/icons/obj/food/
-cp -r icons/runtime/* $1/icons/runtime/
+cp -r code/datums/greyscale/json_configs/* $1/code/datums/greyscale/json_configs/
+cp -r icons/* $1/icons/
 cp -r sound/runtime/* $1/sound/runtime/
 cp -r strings/* $1/strings/
 cp -r tgui/public/* $1/tgui/public/
