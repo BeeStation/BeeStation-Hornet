20d1f297e81b247276059bc4dec1825f4f276323
Tsar-Salat <62388554+Tsar-Salat@users.noreply.github.com>
2024-04-14 15:17:15 -0400

    prettier

--- tgui/packages/tgui/interfaces/common/StatusDisplayControls.tsx
+++ tgui/packages/tgui/interfaces/common/StatusDisplayControls.tsx
@@ -9,32 +9,15 @@ type Data = {
 
 export const StatusDisplayControls = (props, context) => {
   const { act, data } = useBackend<Data>(context);
-  const {
-    upperText: initialUpper,
-    lowerText: initialLower,
-    maxStatusLineLength,
-  } = data;
+  const { upperText: initialUpper, lowerText: initialLower, maxStatusLineLength } = data;
 
-  const [upperText, setUpperText] = useSharedState(
-    context,
-    'statusUpperText',
-    initialUpper
-  );
-  const [lowerText, setLowerText] = useSharedState(
-    context,
-    'statusLowerText',
-    initialLower
-  );
+  const [upperText, setUpperText] = useSharedState(context, 'statusUpperText', initialUpper);
+  const [lowerText, setLowerText] = useSharedState(context, 'statusLowerText', initialLower);
 
   return (
     <>
       <Section>
-        <Button
-          icon="toggle-off"
-          content="Off"
-          color="bad"
-          onClick={() => act('setStatusPicture', { picture: 'blank' })}
-        />
+        <Button icon="toggle-off" content="Off" color="bad" onClick={() => act('setStatusPicture', { picture: 'blank' })} />
         <Button
           icon="space-shuttle"
           content="Shuttle ETA / Off"
@@ -44,17 +27,9 @@ export const StatusDisplayControls = (props, context) => {
       </Section>
 
       <Section title="Graphics">
-        <Button
-          icon="flag"
-          content="Logo"
-          onClick={() => act('setStatusPicture', { picture: 'default' })}
-        />
+        <Button icon="flag" content="Logo" onClick={() => act('setStatusPicture', { picture: 'default' })} />
 
-        <Button
-          icon="bell-o"
-          content="Red Alert"
-          onClick={() => act('setStatusPicture', { picture: 'redalert' })}
-        />
+        <Button icon="bell-o" content="Red Alert" onClick={() => act('setStatusPicture', { picture: 'redalert' })} />
 
         <Button
           icon="exclamation-triangle"
@@ -62,39 +37,21 @@ export const StatusDisplayControls = (props, context) => {
           onClick={() => act('setStatusPicture', { picture: 'lockdown' })}
         />
 
-        <Button
-          icon="biohazard"
-          content="Biohazard"
-          onClick={() => act('setStatusPicture', { picture: 'biohazard' })}
-        />
+        <Button icon="biohazard" content="Biohazard" onClick={() => act('setStatusPicture', { picture: 'biohazard' })} />
       </Section>
 
       <Section title="Message">
         <Flex direction="column" align="stretch">
           <Flex.Item mb={1}>
-            <Input
-              fluid
-              maxLength={maxStatusLineLength}
-              value={upperText}
-              onChange={(_, value) => setUpperText(value)}
-            />
+            <Input fluid maxLength={maxStatusLineLength} value={upperText} onChange={(_, value) => setUpperText(value)} />
           </Flex.Item>
 
           <Flex.Item mb={1}>
-            <Input
-              fluid
-              maxLength={maxStatusLineLength}
-              value={lowerText}
-              onChange={(_, value) => setLowerText(value)}
-            />
+            <Input fluid maxLength={maxStatusLineLength} value={lowerText} onChange={(_, value) => setLowerText(value)} />
           </Flex.Item>
 
           <Flex.Item>
-            <Button
-              icon="comment-o"
-              onClick={() => act('setStatusMessage', { upperText, lowerText })}
-              content="Send"
-            />
+            <Button icon="comment-o" onClick={() => act('setStatusMessage', { upperText, lowerText })} content="Send" />
           </Flex.Item>
         </Flex>
       </Section>
