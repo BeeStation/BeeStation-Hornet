cb25a655adcfd15eb09843475995271c794a3bbf
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2024-09-01 09:31:20 +0100

    Screenshot tests only if they actually exist

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -21,6 +21,6 @@ DreamDaemon beestation.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
 
 mkdir -p data/screenshots_new
-cp -r ci_test/data/screenshots_new data/screenshots_new
+cp -r -u ci_test/data/screenshots_new data/screenshots_new
 
 cat ci_test/data/logs/ci/clean_run.lk
