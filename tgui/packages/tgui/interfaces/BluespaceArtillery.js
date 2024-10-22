369529d7919e4bce95735652d53dbd0eef744083
Dejaku51 <40302913+Dejaku51@users.noreply.github.com>
2024-10-23 00:32:30 +0200

    BSA changes once again! (#11558)
    * fixes BSA sounds not being heard + camera shake on fire
    
    * can't fire at target that doesn't exist anymore
    
    * can't fire multiple times at once, autoupdate, better UI
    
    * improves interfaces + fixes charging
    
    * 20 MW to fire
    
    * oopsie woopsie(make defines global)
    
    * GUHHHH
    
    * fuck you
    
    * changes defines to use full names and reverts changes done to comply with old defines
    
    * proper z level checking
    
    * properly handles refs!
    
    * reviews pt1
    
    * Update bsa.dm (test)
    
    * FAST_REF and remove debug
    
    * tabs
    
    * Fix FAST_REF issue?
    
    * we should skip if not on the same zlevel
    
    * don't call proc if no client
    
    * split target into target_ref and target_name

--- tgui/packages/tgui/interfaces/BluespaceArtillery.js
+++ tgui/packages/tgui/interfaces/BluespaceArtillery.js
@@ -1,64 +1,83 @@
 import { useBackend } from '../backend';
-import { Box, Button, LabeledList, NoticeBox, ProgressBar, Section } from '../components';
+import { Box, Button, LabeledList, NoticeBox, ProgressBar, Section, Stack } from '../components';
 import { Window } from '../layouts';
 
 export const BluespaceArtillery = (props, context) => {
   const { act, data } = useBackend(context);
-  const { notice, connected, unlocked, target, charge, max_charge } = data;
+  const { notice, connected, unlocked, target_ref, target_name, charge, max_charge, formatted_charge, targets } = data;
   return (
-    <Window width={400} height={280}>
+    <Window width={600} height={280}>
       <Window.Content>
         {!!notice && <NoticeBox>{notice}</NoticeBox>}
         {connected ? (
-          <>
-            <Section title="Charge">
-              <ProgressBar
-                ranges={{
-                  good: [1, Infinity],
-                  average: [0.2, 0.99],
-                  bad: [-Infinity, 0.2],
-                }}
-                value={charge / max_charge}
-              />
-            </Section>
-            <Section
-              title="Target"
-              buttons={<Button icon="crosshairs" disabled={!unlocked} onClick={() => act('recalibrate')} />}>
-              <Box color={target ? 'average' : 'bad'} fontSize="25px">
-                {target || 'No Target Set'}
-              </Box>
-            </Section>
-            <Section>
-              {unlocked ? (
-                <Box style={{ margin: 'auto' }}>
-                  <Button
-                    fluid
-                    content="FIRE"
-                    color="bad"
-                    disabled={!target}
-                    fontSize="30px"
-                    textAlign="center"
-                    lineHeight="46px"
-                    onClick={() => act('fire')}
-                  />
+          <Stack>
+            <Stack.Item grow={1}>
+              <Section title="Charge">
+                <ProgressBar
+                  animated
+                  step={1}
+                  minValue={0}
+                  maxValue={max_charge}
+                  value={charge}
+                  ranges={{
+                    good: [max_charge, Infinity],
+                    average: [max_charge * 0.2, max_charge * 0.99],
+                    bad: [-Infinity, max_charge * 0.2],
+                  }}>
+                  {formatted_charge}
+                </ProgressBar>
+              </Section>
+              <Section title="Target">
+                <Box color={target_ref ? 'average' : 'bad'} fontSize="25px">
+                  {target_ref ? target_name : 'No Target Set'}
                 </Box>
-              ) : (
-                <>
-                  <Box color="bad" fontSize="18px">
-                    Bluespace artillery is currently locked.
+              </Section>
+              <Section>
+                {unlocked ? (
+                  <Box style={{ margin: 'auto' }}>
+                    <Button
+                      fluid
+                      content="FIRE"
+                      color="bad"
+                      disabled={!target_ref}
+                      fontSize="30px"
+                      textAlign="center"
+                      lineHeight="46px"
+                      onClick={() => target_ref && act('fire')}
+                    />
                   </Box>
-                  <Box mt={1}>Awaiting authorization via keycard reader from at minimum two station heads.</Box>
-                </>
-              )}
-            </Section>
-          </>
+                ) : (
+                  <>
+                    <Box color="bad" fontSize="18px">
+                      Bluespace artillery is currently locked.
+                    </Box>
+                    <Box mt={1}>Awaiting authorization via keycard reader from at minimum two station heads.</Box>
+                  </>
+                )}
+              </Section>
+            </Stack.Item>
+            <Stack.Item width="200px">
+              <Section title="Available Targets" height="100%" scrollable fill>
+                <Box height="100%">
+                  {Object.entries(targets || {}).map(([key, value], index) => (
+                    <Box key={`${key}-${index}`} mb={1}>
+                      <Button
+                        fluid
+                        color={target_ref && key === target_ref ? 'bad' : 'blue'}
+                        content={value}
+                        onClick={() => act('set_target', { chosen_target: key })}
+                      />
+                    </Box>
+                  ))}
+                </Box>
+              </Section>
+            </Stack.Item>
+          </Stack>
         ) : (
           <Section>
-            <LabeledList>
-              <LabeledList.Item label="Maintenance">
-                <Button icon="wrench" content="Complete Deployment" onClick={() => act('build')} />
-              </LabeledList.Item>
-            </LabeledList>
+            <Box>
+              <Button icon="wrench" content="Complete Deployment" onClick={() => act('build')} />
+            </Box>
           </Section>
         )}
       </Window.Content>
