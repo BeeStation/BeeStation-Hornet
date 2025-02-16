0aa682fe1e1d7c8f6585b4b910122b750cf12d1e
RKz <falldownftw@gmail.com>
2025-02-15 20:09:02 -0500

    extinguisher utility
    https://github.com/tgstation/tgstation/pull/6605

--- tgui/packages/tgui/interfaces/Mecha/ArmPane.tsx
+++ tgui/packages/tgui/interfaces/Mecha/ArmPane.tsx
@@ -118,7 +118,6 @@ const BallisticStats = (props: {weapon: MechWeapon}, context) => {
 const MECHA_SNOWFLAKE_ID_SLEEPER = "sleeper_snowflake";
 const MECHA_SNOWFLAKE_ID_SYRINGE = "syringe_snowflake";
 const MECHA_SNOWFLAKE_ID_MODE = "mode_snowflake";
-const MECHA_SNOWFLAKE_ID_EXTINGUISHER = 'extinguisher_snowflake';
 const MECHA_SNOWFLAKE_ID_OREBOX_MANAGER = 'orebox_manager_snowflake';
 
 // Handles all the snowflake buttons and whatever
@@ -131,8 +130,6 @@ const Snowflake = (props: {weapon: MechWeapon}, context) => {
       return <SnowflakeSleeper weapon={props.weapon} />;
     case MECHA_SNOWFLAKE_ID_SYRINGE:
       return <SnowflakeSyringe weapon={props.weapon} />;
-    case MECHA_SNOWFLAKE_ID_EXTINGUISHER:
-      return <SnowflakeExtinguisher weapon={props.weapon} />;
     case MECHA_SNOWFLAKE_ID_MODE:
       return <SnowflakeMode weapon={props.weapon} />;
     default:
