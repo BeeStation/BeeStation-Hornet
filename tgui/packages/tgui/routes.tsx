8056523a0378ec9524dfdfba9d6162142cc11c8d
Boneyards <falldownftw@gmail.com>
2025-05-24 14:04:25 -0400

    unused

--- tgui/packages/tgui/routes.tsx
+++ tgui/packages/tgui/routes.tsx
@@ -11,7 +11,7 @@ import { Icon, Section, Stack } from './components';
 
 const requireInterface = require.context('./interfaces');
 
-export const routingError = (type: 'notFound' | 'missingExport', name: string) => () => {
+const routingError = (type: 'notFound' | 'missingExport', name: string) => () => {
   return (
     <Window>
       <Window.Content scrollable>
