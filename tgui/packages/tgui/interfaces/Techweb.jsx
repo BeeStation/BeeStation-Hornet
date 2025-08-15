68c1dbd7e416823679b1ef99293aba32440c9fb9
aramix273 <27755442+aramix273@users.noreply.github.com>
2025-08-15 21:07:23 +0300

    ensure 515 compatibility with explicit scroll overflow

--- tgui/packages/tgui/interfaces/Techweb.jsx
+++ tgui/packages/tgui/interfaces/Techweb.jsx
@@ -193,7 +193,7 @@ export const TechwebContent = (props) => {
           </Flex.Item>
         </Flex>
       </Flex.Item>
-      <Flex.Item className="Techweb__RouterContent" minHeight="0" grow={1} basis="auto">
+      <Flex.Item className="Techweb__RouterContent" minHeight="0" grow={1} basis="auto" style={{ overflow: 'scroll' }}>
         <TechwebRouter />
       </Flex.Item>
     </Flex>
