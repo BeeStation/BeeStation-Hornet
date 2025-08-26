d1eaaef5cd99ec34b02bc7c6ebdbcaf0214b6483
SunShine214 <sunshineprojects214@gmail.com>
2025-08-25 20:55:21 -0600

    LINTERS HATE ME

--- tgui/packages/tgui/interfaces/AtmosGasRig.jsx
+++ tgui/packages/tgui/interfaces/AtmosGasRig.jsx
@@ -1,5 +1,15 @@
 import { useBackend } from '../backend';
-import { Section, ProgressBar, NumberInput, Box, Button, Flex, BlockQuote, NoticeBox, Tooltip } from '../components';
+import {
+  Section,
+  ProgressBar,
+  NumberInput,
+  Box,
+  Button,
+  Flex,
+  BlockQuote,
+  NoticeBox,
+  Tooltip,
+} from '../components';
 import { Window } from '../layouts';
 
 export const AtmosGasRig = (props) => {
@@ -25,15 +35,34 @@ const DisplayValues = (position, barHeight, data) => {
   }
   return items.map((item, index) => (
     <>
-      <text key={item} x={position} y={barHeight * (index / count) + 12} fill="white" fontSize="12px">
+      <text
+        key={item}
+        x={position}
+        y={barHeight * (index / count) + 12}
+        fill="white"
+        fontSize="12px"
+      >
         {item}
       </text>
-      <rect x={position - 15} width="10" height="1" y={barHeight * (index / count) + 6} fill="grey" />
+      <rect
+        x={position - 15}
+        width="10"
+        height="1"
+        y={barHeight * (index / count) + 6}
+        fill="grey"
+      />
     </>
   ));
 };
 
-const DisplayGasBar = (position_bar, constant, data, barHeight, color, text) => {
+const DisplayGasBar = (
+  position_bar,
+  constant,
+  data,
+  barHeight,
+  color,
+  text,
+) => {
   const barOffset = 6;
   return (
     <Tooltip content={text}>
@@ -143,16 +172,93 @@ export const AtmosGasRigTemplate = (props) => {
         </Flex.Item>
         <Flex.Item>
           <svg width="200" height="1000">
-            <rect x={140 + svgOffset} y={barOffset} width="10" height={barHeight} fill="black" stroke="grey" />
-            <rect x={143 + svgOffset} y={barOffset} width="4" height={barHeight * (depth / data.max_depth)} fill="grey" />
-            {DisplayGasBar(125 + svgOffset, data.o2_constants, data, barHeight, 'blue', 'O2')} {/* O2 */}
-            {DisplayGasBar(115 + svgOffset, data.n2_constants, data, barHeight, 'red', 'N2')} {/* N2 */}
-            {DisplayGasBar(105 + svgOffset, data.plas_constants, data, barHeight, 'purple', 'Plasma')} {/* Plasma */}
-            {DisplayGasBar(125 + svgOffset, data.co2_constants, data, barHeight, 'grey', 'CO2')} {/* CO2 */}
-            {DisplayGasBar(125 + svgOffset, data.n2o_constants, data, barHeight, 'white', 'N2O')} {/* N2O */}
-            {DisplayGasBar(115 + svgOffset, data.nob_constants, data, barHeight, 'teal', 'Hypernoblium')} {/* Hypernoblium */}
-            {DisplayGasBar(125 + svgOffset, data.bz_constants, data, barHeight, 'brown', 'BZ')} {/* BZ */}
-            {DisplayGasBar(115 + svgOffset, data.plox_constants, data, barHeight, 'yellow', 'Plouxium')} {/* Plouxium */}
+            <rect
+              x={140 + svgOffset}
+              y={barOffset}
+              width="10"
+              height={barHeight}
+              fill="black"
+              stroke="grey"
+            />
+            <rect
+              x={143 + svgOffset}
+              y={barOffset}
+              width="4"
+              height={barHeight * (depth / data.max_depth)}
+              fill="grey"
+            />
+            {DisplayGasBar(
+              125 + svgOffset,
+              data.o2_constants,
+              data,
+              barHeight,
+              'blue',
+              'O2',
+            )}{' '}
+            {/* O2 */}
+            {DisplayGasBar(
+              115 + svgOffset,
+              data.n2_constants,
+              data,
+              barHeight,
+              'red',
+              'N2',
+            )}{' '}
+            {/* N2 */}
+            {DisplayGasBar(
+              105 + svgOffset,
+              data.plas_constants,
+              data,
+              barHeight,
+              'purple',
+              'Plasma',
+            )}{' '}
+            {/* Plasma */}
+            {DisplayGasBar(
+              125 + svgOffset,
+              data.co2_constants,
+              data,
+              barHeight,
+              'grey',
+              'CO2',
+            )}{' '}
+            {/* CO2 */}
+            {DisplayGasBar(
+              125 + svgOffset,
+              data.n2o_constants,
+              data,
+              barHeight,
+              'white',
+              'N2O',
+            )}{' '}
+            {/* N2O */}
+            {DisplayGasBar(
+              115 + svgOffset,
+              data.nob_constants,
+              data,
+              barHeight,
+              'teal',
+              'Hypernoblium',
+            )}{' '}
+            {/* Hypernoblium */}
+            {DisplayGasBar(
+              125 + svgOffset,
+              data.bz_constants,
+              data,
+              barHeight,
+              'brown',
+              'BZ',
+            )}{' '}
+            {/* BZ */}
+            {DisplayGasBar(
+              115 + svgOffset,
+              data.plox_constants,
+              data,
+              barHeight,
+              'yellow',
+              'Plouxium',
+            )}{' '}
+            {/* Plouxium */}
             {DisplayValues(155 + svgOffset, barHeight, data)}
           </svg>
         </Flex.Item>
