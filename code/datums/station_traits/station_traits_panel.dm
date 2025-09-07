8ae18d09e2a2385247894376a194caf8caa3c362
mrmanlikesbt <99309552+mrmanlikesbt@users.noreply.github.com>
2025-09-06 23:18:09 -0500

    fix tgui linters

--- code/datums/station_traits/station_traits_panel.dm
+++ code/datums/station_traits/station_traits_panel.dm
@@ -15,7 +15,7 @@
 	data["too_late_to_revert"] = too_late_to_revert()
 
 	var/list/current_station_traits = list()
-	for(var/datum/station_trait/station_trait as anything in SSstation.station_traits)
+	for (var/datum/station_trait/station_trait as anything in SSstation.station_traits)
 		current_station_traits += list(list(
 			"name" = station_trait.name,
 			"can_revert" = station_trait.can_revert,
@@ -32,7 +32,7 @@
 
 	var/list/valid_station_traits = list()
 
-	for(var/datum/station_trait/station_trait_path as anything in subtypesof(/datum/station_trait))
+	for (var/datum/station_trait/station_trait_path as anything in subtypesof(/datum/station_trait))
 		valid_station_traits += list(list(
 			"name" = initial(station_trait_path.name),
 			"path" = station_trait_path,
@@ -44,25 +44,25 @@
 
 /datum/station_traits_panel/ui_act(action, list/params, datum/tgui/ui, datum/ui_state/state)
 	. = ..()
-	if(.)
+	if (.)
 		return
 
 	switch (action)
-		if("revert")
+		if ("revert")
 			var/ref = params["ref"]
-			if(!ref)
+			if (!ref)
 				return TRUE
 
 			var/datum/station_trait/station_trait = locate(ref)
 
-			if(!istype(station_trait))
+			if (!istype(station_trait))
 				return TRUE
 
-			if(too_late_to_revert())
+			if (too_late_to_revert())
 				to_chat(usr, span_warning("It's too late to revert station traits, the round has already started!"))
 				return TRUE
 
-			if(!station_trait.can_revert)
+			if (!station_trait.can_revert)
 				stack_trace("[station_trait.type] can't be reverted, but was requested anyway.")
 				return TRUE
 
@@ -73,17 +73,17 @@
 			station_trait.revert()
 
 			return TRUE
-		if("setup_future_traits")
-			if(too_late_for_future_traits())
+		if ("setup_future_traits")
+			if (too_late_for_future_traits())
 				to_chat(usr, span_warning("It's too late to add future station traits, the round is already over!"))
 				return TRUE
 
 			var/list/new_future_traits = list()
 			var/list/station_trait_names = list()
 
-			for(var/station_trait_text in params["station_traits"])
+			for (var/station_trait_text in params["station_traits"])
 				var/datum/station_trait/station_trait_path = text2path(station_trait_text)
-				if(!ispath(station_trait_path, /datum/station_trait) || station_trait_path == /datum/station_trait)
+				if (!ispath(station_trait_path, /datum/station_trait) || station_trait_path == /datum/station_trait)
 					log_admin("[key_name(usr)] tried to set an invalid future station trait: [station_trait_text]")
 					to_chat(usr, span_warning("Invalid future station trait: [station_trait_text]"))
 					return TRUE
@@ -103,8 +103,8 @@
 			rustg_file_write(json_encode(params["station_traits"]), FUTURE_STATION_TRAITS_FILE)
 
 			return TRUE
-		if("clear_future_traits")
-			if(!future_traits)
+		if ("clear_future_traits")
+			if (!future_traits)
 				to_chat(usr, span_warning("There are no future station traits."))
 				return TRUE
 
--- tgui/packages/tgui/interfaces/StationTraitsPanel.tsx
+++ tgui/packages/tgui/interfaces/StationTraitsPanel.tsx
@@ -1,9 +1,17 @@
 import { filter, map } from 'common/collections';
-import { Box, Button, Divider, Dropdown, Stack, Tabs } from '../components';
-import { exhaustiveCheck } from 'common/exhaustive';
-import { BooleanLike } from 'common/react';
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Divider,
+  Dropdown,
+  Stack,
+  Tabs,
+} from 'tgui-core/components';
+import { exhaustiveCheck } from 'tgui-core/exhaustive';
+import type { BooleanLike } from 'tgui-core/react';
 
-import { useBackend, useLocalState } from '../backend';
+import { useBackend } from '../backend';
 import { Window } from '../layouts';
 
 type CurrentStationTrait = {
@@ -33,12 +41,12 @@ const FutureStationTraitsPage = (props) => {
   const { act, data } = useBackend<StationTraitsData>();
   const { future_station_traits } = data;
 
-  const [selectedTrait, setSelectedTrait] = useLocalState<string | null>('selectedFutureTrait', null);
+  const [selectedTrait, setSelectedTrait] = useState<string>('');
 
   const traitsByName = Object.fromEntries(
     data.valid_station_traits.map((trait) => {
       return [trait.name, trait.path];
-    })
+    }),
   );
 
   const traitNames = Object.keys(traitsByName);
@@ -70,19 +78,24 @@ const FutureStationTraitsPage = (props) => {
 
               let newStationTraits = [selectedPath];
               if (future_station_traits) {
-                const selectedTraitPaths = future_station_traits.map((trait) => trait.path);
+                const selectedTraitPaths = future_station_traits.map(
+                  (trait) => trait.path,
+                );
 
                 if (selectedTraitPaths.indexOf(selectedPath) !== -1) {
                   return;
                 }
 
-                newStationTraits = newStationTraits.concat(...selectedTraitPaths);
+                newStationTraits = newStationTraits.concat(
+                  ...selectedTraitPaths,
+                );
               }
 
               act('setup_future_traits', {
                 station_traits: newStationTraits,
               });
-            }}>
+            }}
+          >
             Add
           </Button>
         </Stack.Item>
@@ -106,10 +119,11 @@ const FutureStationTraitsPage = (props) => {
                         act('setup_future_traits', {
                           station_traits: filter(
                             map(future_station_traits, (t) => t.path),
-                            (p) => p !== trait.path
+                            (p) => p !== trait.path,
                           ),
                         });
-                      }}>
+                      }}
+                    >
                       Delete
                     </Button>
                   </Stack.Item>
@@ -126,7 +140,8 @@ const FutureStationTraitsPage = (props) => {
                 color="red"
                 icon="times"
                 tooltip="The next round will roll station traits randomly, just like normal"
-                onClick={() => act('clear_future_traits')}>
+                onClick={() => act('clear_future_traits')}
+              >
                 Run Station Traits Normally
               </Button>
             </Box>
@@ -144,7 +159,8 @@ const FutureStationTraitsPage = (props) => {
                 act('setup_future_traits', {
                   station_traits: [],
                 })
-              }>
+              }
+            >
               Prevent station traits from running next round
             </Button>
           </Box>
@@ -170,8 +186,10 @@ const ViewStationTraitsPage = (props) => {
                 color="red"
                 disabled={data.too_late_to_revert || !stationTrait.can_revert}
                 tooltip={
-                  (!stationTrait.can_revert && 'This trait is not revertable.') ||
-                  (data.too_late_to_revert && "It's too late to revert station traits, the round has already started.")
+                  (!stationTrait.can_revert &&
+                    'This trait is not revertable.') ||
+                  (data.too_late_to_revert &&
+                    "It's too late to revert station traits, the round has already started.")
                 }
                 icon="times"
                 onClick={() =>
@@ -191,7 +209,7 @@ const ViewStationTraitsPage = (props) => {
 };
 
 export const StationTraitsPanel = (props) => {
-  const [currentTab, setCurrentTab] = useLocalState('station_traits_tab', Tab.ViewStationTraits);
+  const [currentTab, setCurrentTab] = useState(Tab.ViewStationTraits);
 
   let currentPage;
 
@@ -207,20 +225,22 @@ export const StationTraitsPanel = (props) => {
   }
 
   return (
-    <Window title="Modify Station Traits" theme="admin" height={500} width={500}>
+    <Window title="Modify Station Traits" height={500} width={500}>
       <Window.Content scrollable>
         <Tabs>
           <Tabs.Tab
             icon="eye"
             selected={currentTab === Tab.ViewStationTraits}
-            onClick={() => setCurrentTab(Tab.ViewStationTraits)}>
+            onClick={() => setCurrentTab(Tab.ViewStationTraits)}
+          >
             View
           </Tabs.Tab>
 
           <Tabs.Tab
             icon="edit"
             selected={currentTab === Tab.SetupFutureStationTraits}
-            onClick={() => setCurrentTab(Tab.SetupFutureStationTraits)}>
+            onClick={() => setCurrentTab(Tab.SetupFutureStationTraits)}
+          >
             Edit
           </Tabs.Tab>
         </Tabs>
