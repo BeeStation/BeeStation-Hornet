98b09d0b606a332317b24e2acfd0d5dd119d80e3
mrmanlikesbt <99309552+mrmanlikesbt@users.noreply.github.com>
2025-10-07 20:09:55 -0500

    update SPACEMAN_DMM from 1.9 to 1.11

--- dependencies.sh
+++ dependencies.sh
@@ -17,7 +17,7 @@ export RUST_G_VERSION=3.8.0
 export NODE_VERSION_LTS=22.11.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.9
+export SPACEMAN_DMM_VERSION=suite-1.11
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.11.2
