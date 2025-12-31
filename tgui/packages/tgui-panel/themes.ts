fdba4790010ffac95520c51c2b829e628c5fb857
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2025-12-31 17:06:46 +0000

    Adds the blue chat theme

--- tgui/packages/tgui-panel/themes.ts
+++ tgui/packages/tgui-panel/themes.ts
@@ -4,7 +4,7 @@
  * @license MIT
  */
 
-export const THEMES = ['light', 'dark'];
+export const THEMES = ['light', 'dark', 'blue'];
 
 const COLOR_DARK_BG = '#202020';
 const COLOR_DARK_BG_DARKER = '#171717';
