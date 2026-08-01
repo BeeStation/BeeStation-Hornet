84d65299b2b15854b300681f8bb781b1b3335e09
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2026-08-01 15:12:59 +0300

    prettier

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/flavor_text.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/flavor_text.tsx
@@ -1,4 +1,4 @@
-import { Feature, FeatureLongTextInput } from '../base'
+import { Feature, FeatureLongTextInput } from '../base';
 
 export const flavor_text: Feature<string> = {
   name: 'Flavor Text',
